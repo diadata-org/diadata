00dead2da3f860c0f4a598507ae8e5122454b253
Sajal Kayan <sajal@turbobytes.com>
2018-08-21 17:49:10 +0700

    adding support for ERC20 tokens

---
+++ blockchain-scrapers/cmd/erc20/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+	"time"
+
+	"github.com/diadata-org/api-golang/dia"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+//Be sure to run : abigen --abi erc20.abi --pkg main --type Token --out erc20.go
+//erc20.abi taken from https://github.com/ethereum/wiki/wiki/Contract-ERC20-ABI
+var (
+	rpcEndpoint = flag.String("rpc", "http://127.0.0.1:8545", "geth RPC endpoint")
+	symbol      = flag.String("symbol", "", "ERC20 token name")
+	address     = flag.String("address", "", "The address at which the contract was deployed")
+	dev         = flag.Bool("dev", false, "Dev mode - prints to stdout instead of sending to dia")
+)
+
+func init() {
+	flag.Parse()
+	if *symbol == "" {
+		flag.Usage()
+		log.Fatal("Symbol name is required")
+	}
+	if *address == "" {
+		flag.Usage()
+		log.Fatal("Contract address is required")
+	}
+}
+
+func toFloat(in *big.Int, decimals uint8) float64 {
+	f := new(big.Float).SetInt(in)
+	fl, _ := f.Mul(f, big.NewFloat(math.Pow(float64(10), -1*float64(decimals)))).Float64()
+	return fl
+}
+
+func main() {
+	conn, err := ethclient.Dial(*rpcEndpoint)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+	// Instantiate the contract
+	token, err := NewToken(common.HexToAddress(*address), conn)
+	if err != nil {
+		log.Fatalf("Failed to instantiate a Token contract: %v", err)
+	}
+	//
+	config := dia.GetConfigApi()
+	client := dia.NewClient(config)
+	for {
+		//Infinite loop, sends tokenSupply every 10 seconds
+		supply, err := token.TotalSupply(nil)
+		if err != nil {
+			//Perhaps these should not be a fatal error?
+			log.Fatalf("Failed to retrieve token supply: %v", err)
+		}
+		decimals, err := token.Decimals(nil)
+		if err != nil {
+			log.Fatalf("Failed to retrieve token decimal: %v", err)
+		}
+		if *dev {
+			fmt.Printf("Symbol: %s ; totalSupply: %f\n", *symbol, toFloat(supply, decimals))
+		} else {
+			client.SendSupply(&dia.Supply{
+				Symbol:            *symbol,
+				CirculatingSupply: toFloat(supply, decimals),
+			})
+		}
+		time.Sleep(time.Second * 10)
+	}
+}
