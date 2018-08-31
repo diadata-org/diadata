1b9907df83a7742e10be59232af1c53e6611ecec
Stephan MIller <stephanmil@gmail.com>
2018-08-30 20:44:23 -0500

    xlm dockerfile and docker compose entry

--- blockchain-scrapers/cmd/xlm/xlm.go
+++ blockchain-scrapers/cmd/xlm/xlm.go
@@ -6,19 +6,22 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
+
+	"github.com/diadata-org/api-golang/dia"
 )
 
 type Supply struct {
-	Updatad     string  `json:"updatedAt"`
-	Total       float64 `json:"totalCoins"`
-	Available   float64 `json:"availableCoins"`
-	Distributed float64 `json:"distributedCoins"`
+	Updatad     string `json:"updatedAt"`
+	Total       string `json:"totalCoins"`
+	Available   string `json:"availableCoins"`
+	Distributed string `json:"distributedCoins"`
 	Programs    struct {
-		Direct      float64 `json:"directProgram"`
-		Bitcoin     float64 `json:"bitcoinProgram"`
-		Partnership float64 `json:"partnershipProgram"`
-		Build       int     `json:"buildChallenge"`
+		Direct      string `json:"directProgram"`
+		Bitcoin     string `json:"bitcoinProgram"`
+		Partnership string `json:"partnershipProgram"`
+		Build       string `json:"buildChallenge"`
 	} `json:"programs"`
 }
 
@@ -26,15 +29,15 @@ const endpoint = "https://dashboard.stellar.org/api/lumens"
 const symbol = "XLM"
 
 func main() {
-	// config := dia.GetConfigApi()
-	// if config == nil {
-	// 	panic("Couldnt load config")
-	// }
-	// client := dia.NewClient(config)
-	// if client == nil {
-	// 	panic("Couldnt load client")
-	// }
-	// prevResult := 0.0
+	config := dia.GetConfigApi()
+	if config == nil {
+		panic("Couldnt load config")
+	}
+	client := dia.NewClient(config)
+	if client == nil {
+		panic("Couldnt load client")
+	}
+	prevResult := 0.0
 	for {
 		response, err := http.Get(endpoint)
 		if err == nil {
@@ -44,19 +47,21 @@ func main() {
 			}
 			var supplyObject Supply
 			json.Unmarshal(responseData, &supplyObject)
-			result := supplyObject.Distributed
+			result, cerr := strconv.ParseFloat(supplyObject.Distributed, 64)
+			if cerr != nil {
+				fmt.Println("There was an integer conversion error:", err)
+			}
 			fmt.Printf("Symbol: %s ; circulatingSupply: %f\n", symbol, result)
-			// if prevResult != result {
-			// 	client.SendSupply(&dia.Supply{
-			// 		Symbol:            symbol,
-			// 		CirculatingSupply: result,
-			// 	})
-			// 	prevResult = result
-			// }
+			if prevResult != result {
+				client.SendSupply(&dia.Supply{
+					Symbol:            symbol,
+					CirculatingSupply: result,
+				})
+				prevResult = result
+			}
 		} else {
 			log.Println("Err communicating with node:", err)
 		}
 		time.Sleep(time.Second * 10)
 	}
-
 }
