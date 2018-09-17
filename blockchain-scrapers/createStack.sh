b8f147ddf9c51a871679a2796906b8117e6be5f5
Miguel Pires <miguelpires94@gmail.com>
2018-09-17 15:04:08 +0100

    Fixes to filenames

--- blockchain-scrapers/createStack.sh
+++ blockchain-scrapers/createStack.sh
@@ -5,8 +5,8 @@ function usage {
     echo "Usage: ./push.sh [-bdr] <service|all>"
     echo "  Each flag corresponds to an operation and takes a single argument. Multiple operations can be specified in the same command and will be run in that order. The supported flags/operations are:"
     echo ""
-    echo "  -b      builds an image for a service (builds everything when run with 'all'"
-    echo "  -d      deploys a service in a stack with its name (deploys every service in a stack named 'all' when run with 'all'"
+    echo "  -b      builds an image for a service (builds everything when run with 'all')"
+    echo "  -d      deploys a service in a stack with its name (deploys every service in a stack named 'all' when run with 'all')"
     echo "  -r      removes a service (or every service if run with 'all')"
     echo "  -h      display this usage guide"
     exit 1
@@ -24,13 +24,13 @@ function deploy {
 	case $1 in
 		# deploy every service in a stack called 'all'
 		all)
-			find $blockchain_dir -name docker-compose-*.yml -exec docker stack deploy -c {} $1 \;
+			find $blockchain_dir -name docker-compose.*.yml -exec docker stack deploy -c {} $1 \;
 			echo "Finished deployment" 1>&3
 			;;
 
 		# deploy a service in a stack with its name
 		*)
-			(docker stack deploy -c $(find $blockchain_dir -name docker-compose-$1.yml) $1 &&
+			(docker stack deploy -c $(find $blockchain_dir -name docker-compose.$1.yml) $1 &&
 				echo "Finished deployment" 1>&3) || error "Can't deploy '$1' (might not exist) "
 			;;
 	esac
@@ -42,13 +42,13 @@ function build {
 	case $1 in
 		# build every service
 		all) 
-			find $blockchain_dir -name Dockerfile* -exec sh -c "docker build -f {} -t $DOCKER_HUB_LOGIN/blockchain-scrapers_$(sed -n -e 's/^.*-//p' {}) $GOPATH" \;
+			find $blockchain_dir -name Dockerfile* -exec sh -c "docker build -f {} -t $DOCKER_HUB_LOGIN/blockchain-scrapers_$(sed -n -e 's/^.*-//p' {}):latest $GOPATH" \;
 			echo "Finished build" 1>&3			
 			;;
 
 		# build a particular service
 		*) 
-			(docker build -f $(find $blockchain_dir -name Dockerfile-$1) -t $DOCKER_HUB_LOGIN/blockchain-scrapers_$1 $GOPATH && 
+			(docker build -f $(find $blockchain_dir -name Dockerfile-$1) -t "$DOCKER_HUB_LOGIN/blockchain-scrapers_$1:latest" $GOPATH && 
 				echo "Finished build" 1>&3) || error "Can't build '$1' (might not exist)"
 			;;
 	esac
