d2eed837ff106b5f94a3167308765c5ba46a451f
Miguel Pires <miguelpires94@gmail.com>
2018-09-19 12:55:25 +0100

    Fixes (#53)
    * Separated scrapers and created individual docker-compose files
    
    * Added absolute path to secrets
    
    * Added a docker-compose to launch all services
    
    * Fixed path to secrets
    
    * Updated .gitignore
    
    * Added push.sh with build, deploy and remove features
    
    * Added dynamic search for services
    
    * Added multiple operations per command and improved messages
    
    * Fixed BCH config file
    
    * Made execution order equal to specified order
    
    * Removed old docker-compose
    
    * removing docker-compose.blockchain-scrapers.yml
    
    * Joined ETH and ERC20 in the same stack
    
    * Fixed image names
    
    * Changed filenames and script to match
    
    * Fixes to filenames
    
    * Update .gitignore; Changes to image names
    
    * Refactored stack names

--- blockchain-scrapers/createStack.sh
+++ blockchain-scrapers/createStack.sh
@@ -5,8 +5,8 @@ function usage {
     echo "Usage: ./push.sh [-bdr] <service|all>"
     echo "  Each flag corresponds to an operation and takes a single argument. Multiple operations can be specified in the same command and will be run in that order. The supported flags/operations are:"
     echo ""
-    echo "  -b      builds an image for a service (builds everything when run with 'all'"
-    echo "  -d      deploys a service in a stack with its name (deploys every service in a stack named 'all' when run with 'all'"
+    echo "  -b      builds an image for a service (builds everything when run with 'all')"
+    echo "  -d      deploys a service in a stack with its name (deploys every service in a stack named 'all' when run with 'all')"
     echo "  -r      removes a service (or every service if run with 'all')"
     echo "  -h      display this usage guide"
     exit 1
@@ -24,13 +24,13 @@ function deploy {
 	case $1 in
 		# deploy every service in a stack called 'all'
 		all)
-			find $blockchain_dir -name docker-compose-*.yml -exec docker stack deploy -c {} $1 \;
+			find $blockchain_dir -name docker-compose.*.yml -exec docker stack deploy -c {} $1 \;
 			echo "Finished deployment" 1>&3
 			;;
 
 		# deploy a service in a stack with its name
 		*)
-			(docker stack deploy -c $(find $blockchain_dir -name docker-compose-$1.yml) $1 &&
+			(docker stack deploy -c $(find $blockchain_dir -name docker-compose.$1.yml) $1 &&
 				echo "Finished deployment" 1>&3) || error "Can't deploy '$1' (might not exist) "
 			;;
 	esac
@@ -42,13 +42,13 @@ function build {
 	case $1 in
 		# build every service
 		all) 
-			find $blockchain_dir -name Dockerfile* -exec sh -c "docker build -f {} -t $DOCKER_HUB_LOGIN/blockchain-scrapers_$(sed -n -e 's/^.*-//p' {}) $GOPATH" \;
+			find $blockchain_dir -name Dockerfile* -exec sh -c "docker build -f {} -t $DOCKER_HUB_LOGIN/blockchain-scrapers_$(sed -n -e 's/^.*-//p' {}):latest $GOPATH" \;
 			echo "Finished build" 1>&3			
 			;;
 
 		# build a particular service
 		*) 
-			(docker build -f $(find $blockchain_dir -name Dockerfile-$1) -t $DOCKER_HUB_LOGIN/blockchain-scrapers_$1 $GOPATH && 
+			(docker build -f $(find $blockchain_dir -name Dockerfile-$1) -t "$DOCKER_HUB_LOGIN/blockchain-scrapers_$1:latest" $GOPATH && 
 				echo "Finished build" 1>&3) || error "Can't build '$1' (might not exist)"
 			;;
 	esac
@@ -74,16 +74,17 @@ fi
 
 # create necessary volumes 
 blockchain_dir=$GOPATH/src/github.com/diadata-org/api-golang/blockchain-scrapers/blockchains
+export STACKNAME=blockchain-scrapers
 
 if [ ! -e $HOME/srv ]; then
-	sudo mkdir -p $HOME/srv/bitcoin $HOME/srv/geth $HOME/srv/monero $HOME/srv/litecoin $HOME/srv/cardano $HOME/srv/bitcoin-cash $HOME/srv/neo 
+	sudo mkdir -p $HOME/srv/bitcoin $HOME/srv/geth $HOME/srv/monero $HOME/srv/litecoin  \
+		$HOME/srv/cardano $HOME/srv/bitcoin-cash $HOME/srv/neo $HOME/srv/dash
 	sudo chmod -R 777 $HOME/srv
 fi
 
 exec 3>&1 4>&2
-
-# parse input (silencing the output)
-{
+# parse input (uncomment these to silence non-script output)
+#{
 
 	while getopts "d:b:r:" opt; do
 		case $opt in
@@ -104,4 +105,4 @@ exec 3>&1 4>&2
 				error "Unknown operation '$opt'"
 		esac
 	done
-} &> /dev/null
+#} &> /dev/null
