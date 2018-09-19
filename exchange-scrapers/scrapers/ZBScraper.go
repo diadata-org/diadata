30a4ec0a8d5e4332560134a211fd07a54fc8c65a
Montana Flynn <montana949@gmail.com>
2018-09-19 13:19:18 +0700

    Add ZB exchange scraper (#48)

---
+++ exchange-scrapers/scrapers/ZBScraper.go
@@ -0,0 +1,212 @@
+package scrapers
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/diadata-org/api-golang/dia"
+	ws "github.com/gorilla/websocket"
+)
+
+var ZBSocketURL string = "wss://api.zb.cn:9999/websocket"
+
+type ZBSubscribe struct {
+	Event   string `json:"event"`
+	Channel string `json:"channel"`
+}
+
+type ZBTradeResponse struct {
+	DataType string `json:"dataType"`
+	Data     []struct {
+		Amount    string `json:"amount"`
+		Price     string `json:"price"`
+		Tid       int    `json:"tid"`
+		Date      int    `json:"date"`
+		Type      string `json:"type"`
+		TradeType string `json:"trade_type"`
+	} `json:"data"`
+	Channel string `json:"channel"`
+}
+
+type ZBScraper struct {
+	wsClient *ws.Conn
+	// signaling channels for session initialization and finishing
+	//initDone     chan nothing
+	shutdown     chan nothing
+	shutdownDone chan nothing
+	// error handling; to read error or closed, first acquire read lock
+	// only cleanup method should hold write lock
+	errorLock sync.RWMutex
+	error     error
+	closed    bool
+	// used to keep track of trading pairs that we subscribed to
+	pairScrapers map[string]*ZBPairScraper
+}
+
+// NewZBScraper returns a new ZBScraper for the given pair
+func NewZBScraper() *ZBScraper {
+
+	s := &ZBScraper{
+		shutdown:     make(chan nothing),
+		shutdownDone: make(chan nothing),
+		pairScrapers: make(map[string]*ZBPairScraper),
+		error:        nil,
+	}
+
+	var wsDialer ws.Dialer
+	SwConn, _, err := wsDialer.Dial(ZBSocketURL, nil)
+
+	if err != nil {
+		println(err.Error())
+	}
+
+	s.wsClient = SwConn
+	go s.mainLoop()
+	return s
+}
+
+// runs in a goroutine until s is closed
+func (s *ZBScraper) mainLoop() {
+
+	for true {
+
+		message := &ZBTradeResponse{}
+		if err := s.wsClient.ReadJSON(&message); err != nil {
+			println(err.Error())
+			break
+		}
+
+		for _, trade := range message.Data {
+			ps, ok := s.pairScrapers[strings.TrimSuffix(message.Channel, "_trades")]
+			if !ok {
+				log.Printf("unknown pair: %s", message.Channel)
+				continue
+			}
+
+			f64Price, err := strconv.ParseFloat(trade.Price, 64)
+			if err != nil {
+				log.Printf("error parsing price: %s", trade.Price)
+				continue
+			}
+
+			f64Volume, err := strconv.ParseFloat(trade.Amount, 64)
+			if err != nil {
+				log.Printf("error parsing volume: %s", trade.Price)
+				continue
+			}
+
+			if trade.Type == "sell" {
+				f64Volume = -f64Volume
+			}
+
+			t := &dia.Trade{
+				Symbol:         ps.Pair().Symbol,
+				Pair:           strings.TrimSuffix(message.Channel, "_trades"),
+				Price:          f64Price,
+				Volume:         f64Volume,
+				Time:           time.Unix(int64(trade.Date), 0),
+				ForeignTradeID: fmt.Sprint(trade.Tid),
+				Source:         dia.ZBExchange,
+			}
+			ps.chanTrades <- t
+		}
+	}
+}
+
+func (s *ZBScraper) cleanup(err error) {
+	s.errorLock.Lock()
+	defer s.errorLock.Unlock()
+
+	if err != nil {
+		s.error = err
+	}
+	s.closed = true
+
+	close(s.shutdownDone)
+}
+
+// Close closes any existing API connections, as well as channels of
+// PairScrapers from calls to ScrapePair
+func (s *ZBScraper) Close() error {
+
+	if s.closed {
+		return errors.New("ZBScraper: Already closed")
+	}
+
+	close(s.shutdown)
+	<-s.shutdownDone
+	s.errorLock.RLock()
+	defer s.errorLock.RUnlock()
+	return s.error
+}
+
+// ScrapePair returns a PairScraper that can be used to get trades for a single pair from
+// this APIScraper
+func (s *ZBScraper) ScrapePair(pair dia.Pair) (PairScraper, error) {
+	s.errorLock.RLock()
+	defer s.errorLock.RUnlock()
+
+	if s.error != nil {
+		return nil, s.error
+	}
+
+	if s.closed {
+		return nil, errors.New("ZBScraper: Call ScrapePair on closed scraper")
+	}
+
+	ps := &ZBPairScraper{
+		parent:     s,
+		pair:       pair,
+		chanTrades: make(chan *dia.Trade),
+	}
+
+	s.pairScrapers[pair.ForeignName] = ps
+
+	a := &ZBSubscribe{
+		Event:   "addChannel",
+		Channel: pair.ForeignName + "_trades",
+	}
+
+	if err := s.wsClient.WriteJSON(a); err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return ps, nil
+}
+
+// ZBPairScraper implements PairScraper for ZB
+type ZBPairScraper struct {
+	parent     *ZBScraper
+	pair       dia.Pair
+	chanTrades chan *dia.Trade
+	closed     bool
+}
+
+// Close stops listening for trades of the pair associated with s
+func (ps *ZBPairScraper) Close() error {
+	return nil
+}
+
+// Channel returns a channel that can be used to receive trades
+func (ps *ZBPairScraper) Channel() chan *dia.Trade {
+	return ps.chanTrades
+}
+
+// Error returns an error when the channel Channel() is closed
+// and nil otherwise
+func (ps *ZBPairScraper) Error() error {
+	s := ps.parent
+	s.errorLock.RLock()
+	defer s.errorLock.RUnlock()
+	return s.error
+}
+
+// Pair returns the pair this scraper is subscribed to
+func (ps *ZBPairScraper) Pair() dia.Pair {
+	return ps.pair
+}
