a3eab8efcfb0bf2feed331e3e96d8afd3aa15af6
Franck <franck@mumblecore.org>
2018-09-27 17:56:16 +0200

    Adding push.sh

---
+++ push.sh
@@ -0,0 +1,279 @@
+#!/bin/bash
+
+if [  "$DOCKER_HUB_LOGIN" == "" ]
+then
+	echo missing env DOCKER_HUB_LOGIN variable
+	exit
+fi
+
+REMOTE="n"
+NOCACHE="--no-cache"
+LINENUMBER=""
+DEPLOY="n"
+SLAVES="n"
+WATCH="n"
+DIRECTORY=`pwd`
+MACHINE=""
+STACKNAME=""
+
+selectLocalHost() {
+	echo DOCKER_HOST:$DOCKER_HOST
+	unset DOCKER_HOST
+	unset DOCKER_TLS_VERIFY
+	unset DOCKER_CERT_PATH
+	echo switching to LocalHost
+	env | grep DOCKER
+}
+
+checkMachine() {
+	if [ ! -d ~/secrets/live/$1/ ]; then
+		echo "no configuration for machine $1 in your ~/secrets directory"
+		exit 
+	fi
+}
+
+selectMachine() {
+
+export DOCKER_HOST="tcp://$1:2376"
+export DOCKER_ID_USER="reg"
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_CERT_PATH=~/secrets/live/$1/
+
+H=`echo $1 | cut -d"." -f2-3`
+
+#export DOCKER_HUB_LOGIN=registry.$H
+
+if [ -d "$DOCKER_CERT_PATH" ]; then
+	rm ~/secrets/live/$1/ca.pem
+	rm ~/secrets/live/$1/key.pem
+	ln -s ~/secrets/live/$1/fullchain.pem ~/secrets/live/$1/ca.pem 2>&1 > /dev/null
+	ln -s ~/secrets/live/$1/privkey.pem ~/secrets/live/$1/key.pem 2>&1 > /dev/null
+else
+	echo cant find machine $1
+	exit
+fi
+}
+
+selectMaster() {
+	if [ "$REMOTE" != "n" ]
+	then
+		selectMachine $MACHINE
+		echo switching to $DOCKER_HOST
+	fi
+}
+
+
+selectSlave() {
+	if [ "$REMOTE" != "n" ]
+	then
+		export DOCKER_HOST="tcp://TOFIX:2376"
+		export DOCKER_ID_USER="TOFIX"
+		export DOCKER_TLS_VERIFY="TOFIX"
+		export DOCKER_CERT_PATH="TOFIX"
+		echo switching to $DOCKER_HOST
+	fi
+}
+
+watch_service() {
+	docker service logs $1 -f 
+}
+
+rebuild_service() {
+
+echo building $1 NOCACHE: $NOCACHE
+
+GITHUB_DIRECTORY=`pwd | sed 's/\.com.*/.com/'`
+echo cd $GITHUB_DIRECTORY
+cd $GITHUB_DIRECTORY
+selectLocalHost
+docker-compose  -f $DIRECTORY/$COMPOSE_FILE build $NOCACHE $1 
+
+if [ $? -eq 0 ]
+then
+
+	IMAGE=${DOCKER_HUB_LOGIN}/${STACKNAME}_$1
+
+	echo pushing $IMAGE
+
+	docker push $IMAGE
+	if [ $? -ne 0 ]
+	then
+		echo error pushing image
+		exit 0
+	fi
+	if [ "$REMOTE" != "n" ]
+	then
+		if [ "$SLAVES" != "n" ]
+		then
+			selectSlave
+			echo $DOCKER_HOST
+			docker pull $IMAGE
+		fi
+		selectMaster
+		docker pull $IMAGE
+	fi
+
+    SERVICE=${STACKNAME}_$1
+	echo service update on $DOCKER_HOST for $SERVICE
+	RESTARTED=1
+	while [ $RESTARTED -ne 0 ]
+	do
+		docker service update --with-registry-auth --force ${SERVICE} --image ${IMAGE}
+		RESTARTED=$?
+		if [ $RESTARTED -ne 0 ]
+		then
+			echo "failed on update service ${SERVICE} trying again in 10 secs"
+			echo "run this to get logs:"
+			echo "docker service logs -f ${SERVICE}"
+			sleep 10
+		fi
+	done
+	
+	if [ "$WATCH" == "y" ]
+	then
+		watch_service $SERVICE
+	fi
+
+else 
+	exit
+fi
+
+cd -
+}
+
+clean() {
+docker volume rm $(docker volume ls -qf dangling=true) 2> /dev/null
+docker system prune -f
+}
+
+main() {
+
+while getopts "n:hr:dcws:" opt; do
+  case $opt in
+  	s)
+     	export STACKNAME=$OPTARG
+		export COMPOSE_FILE=`find . | grep docker-compose | grep "$STACKNAME\.yml"`
+   	    if [ ! -f "$COMPOSE_FILE" ]
+	    then
+			echo cant find stack $STACKNAME
+			exit
+     	fi
+		;;
+  	w)
+	  WATCH="y"
+	  ;;
+  	c)
+      echo using cache
+	  NOCACHE=""
+	  ;;
+  	d)
+	  DEPLOY="y"
+	  ;;
+    n)
+      echo "-n was triggered, Parameter: $OPTARG" >&2
+      LINENUMBER=$OPTARG
+      ;;
+    h)
+      echo "-c to use cache" >&2
+      echo "-d to deploy stack" >&2
+      echo "-r for remote" >&2
+      echo "-n <x> for do x" >&2
+      exit
+      ;;
+    r)
+      REMOTE="y"
+      MACHINE=$OPTARG
+      if [ "$MACHINE" == "" ]
+      then
+      	echo you must select a machine
+      	exit
+      fi
+      checkMachine $MACHINE
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$STACKNAME" == "" ]
+then
+	echo give -s option to choose stack
+	exit
+fi
+
+
+if [ "$DEPLOY" = "y" ] 
+then
+	  selectMaster
+	  if [ $STACKNAME == "dia" ]
+	  then
+	  	docker stack deploy -c docker-compose.kafka.yml kafka
+	  fi
+	  echo deploying on stack  $DIRECTORY/$COMPOSE_FILE $STACKNAME
+	  docker stack deploy -c $DIRECTORY/$COMPOSE_FILE $STACKNAME
+	  exit
+fi
+
+NUMBER=0
+FILETMP=/tmp/ok
+rm -f $FILETMP
+
+docker-compose -f $DIRECTORY/$COMPOSE_FILE config --services | sort -u > /tmp/services.txt.$$
+
+if [ $? -ne 0 ]
+then
+	echo error in docker compose file
+	exit
+fi
+
+cat /tmp/services.txt.$$ | grep -v visualizer | while read line; do echo "$NUMBER-$line" >> $FILETMP;NUMBER=`expr $NUMBER + 1`; done
+
+rm -f /tmp/services.txt.$$
+
+cat $FILETMP
+echo "a-for all"
+
+if [ "$LINENUMBER" == "" ]
+then
+read LINENUMBER
+fi
+
+
+if [ "$LINENUMBER" == "a" ]
+then
+	NUMBER=`wc -l $FILETMP | cut -f1 -d\ `
+	for (( c=0; c<$NUMBER; c++ ))
+	do  
+		 RESULT=`cat $FILETMP | grep $c | cut -f2 -d\- `
+ 	 	 rebuild_service $RESULT
+	done
+else
+	RESULT=`cat $FILETMP | grep $LINENUMBER | cut -f2 -d\- `
+	rebuild_service $RESULT
+fi
+
+clean
+
+if [ "$SLAVES" != "n" ]
+then
+	selectSlave
+	clean
+fi
+
+selectLocalHost
+clean
+
+echo "docker stack services $STACKNAME  "
+echo "run this to get logs:"
+echo "docker service logs -f ${SERVICE}"
+}
+
+
+main $*
+
+echo bye bye
