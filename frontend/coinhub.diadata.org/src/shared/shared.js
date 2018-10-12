23307ddeb5133ddfae25313f079dc112e00ee420
Mulili Nzuki <mulili.nzuki@gmail.com>
2018-10-12 15:47:51 +0300

    changes requested (#80)

--- frontend/coinhub.diadata.org/src/shared/shared.js
+++ frontend/coinhub.diadata.org/src/shared/shared.js
@@ -9,7 +9,7 @@ export default {
 
 		try{
 		 res = await axios.get('https://api.diadata.org/v1/coins');
-		 coins = res.data.Coins.reverse();
+		 coins = res.data;
 		}
 		catch(error) {
 		  console.log(error);
@@ -18,8 +18,9 @@ export default {
 		return coins;
 	  
 	},
-	formatCoinData: function(coins) {
-      const coinData = [];
+	formatCoinData: function(coins, change) {
+      let coinDataUSD = [];
+      let coinDataEUR = [];
 
       for (let i = 0; i < coins.length; i++) {
         const coin = coins[i];
@@ -32,46 +33,101 @@ export default {
     		}
         const coinSymbol = coin.Symbol;
         const coinName = coin.Name;
-        const coinPrice = coin.Price;
-        const coinPriceFormatted = coinPrice < 1 ? '$'.concat(numeral(coin.Price).format('0.0[0000]')) : '$'.concat(numeral(coin.Price).format('0,0.00'));
-        let change24 = (coin.Price - coin.PriceYesterday) / coin.PriceYesterday * 100;
-        const change24Formatted = change24 !== Number.POSITIVE_INFINITY ? numeral(change24).format('0,0.00').concat('%') : 'N/A';
-        change24 = change24 !== Number.POSITIVE_INFINITY ? change24 : Number.NEGATIVE_INFINITY ;
+
+        // coin price
+        // USD
+        const coinPriceUSD = coin.Price;
+        const coinPriceUSDFormatted = coinPriceUSD < 1 ? '$'.concat(numeral(coinPriceUSD).format('0.0[0000]')) : '$'.concat(numeral(coinPriceUSD).format('0,0.00'));
+        const coinPriceYesterdayUSD = coin.PriceYesterday;
+        // EUR
+        const coinPriceEUR = coin.Price / change.EURUSD;
+        const coinPriceEURFormatted = coinPriceEUR < 1 ? '€'.concat(numeral(coinPriceEUR).format('0.0[0000]')) : '€'.concat(numeral(coinPriceEUR).format('0,0.00'));
+        const coinPriceYesterdayEUR = coin.PriceYesterday / change.EURUSDYesterday;
+
+        // change 24
+        // USD
+        let change24USD = (coinPriceUSD  - coinPriceYesterdayUSD ) / coinPriceYesterdayUSD * 100;
+        const change24USDFormatted = change24USD !== Number.POSITIVE_INFINITY ? numeral(change24USD).format('0,0.00').concat('%') : 'N/A';
+        change24USD = change24USD !== Number.POSITIVE_INFINITY ? change24USD : Number.NEGATIVE_INFINITY ;
+        //EUR
+        let change24EUR = (coinPriceEUR  - coinPriceYesterdayEUR ) / coinPriceYesterdayEUR * 100;
+        const change24EURFormatted = change24EUR !== Number.POSITIVE_INFINITY ? numeral(change24EUR).format('0,0.00').concat('%') : 'N/A';
+        change24EUR = change24EUR !== Number.POSITIVE_INFINITY ? change24EUR : Number.NEGATIVE_INFINITY ;
+
+        // Price graph
         const priceGraph = `https://api.diadata.org/v1/chart/${coin.Symbol}`;
-        const volume24 = coin.VolumeYesterdayUSD;
-        const volume24Formatted = '$'.concat(numeral(coin.VolumeYesterdayUSD).format('0,0'));
-        const marketCapFormatted = '$'.concat(numeral(coin.Price * coin.CirculatingSupply).format('0,0'));
-        const marketCap = coin.Price * coin.CirculatingSupply;
-        const circulatingSupplyFormattedWithoutSymbol = numeral(coin.CirculatingSupply).format('0,0');
-        const circulatingSupplyFormatted = numeral(coin.CirculatingSupply).format('0,0').concat(` (${coin.Symbol})`);
+
+        // volume24
+        // USD
+        const volume24USD = coin.VolumeYesterdayUSD;
+        const volume24USDFormatted = '$'.concat(numeral(volume24USD).format('0,0'));
+        // EUR
+        const volume24EUR = coin.VolumeYesterdayUSD / change.EURUSDYesterday;
+        const volume24EURFormatted = '€'.concat(numeral(volume24EUR).format('0,0'));
+
+        // circulating supply
         const circulatingSupply = coin.CirculatingSupply;
+        const circulatingSupplyFormattedWithoutSymbol = numeral(circulatingSupply).format('0,0');
+        const circulatingSupplyFormatted = numeral(circulatingSupply).format('0,0').concat(` (${coin.Symbol})`);
+
+        // market cap
+        // USD
+        const marketCapUSDFormatted = '$'.concat(numeral(coinPriceUSD * circulatingSupply ).format('0,0'));
+        const marketCapUSD = coinPriceUSD * circulatingSupply;
+        // EUR
+        const marketCapEURFormatted = '€'.concat(numeral(coinPriceEUR * circulatingSupply ).format('0,0'));
+        const marketCapEUR = coinPriceEUR * circulatingSupply;
+      
         const oracle = require('@/assets/icons/oracle_icon.png');
 
-        coinData.push({coinImage, 
+        coinDataUSD.push({coinImage, 
                        coinSymbol, 
                        coinName,
-                       coinPrice, 
-                       coinPriceFormatted,
-                       change24, 
-                       change24Formatted,
+                       coinPrice:coinPriceUSD, 
+                       coinPriceFormatted:coinPriceUSDFormatted,
+                       change24:change24USD, 
+                       change24Formatted:change24USDFormatted,
                        priceGraph, 
-                       volume24, 
-                       volume24Formatted,
-                       marketCap, 
-                       marketCapFormatted, 
+                       volume24:volume24USD, 
+                       volume24Formatted:volume24USDFormatted,
+                       marketCap:marketCapUSD, 
+                       marketCapFormatted:marketCapUSDFormatted, 
+                       circulatingSupply, 
+                       circulatingSupplyFormatted,
+                       circulatingSupplyFormattedWithoutSymbol, 
+                       oracle});
+
+        coinDataEUR.push({coinImage, 
+                       coinSymbol, 
+                       coinName,
+                       coinPrice:coinPriceEUR, 
+                       coinPriceFormatted:coinPriceEURFormatted,
+                       change24:change24EUR, 
+                       change24Formatted:change24EURFormatted,
+                       priceGraph, 
+                       volume24:volume24EUR, 
+                       volume24Formatted:volume24EURFormatted,
+                       marketCap:marketCapEUR, 
+                       marketCapFormatted:marketCapEURFormatted, 
                        circulatingSupply, 
                        circulatingSupplyFormatted,
                        circulatingSupplyFormattedWithoutSymbol, 
                        oracle});
       }
 
-      const descendingOrder = sortBy(coinData, 'marketCap').reverse();
+      coinDataUSD = sortBy(coinDataUSD, 'marketCap').reverse();
+      coinDataEUR = sortBy(coinDataEUR, 'marketCap').reverse();
 
-      descendingOrder.forEach((coin,index) => {
+      coinDataUSD.forEach((coin,index) => {
         const rank = (index + 1);
         coin.rank = rank;
       });
-      return descendingOrder;
+
+      coinDataEUR.forEach((coin,index) => {
+        const rank = (index + 1);
+        coin.rank = rank;
+      });
+      return {coinDataUSD,coinDataEUR};
   	},
 
 }
