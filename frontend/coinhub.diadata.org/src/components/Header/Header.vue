9b64d46c5104442b7cf8882481abc11ad9a2ab9a
mulili <mulili.nzuki@gmail.com>
2018-10-15 15:22:51 +0300

    header changes

--- frontend/coinhub.diadata.org/src/components/Header/Header.vue
+++ frontend/coinhub.diadata.org/src/components/Header/Header.vue
@@ -1,37 +1,75 @@
 <style src="./Header.css" scoped></style>
 <template>
-  <header>
-    <b-navbar type="dark" variant="info">
-      <b-navbar-brand href="/">CoinHub by DIA</b-navbar-brand>
-	    <b-navbar-nav class="ml-auto logo">
-	        <b-link href="https://diadata.org" target="_blank">
-	    		<b-img :src="logo" fluid alt="logo" width="150" />
-	    	</b-link>
-	    </b-navbar-nav>
-      <div class="w-100 sub-header text-left">
+  <div>
+    <header class="lg-screens">
+      <b-navbar type="dark" variant="info">
         <b-row>
-          <b-col sm="12" md="8" lg="5" xl="4">
-              Decentrally sourced and maintained crypto-financial-data
+          <b-col>
+            <b-link href="https://diadata.org" target="_blank">
+              <b-img :src="logo" fluid alt="logo" width="150" />
+            </b-link>
           </b-col>
-          <b-col sm="12" md="4" lg="5" xl="3">
-            <b-navbar-nav class="search-form">
-              <b-nav-form>
-                <b-form-input size="md" class="search-input mr-lg-1" type="text" placeholder="Search for Digital Asset"/>
-              </b-nav-form>
-            </b-navbar-nav>
-          </b-col>
-          <b-col sm="12" md="12" lg="2" xl="2" offset-xl="3">
-            <b-dropdown id="dd-currency" text="EUR">
-              <b-dropdown-item>EUR</b-dropdown-item>
-              <b-dropdown-item>USD</b-dropdown-item>
-              <b-dropdown-item>NOK</b-dropdown-item>
-            </b-dropdown>
+          <b-col>
+            <div class="w-100 text-right">
+              <b-navbar-brand href="/">CoinHub by DIA</b-navbar-brand>
+              <br>
+              <div class="sub-header">
+                  Decentrally sourced and maintained crypto-financial-data
+              </div>
+            </div>
+            
           </b-col>
         </b-row>
-      </div>
-     
-    </b-navbar>
-  </header>
+        <b-row class="header-components">
+            <b-col md="12" lg="5" xl="4" >
+              <b-navbar-nav class="search-form">
+                <b-nav-form>
+                  <b-form-input size="md" class="search-input" type="text" placeholder="Search for Digital Asset"/>
+                </b-nav-form>
+              </b-navbar-nav>
+            </b-col>
+            <b-col md="12" lg="1" offset-lg="6" xl="1" offset-xl="7">
+              <b-dropdown id="dd-currency" text="EUR">
+                <b-dropdown-item>EUR</b-dropdown-item>
+                <b-dropdown-item>USD</b-dropdown-item>
+                <b-dropdown-item>NOK</b-dropdown-item>
+              </b-dropdown>
+            </b-col>    
+        </b-row>
+      </b-navbar>
+    </header>
+    <header class="sm-screens">
+      <b-navbar type="dark" variant="info">
+        <b-row>
+          <b-col>&nbsp;</b-col>
+          <b-col>
+            <b-link href="/">
+              <b-img :src="logo" fluid alt="logo" width="150" />
+            </b-link>
+            <b-navbar-nav class="search-form d-none">
+                <b-nav-form>
+                  <b-form-input size="md" class="search-input" type="text" placeholder="Search for Digital Asset"/>
+                </b-nav-form>
+            </b-navbar-nav>
+          </b-col>
+          <b-col class="text-right">
+            <b-link>
+              <font-awesome-icon icon="search" />
+            </b-link>
+          </b-col>
+        </b-row>
+        <b-row class="header-components">
+            <b-col md="12" lg="1" offset-lg="6" xl="1" offset-xl="7">
+              <b-dropdown id="dd-currency" text="EUR">
+                <b-dropdown-item>EUR</b-dropdown-item>
+                <b-dropdown-item>USD</b-dropdown-item>
+                <b-dropdown-item>NOK</b-dropdown-item>
+              </b-dropdown>
+            </b-col>    
+        </b-row>
+      </b-navbar>
+    </header>
+  </div>
 </template>
 <script src="./Header.js"></script>
 
--- frontend/coinhub.diadata.org/src/main.js
+++ frontend/coinhub.diadata.org/src/main.js
@@ -2,10 +2,17 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
 
+
+library.add(faSearch);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
