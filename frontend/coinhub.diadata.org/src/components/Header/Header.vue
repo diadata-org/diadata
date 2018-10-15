4dee87a4dcb887b3e2869b4abe196b5c632ca874
mulili <mulili.nzuki@gmail.com>
2018-10-15 09:02:10 +0300

    header changes

--- frontend/coinhub.diadata.org/src/components/Header/Header.vue
+++ frontend/coinhub.diadata.org/src/components/Header/Header.vue
@@ -10,15 +10,22 @@
 	    </b-navbar-nav>
       <div class="w-100 sub-header text-left">
         <b-row>
-          <b-col sm="12" md="8" lg="8">
+          <b-col sm="12" md="8" lg="5">
               Decentrally sourced and maintained crypto-financial-data
           </b-col>
-          <b-col sm="12" md="4" lg="4">
+          <b-col sm="12" md="4" lg="5">
             <b-navbar-nav class="search-form">
               <b-nav-form>
-                <b-form-input size="sm" class="search-input mr-lg-5" type="text" placeholder="Search for Digital Asset"/>
+                <b-form-input size="md" class="search-input mr-lg-1" type="text" placeholder="Search for Digital Asset"/>
               </b-nav-form>
             </b-navbar-nav>
+          </b-col>
+          <b-col sm="12" md="12" lg="2">
+            <b-dropdown id="dd-currency" text="EUR" class="m-md-2">
+              <b-dropdown-item>EUR</b-dropdown-item>
+              <b-dropdown-item>USD</b-dropdown-item>
+              <b-dropdown-item>NOK</b-dropdown-item>
+            </b-dropdown>
           </b-col>
         </b-row>
       </div>
