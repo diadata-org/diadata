6d075a172f8d1591502d745ea34f52e4c93961e8
mulili <mulili.nzuki@gmail.com>
2018-10-18 16:10:06 +0300

    changes requested

--- frontend/coinhub.diadata.org/src/components/Header/Header.vue
+++ frontend/coinhub.diadata.org/src/components/Header/Header.vue
@@ -3,12 +3,12 @@
   <header>
     <b-navbar type="dark" variant="info">
       <b-row>
-        <b-col>
-          <b-link href="/">
-            <b-img :src="logo" fluid alt="logo" width="150" />
+        <b-col cols="6">
+          <b-link href="/" class="logo">
+            <b-img :src="logo"  fluid alt="logo" width="150" />
           </b-link>
         </b-col>
-        <b-col>
+        <b-col cols="6">
           <div class="w-100 text-right">
             <b-navbar-brand href="/">CoinHub by DIA</b-navbar-brand>
             <br>
