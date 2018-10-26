3ce17c645100f7164e8b6d71f4b1546bae2dae4e
Samuel Brack <samuel.brack@blockstate.com>
2018-10-26 12:27:00 +0200

    Coinhub: src code formatting

--- frontend/coinhub.diadata.org/src/components/CoinDataTable/CoinDataTable.js
+++ frontend/coinhub.diadata.org/src/components/CoinDataTable/CoinDataTable.js
@@ -6,89 +6,83 @@ import shared from  '@/shared/shared';
 let vm = null;
 let allCoinData = [];
 export default {
-  components: {
-      AtomSpinner,
-      ModelListSelect
-  },
-  name: 'CoinDataTable',
-  props: {},
-  data() {
-    return {
-      fields: [{ key: 'rank', label: '#', sortable: true },
-        { key: 'coinName', label: 'Name', sortable: true },
-        { key: 'coinPrice', label: 'Price', sortable: true },
-        { key: 'change24', label: 'Change (24h)', sortable: true },
-        { key: 'priceGraph', label: 'Price Graph (7d)', sortable: false },
-        { key: 'volume24', label: 'Volume (24h)', sortable: true },
-        { key: 'marketCap', label: 'Market Cap', sortable: true },
-        { key: 'circulatingSupply', label: 'Circulating Supply', sortable: true },
-        { key: 'oracle', label: 'Oracle', sortable: false },
-      ],
-      coindata: [],
-      loading: true,
-      errored: false,
-      selectedCurrency: '',
-      selectedCoin:undefined,
-      options: [],
-      currencies: [],
-      searcharray: [],
-    };
-  },
-  created: function () {
-    vm = this
-  },
-  async mounted() {
-    try {
-      const { currencyArray, coinsArray, searchArray } = shared.formatCoinData(await shared.fetchCoins());
-      allCoinData = coinsArray;
-      vm.currencies = currencyArray;
-      vm.searcharray = searchArray;
+	components: {
+		AtomSpinner,
+		ModelListSelect
+	},
+	name: 'CoinDataTable',
+	props: {},
+	data() {
+		return {
+			fields: [{ key: 'rank', label: '#', sortable: true },
+				{ key: 'coinName', label: 'Name', sortable: true },
+				{ key: 'coinPrice', label: 'Price', sortable: true },
+				{ key: 'change24', label: 'Change (24h)', sortable: true },
+				{ key: 'priceGraph', label: 'Price Graph (7d)', sortable: false },
+				{ key: 'volume24', label: 'Volume (24h)', sortable: true },
+				{ key: 'marketCap', label: 'Market Cap', sortable: true },
+				{ key: 'circulatingSupply', label: 'Circulating Supply', sortable: true },
+				{ key: 'oracle', label: 'Oracle', sortable: false },
+			],
+			coindata: [],
+			loading: true,
+			errored: false,
+			selectedCurrency: '',
+			selectedCoin: undefined,
+			options: [],
+			currencies: [],
+			searcharray: [],
+		};
+	},
+	created: function () {
+		vm = this
+	},
+	async mounted() {
+		try {
+			const { currencyArray, coinsArray, searchArray } = shared.formatCoinData(await shared.fetchCoins());
+				allCoinData = coinsArray;
+				vm.currencies = currencyArray;
+				vm.searcharray = searchArray;
 
-      if(vm.currencies.length > 0) {
-        if(localStorage.selectedCurrency) {
-           vm.selectedCurrency = localStorage.selectedCurrency;
-        }
-        else {
-          if(vm.currencies.includes("EUR")){
-            vm.selectedCurrency = "EUR";
-          }
-          else{
-            vm.selectedCurrency = "USD";
-          }
-        }
-        
-      }
-      vm.loading = false;
-      // load the currencies
-      vm.switchCurrencies(vm.selectedCurrency);
-    }
-    catch (error) {
-      console.log(error);
-      vm.errored = true;
-      vm.loading = false;
-    }
+				if(vm.currencies.length > 0) {
+					if(localStorage.selectedCurrency) {
+						vm.selectedCurrency = localStorage.selectedCurrency;
+					} else {
+						if(vm.currencies.includes("EUR")){
+						vm.selectedCurrency = "EUR";
+						} else {
+							vm.selectedCurrency = "USD";
+						}
+					}
+				}
+				vm.loading = false;
+				// load the currencies
+				vm.switchCurrencies(vm.selectedCurrency);
+		} catch (error) {
+			console.log(error);
+			vm.errored = true;
+			vm.loading = false;
+		}
+	},
+	methods: {
+		switchCurrencies : function(selectedCurrency){
+			allCoinData.forEach(function(coin,index){
+				if(coin[selectedCurrency]) {
+					vm.selectedCurrency = selectedCurrency;
+					localStorage.selectedCurrency = selectedCurrency;
+					vm.coindata = coin[selectedCurrency];
+				}
+			});
+		},
+		initSearch : () => {
+			if(vm.options.length <=0){
+				vm.options = vm.searcharray;
+			}
 
-  },
-  methods: {
-      switchCurrencies : function(selectedCurrency){
-        allCoinData.forEach(function(coin,index){
-            if(coin[selectedCurrency]) {
-              vm.selectedCurrency = selectedCurrency;
-              localStorage.selectedCurrency = selectedCurrency;
-              vm.coindata = coin[selectedCurrency];
-            }
-        });
-      },
-      initSearch : () => {
-        if(vm.options.length <=0){
-          vm.options = vm.searcharray;
-        }
-
-        if(vm.selectedCoin){
-          vm.$router.push({ name: 'coin-details', params: { coinSymbol: vm.selectedCoin}});
-          vm.selectedCoin = undefined;
-        }
-      }
-  },
-
+			if(vm.selectedCoin){
+				vm.$router.push({ name: 'coin-details', params: { coinSymbol: vm.selectedCoin}});
+				vm.selectedCoin = undefined;
+			}
+		}
+	},
 };
