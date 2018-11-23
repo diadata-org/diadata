264e2f9c9b36025ed82dc5e2ddadb6342186083f
mestorlx <github@martindiz.com.ar>
2018-11-22 18:13:34 -0600

    EstimatorService added

---
+++ internal/pkg/estimators/EstimatorsService.go
@@ -0,0 +1,119 @@
+package estimators
+
+import (
+	"errors"
+	"github.com/diadata-org/diadata/pkg/dia"
+	log "github.com/sirupsen/logrus"
+	"sync"
+	"time"
+)
+
+type nothing struct{}
+
+//EstimatorsService Process trades and estimates PDF's
+type EstimatorsService struct {
+	shutdown         chan nothing
+	shutdownDone     chan nothing
+	chanTradesBlock  chan *dia.TradesBlock
+	chanPDFEstimates chan PDF
+	errorLock        sync.RWMutex
+	error            error
+	closed           bool
+	started          bool
+	currentTime      time.Time
+	estimators       map[string][]PDFEstimator
+	lastLog          time.Time
+}
+
+//NewEstimatorsService Open channels and start main lopp
+func NewEstimatorsService(chanPDFEstimates chan PDF) *EstimatorsService {
+	s := &EstimatorsService{
+		shutdown:         make(chan nothing),
+		shutdownDone:     make(chan nothing),
+		chanTradesBlock:  make(chan *dia.TradesBlock),
+		chanPDFEstimates: chanPDFEstimates,
+		error:            nil,
+		started:          false,
+		estimators:       make(map[string][]PDFEstimator),
+		lastLog:          time.Now(),
+	}
+	go s.mainLoop()
+	return s
+}
+
+//ProcessTradesBlock publish trade to internal channel
+func (s *EstimatorsService) ProcessTradesBlock(tradesBlock *dia.TradesBlock) {
+	s.chanTradesBlock <- tradesBlock
+	log.Info("completed ProcessTradesBlock")
+}
+
+//Close channel and terminate main loop
+func (s *EstimatorsService) Close() error {
+	if s.closed {
+		return errors.New("Estimators: Already closed")
+	}
+	close(s.shutdown)
+	<-s.shutdownDone
+	return s.error
+}
+
+// must only be called from mainLoop
+func (s *EstimatorsService) cleanup(err error) {
+	s.errorLock.Lock()
+	defer s.errorLock.Unlock()
+	if err != nil {
+		s.error = err
+	}
+	s.closed = true
+	close(s.shutdownDone) // signal that shutdown is complete
+}
+
+//CreateEstimator creates only selected estimators since some are really CPU intense process
+func (s *EstimatorsService) CreateEstimator(estimator string, symbol string, exchange string) {
+	s.estimators[symbol+exchange] = append(s.estimators[symbol+exchange], NewPDFEstimator(estimator))
+}
+
+//computeEstimators will run the estimators which can be cpu intensive
+func (s *EstimatorsService) computeEstimators(key string) {
+	for _, e := range s.estimators[key] {
+		e.Compute()
+	}
+}
+
+func (s *EstimatorsService) processTradesBlock(tb *dia.TradesBlock) {
+	log.Infoln("processTradesBlock starting")
+	for _, trade := range tb.TradesBlockData.Trades {
+		if ea, ok := s.estimators[trade.Symbol+trade.Source]; ok == true {
+			for _, e := range ea {
+				e.AddSamples([]float64{trade.EstimatedUSDPrice})
+			}
+		}
+		if ea, ok := s.estimators[trade.Symbol]; ok == true {
+			for _, e := range ea {
+				e.AddSamples([]float64{trade.EstimatedUSDPrice})
+			}
+		}
+	}
+	for _, v := range s.estimators {
+		for _, e := range v {
+			if err := e.Compute(); err == nil {
+				s.chanPDFEstimates <- e.GetPDF()
+			}
+		}
+	}
+}
+
+// runs in a goroutine until s is closed
+func (s *EstimatorsService) mainLoop() {
+	for {
+		log.Info("Estimators Service mainloop")
+		select {
+		case <-s.shutdown:
+			log.Println("Estimators Service shutting down")
+			s.cleanup(nil)
+			return
+		case tb, _ := <-s.chanTradesBlock:
+			s.processTradesBlock(tb)
+		}
+	}
+}
---
+++ internal/pkg/estimators/EstimatorsService_test.go
@@ -0,0 +1,96 @@
+package estimators
+
+import (
+	"github.com/diadata-org/diadata/pkg/dia"
+	"golang.org/x/exp/rand"
+	"gonum.org/v1/gonum/floats"
+	"gonum.org/v1/gonum/stat/distuv"
+	"testing"
+	"time"
+)
+
+func TestShutdown(t *testing.T) {
+	e := NewEstimatorsService(nil)
+	time.Sleep(time.Second)
+	e.Close()
+}
+
+type distribution interface {
+	Rand() float64
+}
+
+func TestProcessTradesBlock(t *testing.T) {
+	cases := []struct {
+		nSamples       int
+		wantParameters []float64
+		estimator      string
+	}{
+		{
+			nSamples:       10000,
+			wantParameters: []float64{1, 0, 1},
+			estimator:      "Student",
+		},
+		{
+			nSamples:       100000,
+			wantParameters: []float64{10, 1},
+			estimator:      "Laplace",
+		},
+	}
+	for i, test := range cases {
+		c := make(chan PDF)
+		exchange := "none"
+		symbol := "test"
+		src := rand.New(rand.NewSource(1))
+		trades := dia.TradesBlock{}
+		trades.TradesBlockData.Trades = make([]dia.Trade, test.nSamples)
+		var d distribution
+		switch test.estimator {
+		case "Laplace":
+			d = distuv.Laplace{
+				Mu:    test.wantParameters[0],
+				Scale: test.wantParameters[1],
+				Src:   src,
+			}
+		case "Student":
+			d = distuv.StudentsT{
+				Nu:    test.wantParameters[0],
+				Mu:    test.wantParameters[1],
+				Sigma: test.wantParameters[2],
+				Src:   src,
+			}
+		}
+		for i := range trades.TradesBlockData.Trades {
+			trades.TradesBlockData.Trades[i] = dia.Trade{
+				Symbol:            symbol,
+				Source:            exchange,
+				EstimatedUSDPrice: d.Rand(),
+			}
+		}
+		s := NewEstimatorsService(c)
+		s.CreateEstimator(test.estimator, symbol, exchange)
+		s.ProcessTradesBlock(&trades)
+		pdf := <-c
+		gotParameter := make([]float64, len(test.wantParameters))
+		switch test.estimator {
+		case "Laplace":
+			gotParameter[0] = pdf.(*PDFLaplace).GetLocation()
+			gotParameter[1] = pdf.(*PDFLaplace).GetScale()
+		case "Student":
+			gotParameter[0] = pdf.(*PDFStudentT).GetNu()
+			gotParameter[1] = pdf.(*PDFStudentT).GetLocation()
+			gotParameter[2] = pdf.(*PDFStudentT).GetScale()
+		}
+
+		if !pdf.DidGetSolution() {
+			t.Errorf("Estimator did not get solution for test %d", i)
+		}
+		if !pdf.DidConverge() {
+			t.Errorf("Estimator did not converged for test %d", i)
+		}
+		for j := range test.wantParameters {
+			if !floats.EqualWithinAbsOrRel(gotParameter[j], test.wantParameters[j], 1e-1, 1e-1) {
+				t.Errorf("unexpected parameter:%d result for test :%d got:%f, want:%f", j, i, gotParameter[j], test.wantParameters[j])
+			}
+		}
+	}
+}
