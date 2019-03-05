ea5c889c22383a44848fcb9c14287bd24f47f180
Santosh Dhirwani <37183172+SantoshDhirwani@users.noreply.github.com>
2019-03-05 13:43:12 +0100

    main.go for maker

---
+++ internal/pkg/blockchain-scrapers/blockchains/ethereum/scrapers/maker/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/diadata-org/diadata/pkg/dia"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+	"math"
+	"math/big"
+	"time"
+)
+
+//Be sure to run : abigen --abi erc20.abi --pkg main --type Token --out erc20.go
+//erc20.abi taken from https://github.com/ethereum/wiki/wiki/Contract-ERC20-ABI
+var (
+	rpcEndpoint  = flag.String("rpc", "https://mainnet.infura.io/v3/7db804679c8644fcbfbfd440ed2332eb", "geth RPC endpoint")
+	dev          = flag.Bool("dev", false, "Dev mode - prints to stdout instead of sending to dia")
+	symbol       = "MKR"
+	address      = "0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359"
+	ownerAddress = "0x0000000000000000000000000000000000000000"
+)
+
+func init() {
+	flag.Parse()
+}
+
+func toFloat(in *big.Int, decimals uint8) float64 {
+	f := new(big.Float).SetInt(in)
+	fl, _ := f.Mul(f, big.NewFloat(math.Pow(float64(10), -1*float64(decimals)))).Float64()
+	return fl
+}
+
+func main() {
+
+	conn, err := ethclient.Dial(*rpcEndpoint)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+	// Instantiate the contract
+	token, err := NewMKR(common.HexToAddress(address), conn)
+	if err != nil {
+		log.Fatalf("Failed to instantiate a Token contract: %v", err)
+	}
+	//Check for unexpected errors while loading config file or a new client
+	config := dia.GetConfigApi()
+	if config == nil {
+		panic("Couldnt load config")
+	}
+	client := dia.NewClient(config)
+	if client == nil {
+		panic("Couldnt load client")
+	}
+	for {
+		//Infinite loop, sends tokenSupply every 10 seconds
+		supply, err := token.TotalSupply(nil)
+		if err != nil {
+			//Perhaps these should not be a fatal error?
+			log.Fatalf("Failed to retrieve token supply: %v", err)
+		}
+
+		ownerSupply, err := token.BalanceOf(nil, common.HexToAddress(ownerAddress))
+		if err != nil {
+			log.Fatalf("Failed to retrieve token ownerSupply: %v", err)
+		}
+
+		//frozenSupply, err := token.FreezeOf(nil, common.HexToAddress(ownerAddress))
+		//if err != nil {
+			//log.Fatalf("Failed to retrieve token frozenSupply: %v", err)
+		//}
+
+		tmpSupply := supply.Sub(supply, ownerSupply)
+		resultSupply := tmpSupply//supply.Sub(tmpSupply, frozenSupply)
+
+		decimals, err := token.Decimals(nil)
+		if err != nil {
+			log.Fatalf("Failed to retrieve token decimal: %v", err)
+		}
+
+		result := toFloat(resultSupply, decimals)
+
+		fmt.Printf("Symbol: %s ; totalSupply: %f\n", symbol, result)
+
+		client.SendSupply(&dia.Supply{
+			Symbol: symbol,
+			CirculatingSupply: result,
+		})
+
+		time.Sleep(time.Second * 15)
+	}
+}
