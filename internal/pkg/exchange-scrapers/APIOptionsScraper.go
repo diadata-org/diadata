9397d0dce5d9a1f7a82577189161676b6bae4b13
Nazariy <nazariyv@users.noreply.github.com>
2019-12-23 14:34:37 +0000

    VIX Algo (#195)
    * Coinflex scraper + refactors for Deribit, Huobi, FTX (#190) (#1)
    
    * [+] added huobi futures scraper
    
    * [*] 1. refactored futures huobi scraper && 2. ftx futures scraper
    
    * [+] futures ftx: not writing the pongs & subscription & unsubscription messages
    
    * [+] futures scraper: added deribit & refactors to all other futures scrapers
    
    * [*] refactors: ftx, deribit, huobi
    
    * [+] futures scarper: coinflex
    
    * [+] deribit scraper: 1. unique refresh token for each scraper; 2. better handling of failure to refresh the token; 3. better handling of errors; 4. better saving of trades; 5. only futures markets allowed in the futures scraper (excluded options);
    
    * [+] futures-scrapers: 1. better retry after error; 2. better file writing
    
    * [+] deribit: generalized code to accommodate for options
    
    * [+] deribit: options scraper introduced & options data
    
    * [+] gitignore: added .vscode, bin/ and pkg/mod/
    
    * [+] CVI algo implemented

--- internal/pkg/exchange-scrapers/APIOptionsScraper.go
+++ internal/pkg/exchange-scrapers/APIOptionsScraper.go
@@ -10,7 +10,16 @@ type OptionsScraper interface {
 	Authenticate(market string, websocketConnection interface{}) error
 }
 
-// OptionOrderbookDatum is a unit of option order book data. Other meta thing like expiration time and strike price can be queried from the meta files / db.
+// OptionType is used to signal whether the option is a call or a put
+type OptionType int
+
+// signals if the option is call or a put
+const (
+	CallOption OptionType = iota + 1
+	PutOption
+)
+
+// OptionOrderbookDatum is a unit of option order book data; Other meta items like expiration time and strike price can be queried from the meta files / db.
 type OptionOrderbookDatum struct {
 	InstrumentName  string
 	ObservationTime time.Time
@@ -19,3 +28,41 @@ type OptionOrderbookDatum struct {
 	AskSize         float64
 	BidSize         float64
 }
+
+// OptionMeta is option's meta information that can be directly extracted and formatted from the meta files (option scrapers)
+type OptionMeta struct {
+	ExpirationTime time.Time
+	StrikePrice    float64
+	OptionType     OptionType
+}
+
+// OptionMetaIndex is option's meta enriched with additional information OptionOrderbookDatum; This is the format used in the CVI calculation
+type OptionMetaIndex struct {
+	OptionMeta
+	OptionOrderbookDatum
+}
+
+// OptionMetaForward is the type to be used in the calculation of the foward index level
+type OptionMetaForward struct {
+	StrikePrice float64
+	CallPrice   float64
+	PutPrice    float64 // this, as well as the above is defined as the bid price at a given strike price
+}
+
+// ComputedCVI is a struct representing our CVI value at a point in time
+type ComputedCVI struct {
+	CVI             float64
+	CalculationTime time.Time
+}
+
+// ComputedCVIs is the channel type that will communicate the cvis
+type ComputedCVIs chan ComputedCVI
+
+// OptionSettlement - is an enum, signalling if the settlement is regular or weekly
+type OptionSettlement int
+
+// OptionSettlement enums
+const (
+	RegularOptionSettlement OptionSettlement = iota + 1
+	WeeklyOptionSettlement
+)
