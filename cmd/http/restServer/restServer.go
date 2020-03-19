a93f4f532ae8a166cd971b636d8379681dc50c01
Samuel Brack <samuel.brack@blockstate.com>
2020-03-19 14:49:41 +0100

    restServer: fix listen port to production

--- cmd/http/restServer/restServer.go
+++ cmd/http/restServer/restServer.go
@@ -205,5 +205,5 @@ func main() {
 	r.Use(static.Serve("/v1/chart", static.LocalFile("/charts", true)))
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	r.Run(":8081")
+	r.Run(":8080")
 }
