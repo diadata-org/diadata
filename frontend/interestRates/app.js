5ac1a10d0680b82ecf793f054d6d77bcdea009eb
jppade <jppade@googlemail.com>
2020-03-25 10:52:17 +0100

    Site works locally. Wait for API extension.

--- frontend/interestRates/app.js
+++ frontend/interestRates/app.js
@@ -2,13 +2,47 @@
 var chart;
 const maxPoints = 50,
 	timeUpdate = 1000;
+// prefill = [];
+
+// function getData(url, callback) {
+
+// 	// Instantiate request object
+// 	var request = new XMLHttpRequest()
+// 	request.open('GET', url, false)
+
+// 	// Load data from GET request
+// 	request.onload = function() {
+// 		var data = JSON.parse(this.response)
+// 		if(this.status == 200) {
+// 			if (typeof callback === "function") {
+// 				callback(data)
+// 			}
+// 		} else if(this.status == 404) {
+// 			console.log('Not found error')
+// 		}
+// 	}
+
+// 	request.onerror = function() {
+// 		console.log('Request error.')
+// 	}
+// 	request.send()
+// }
+
+// getData(getApi('quotation'), function(obj) {
+// 	prefill = obj.Value
+// });
+
+// console.log(prefill)
 
 function initialFill() {
+	// for(range of dates) {
+	// 		prefill.append(getData(date))
+	// }
 	// Get the last @maxPoints from the API in order to have a
-	// full series when the chart is loaded
+	// full series when the chart is loaded.
 }
 
-function getRate() {
+function getRateInfo() {
 	// Returns an object with information on the rate for labelling the plot
 	rate = {
 		name: 'Ethereum',
@@ -32,7 +66,6 @@ function requestData() {
 
 		// If GET request is successful, add data to the chart
         success: function(point) {
-
 			// @timeseries points to the first chart's series.
 			// @shift restricts the number of displayed points.
 			var timeseries = chart.series[0],
@@ -40,7 +73,7 @@ function requestData() {
 			
 			// convert time (string) to Unix time for plotting
 			var date = Date.parse(point.Time);
-
+			
 			// Append a data point to the chart's series if its timestamp is new
 			L = chart.series[0].xData.length;
 			if(L == 0) {
@@ -48,19 +81,30 @@ function requestData() {
 				console.log("Initial fill: " + point.Time)
 			} else if(L > 0 && date != chart.series[0].xData[L-1]){
 				chart.series[0].addPoint([date, point.Price], true, shift);
+				console.log("Length of array is: ", L)
 				console.log("Updated point at: " + point.Time)
 			}
 
-            // Check for new data after @timeUpdate milliseconds
+			// Check for new data after @timeUpdate milliseconds
 			setTimeout(requestData, timeUpdate); 
         },
 		cache: false,
 	});
 }
 
+// $.ajax({
+// 	url: 'https://api.diadata.org/v1/interestrate/SOFR/2020-03-23',
+// 	type: 'GET',
+// 	success: function(point) {
+// 		console.log([Date.parse(point.Time), point.Value])
+// 		chart.series[0].addPoint([Date.parse(point.Time), point.Value], true, 5)
+// 	}
+// });
 
 document.addEventListener('DOMContentLoaded', function() {
-    chart = Highcharts.chart('container', {
+	
+	// chart = Highcharts.chart('container', {
+    chart = Highcharts.stockChart('container', {
         chart: {
             type: 'spline',
             events: {
@@ -72,25 +116,25 @@ document.addEventListener('DOMContentLoaded', function() {
 			href: 'https://diadata.org'
 		},
         title: {
-            text: getRate().name,
+            text: getRateInfo().name,
         },
         xAxis: {
             type: 'datetime',
             tickPixelInterval: 150,
 			maxZoom: 20 * 1000,
-        },
+		},
         yAxis: {
             minPadding: 0.2,
             maxPadding: 0.2,
             title: {
                 text: 'Value',
                 margin: 80
-            }
+			}	
         },
         series: [
 			{
-				name: getRate().symbol,
-            	data: []
+				name: getRateInfo().symbol,
+				data: []
 			},
 		]
 	});	
