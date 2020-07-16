0e163a55751759d96138fa36de8ad213f4720d04
niktrix <nicky.gurbani@gmail.com>
2020-07-16 12:23:44 +0530

    Enable assets

--- internal/pkg/defiscrapers/updateCompound.go
+++ internal/pkg/defiscrapers/updateCompound.go
@@ -30,15 +30,15 @@ type CompoundProtocol struct {
 func NewCompound(scraper *DefiScraper, protocol dia.DefiProtocol) *CompoundProtocol {
 	assets := make(map[string]string)
 	//https://compound.finance/docs#networks
-	//assets["BAT"] = "0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e"
-	//assets["DAI"] = "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643"
-	//assets["ETH"] = "0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5"
+	assets["BAT"] = "0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e"
+	assets["DAI"] = "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643"
+	assets["ETH"] = "0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5"
 	assets["REP"] = "0x158079ee67fce2f58472a96584a73c7ab9ac95c1"
-	//assets["SAI"] = "0xf5dce57282a584d2746faf1593d3121fcac444dc"
-	//assets["USDC"] = "0x39aa39c021dfbae8fac545936693ac917d5e7563"
-	//assets["USDT"] = "0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9"
-	//assets["WBTC"] = "0xc11b1268c1a384e55c48c2391d8d480264a3a7f4"
-	//assets["ZRX"] = "0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407"
+	assets["SAI"] = "0xf5dce57282a584d2746faf1593d3121fcac444dc"
+	assets["USDC"] = "0x39aa39c021dfbae8fac545936693ac917d5e7563"
+	assets["USDT"] = "0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9"
+	assets["WBTC"] = "0xc11b1268c1a384e55c48c2391d8d480264a3a7f4"
+	assets["ZRX"] = "0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407"
 
 	connection, err := ethclient.Dial("https://mainnet.infura.io/v3/f619e28e13f0428cba6f9243b09d4af0")
 	if err != nil {
