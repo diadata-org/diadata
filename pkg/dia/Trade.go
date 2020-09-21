7544e972014c2d3d89a886d3c473a99c033f6cc7
jppade <jppade@googlemail.com>
2020-09-21 12:42:48 +0200

    prevent slice-out-of-bounds-panic in Trade.go

--- pkg/dia/Trade.go
+++ pkg/dia/Trade.go
@@ -4,55 +4,45 @@ import (
 	"strings"
 )
 
-func (t *Trade) SecondPair() string {
+// BaseToken returns the base token of a trading pair
+func (t *Trade) BaseToken() string {
 
 	pair := strings.ToUpper(t.Pair)
-
-	switch t.Source {
-	case KrakenExchange:
-		if pair[len(pair)-3:] == "XBT" {
-			return "BTC"
+	if len(pair) > 3 {
+		switch t.Source {
+		case KrakenExchange:
+			if pair[len(pair)-3:] == "XBT" {
+				return "BTC"
+			}
+		case BitfinexExchange:
+			if pair[len(pair)-3:] == "USD" {
+				return "USDT"
+			}
+		case HitBTCExchange:
+			if pair[len(pair)-3:] == "USD" {
+				return "USDT"
+			}
 		}
-	case BitfinexExchange:
-		if pair[len(pair)-3:] == "USD" {
-			return "USDT"
+
+		// If one of these assets is base token, be sure to get price from redis
+		topAssets := []string{"EUR", "USD", "BTC", "ETH", "USDC", "USDT", "BNB"}
+		for _, asset := range topAssets {
+			if pair[len(pair)-len(asset):] == asset {
+				return asset
+			}
 		}
-	case HitBTCExchange:
-		if pair[len(pair)-3:] == "USD" {
-			return "USDT"
-		}
-	}
 
-	if pair[len(pair)-4:] == "USDT" {
-		return "USDT"
-	}
-
-	if pair[len(pair)-3:] == "EUR" {
-		return "EUR"
-	}
-
-	if pair[len(pair)-3:] == "USD" {
-		return "USD"
-	}
-
-	if pair[len(pair)-3:] == "ETH" {
-		return "ETH"
-	}
-
-	if pair[len(pair)-3:] == "BTC" {
-		return "BTC"
-	}
-
-	if pair[len(pair)-3:] == "BNB" {
-		return "BNB"
 	}
 
 	second := strings.TrimPrefix(pair, t.Symbol+"_")
 	if second != pair {
 		return second
 	}
-
 	second = strings.TrimPrefix(pair, t.Symbol+"-")
+	if second != pair {
+		return second
+	}
+	second = strings.TrimPrefix(pair, t.Symbol+"/")
 	if second != pair {
 		return second
 	}
@@ -61,44 +51,4 @@ func (t *Trade) SecondPair() string {
 		return pair[len(pair)-3:]
 	}
 	return strings.TrimPrefix(pair, t.Symbol)
-}
-
-// TODO: Check if we can delete this function
-func (t *Trade) NormalizedSymbol() string {
-	symbol := strings.ToUpper(t.Symbol)
-
-	switch t.Source {
-	case KrakenExchange:
-		if (len(symbol) < 4) {
-			return t.Symbol
-		}
-		if (symbol[:3] == "XBT" || symbol[:4] == "XXBT") {
-			return "BTC"
-		}
-		if (symbol[:4] == "XREP") {
-			return "REP"
-		}
-		if (symbol[:4] == "XETH") {
-			return "ETH"
-		}
-		if (symbol[:4] == "XETC") {
-			return "ETC"
-		}
-		if (symbol[:4] == "XLTC") {
-			return "LTC"
-		}
-		if (symbol[:4] == "XXMR") {
-			return "XMR"
-		}
-		if (symbol[:4] == "XXRP") {
-			return "XRP"
-		}
-		if (symbol[:4] == "XXLM") {
-			return "XLM"
-		}
-		if (symbol[:4] == "XZEC") {
-			return "ZEC"
-		}
-	}
-	return t.Symbol
 }
--- pkg/dia/Trade_test.go
+++ pkg/dia/Trade_test.go
@@ -7,39 +7,39 @@ import (
 func TestTrade(t *testing.T) {
 
 	trade := &Trade{Pair: "BTCUSD", Source: KrakenExchange}
-	r := trade.SecondPair()
+	r := trade.BaseToken()
 	if r != "USD" {
-		t.Errorf("error secondpair %v", r)
+		t.Errorf("error base token %v", r)
 	}
 
 	trade2 := &Trade{Symbol: "BTC", Pair: "BTC_USDT", Source: KrakenExchange}
-	r = trade2.SecondPair()
+	r = trade2.BaseToken()
 	if r != "USDT" {
-		t.Errorf("error secondpair %v", r)
+		t.Errorf("error base token %v", r)
 	}
 
 	trade3 := &Trade{Symbol: "BTC", Pair: "BTCUSDT", Source: KrakenExchange}
-	r = trade3.SecondPair()
+	r = trade3.BaseToken()
 	if r != "USDT" {
-		t.Errorf("error secondpair %v", r)
+		t.Errorf("error base token %v", r)
 	}
 
 	trade4 := &Trade{Symbol: "ZB", Pair: "zbusdt", Source: KrakenExchange}
-	r = trade4.SecondPair()
+	r = trade4.BaseToken()
 	if r != "USDT" {
-		t.Errorf("error secondpair %v", r)
+		t.Errorf("error base token %v", r)
 	}
 }
 
 func TestBitfinexAndHitBTCSecondPairAreUSDT(t *testing.T) {
 	trade := &Trade{Pair: "BTCUSD", Source: BitfinexExchange}
-	r := trade.SecondPair()
+	r := trade.BaseToken()
 	if r != "USDT" {
-		t.Errorf("error secondpair %v", r)
+		t.Errorf("error base token %v", r)
 	}
 	trade2 := &Trade{Symbol: "BTC", Pair: "BTCUSD", Source: HitBTCExchange}
-	r = trade2.SecondPair()
+	r = trade2.BaseToken()
 	if r != "USDT" {
-		t.Errorf("error secondpair %v", r)
+		t.Errorf("error base token %v", r)
 	}
 }
