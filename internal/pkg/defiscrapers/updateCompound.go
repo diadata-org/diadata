3b3b01042368780b8313a8acaabb761992b8eda1
niktrix <nicky.gurbani@gmail.com>
2020-10-06 19:25:42 +0530

    Scraper for cream finance

--- internal/pkg/defiscrapers/updateCompound.go
+++ internal/pkg/defiscrapers/updateCompound.go
@@ -70,6 +70,61 @@ func NewCompound(scraper *DefiScraper, protocol dia.DefiProtocol) *CompoundProto
 	return &CompoundProtocol{scraper: scraper, protocol: protocol, assets: assets, decimals: decimals, connection: connection}
 }
 
+func NewCreamFinance(scraper *DefiScraper, protocol dia.DefiProtocol) *CompoundProtocol {
+	assets := make(map[string]string)
+	//https://compound.finance/docs#networks
+	assets["ETH"] = "0xD06527D5e56A3495252A528C4987003b712860eE"   //done
+	assets["USDC"] = "0x44fbeBd2F576670a6C33f6Fc0B00aA8c5753b322"  //done
+	assets["USDT"] = "0x797AAB1ce7c01eB727ab980762bA88e7133d2157"  //done
+	assets["MTA"] = "0x3623387773010d9214B10C551d6e7fc375D31F58"   //done
+	assets["COMP"] = "0x19D1666f543D42ef17F66E376944A22aEa1a8E46"  //done
+	assets["BAL"] = "0xcE4Fe9b4b8Ff61949DCfeB7e03bc9FAca59D2Eb3"   //done
+	assets["YFI"] = "0xCbaE0A83f4f9926997c8339545fb8eE32eDc6b76"   //done
+	assets["yCRV"] = "0x9baF8a5236d44AC410c0186Fe39178d5AAD0Bb87"  //done
+	assets["LINK"] = "0x697256CAA3cCaFD62BB6d3Aa1C7C5671786A5fD9"  //done
+	assets["CREAM"] = "0x892B14321a4FCba80669aE30Bd0cd99a7ECF6aC0" //done
+	assets["LEND"] = "0x8B86e0598616a8d4F1fdAE8b59E55FB5Bc33D0d6"  //done
+	assets["CRV"] = "0xc7Fd8Dcee4697ceef5a2fd4608a7BD6A94C77480"   //done
+	assets["BUSD"] = "0x1FF8CDB51219a8838b52E9cAc09b71e591BC998e"  //done
+	assets["yUSD"] = "0x4EE15f44c6F0d8d1136c83EfD2e8E4AC768954c6"  //done
+	assets["SUSHI"] = "0x338286C0BC081891A4Bda39C7667ae150bf5D206" //done
+	assets["FTT"] = "0x10FDBD1e48eE2fD9336a482D746138AE19e649Db"   //done
+	assets["yETH"] = "0x01da76DEa59703578040012357b81ffE62015C2d"  //done
+	assets["SRM"] = "0xef58b2d5A1b8D3cDE67b8aB054dC5C831E9Bc025"   //done
+	assets["UNI"] = "0xe89a6D0509faF730BD707bf868d9A2A744a363C7"   //done
+
+	assets["renBTC"] = "0x17107f40d70f4470d20CB3f138a052cAE8EbD4bE" //done
+
+	decimals := make(map[string]int)
+	decimals["ETH"] = 18
+	decimals["USDT"] = 6
+	decimals["USDC"] = 6
+	decimals["COMP"] = 18
+	decimals["BAL"] = 18
+	decimals["YFI"] = 18
+	decimals["yCRV"] = 18
+	decimals["LINK"] = 18
+	decimals["CREAM"] = 18
+	decimals["LEND"] = 18
+	decimals["CRV"] = 18
+	decimals["renBTC"] = 8
+	decimals["BUSD"] = 18
+	decimals["MTA"] = 18
+	decimals["yUSD"] = 18
+	decimals["SUSHI"] = 18
+	decimals["FTT"] = 18
+	decimals["yETH"] = 18
+	decimals["SRM"] = 6
+	decimals["UNI"] = 18
+
+	connection, err := ethhelper.NewETHClient()
+	if err != nil {
+		log.Error("Error connecting Eth Client")
+	}
+
+	return &CompoundProtocol{scraper: scraper, protocol: protocol, assets: assets, decimals: decimals, connection: connection}
+}
+
 func (proto *CompoundProtocol) fetch(asset string) (rate CompoundRate, err error) {
 	var contract *compoundcontract.CTokenCaller
 	contract, err = compoundcontract.NewCTokenCaller(common.HexToAddress(proto.assets[asset]), proto.connection)
