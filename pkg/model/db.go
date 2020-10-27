6dbb9657e670c750cbf34c10aaa60650e92235d3
nicky g <nicky.gurbani@gmail.com>
2020-10-28 01:47:03 +0530

    add poolid and blocknumber (#330)
    * add poolid and blocknumber
    
    * update influxdb save for farming pools
    
    * remove unused UpdateRate method
    
    * remove Updaterate
    
    * Add 1 minute timer for yfi scrapping

--- pkg/model/db.go
+++ pkg/model/db.go
@@ -554,11 +554,22 @@ func (db *DB) SetFarmingPool(pool *FarmingPool) error {
 	fields := map[string]interface{}{
 		"rate":    pool.Rate,
 		"balance": pool.Balance,
+		"blockNumber": pool.BlockNumber,
+
+	}
+	inputAssetBytes,err := json.Marshal(pool.InputAsset)
+	if err != nil{
+		return err
+	}
+	outputAssetBytes,err := json.Marshal(pool.OutputAsset)
+	if err != nil{
+		return err
 	}
 	tags := map[string]string{
-		"inputAsset":  pool.InputAsset[0],
-		"outputAsset": pool.OutputAsset,
+		"inputAssets":  string(inputAssetBytes),
+		"outputAssets": string(outputAssetBytes),
 		"protocol":    pool.ProtocolName,
+		"poolID":      pool.PoolID,
 	}
 	pt, err := clientInfluxdb.NewPoint(influxDbPoolTable, tags, fields, pool.TimeStamp)
 	if err != nil {
