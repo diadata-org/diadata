adeb4253529d372a7cf0b1ac105ff55bb77d2b73
nnn-gif <nicky.gurbani@gmail.com>
2021-05-26 11:53:24 +0530

    Set heartbeat

--- internal/pkg/option-scrapers/optionDeribitScraper.go
+++ internal/pkg/option-scrapers/optionDeribitScraper.go
@@ -11,6 +11,10 @@ import (
 	"time"
 )
 
+const clientID = "2fSbOqqC"
+const clientSecret = "78gpYOitaZkn3y39XboRAWgDrtocdtHVWOz1_IlaN74"
+
+
 type DeribitInstrumentsResponse struct {
 	Jsonrpc string              `json:"jsonrpc"`
 	Result  []DeribitInstrument `json:"result"`
@@ -51,13 +55,14 @@ type DeribitETHOptionScraper struct {
 	DataStore          *models.DB
 	chanOrderBook      chan *dia.OptionOrderbookDatum
 	Ratelimiter        *rate.Limiter
+	refreshToken   string
 }
 
 type DeribitRequest struct {
 	Jsonrpc string       `json:"jsonrpc"`
 	ID      int          `json:"id"`
 	Method  string       `json:"method"`
-	Params  DeribitParam `json:"params"`
+	Params  interface{} `json:"params"`
 }
 
 type DeribitParam struct {
@@ -89,6 +94,8 @@ func NewDeribitETHOptionScraper() *DeribitETHOptionScraper {
 	return s
 
 }
+
+
 
 func (scraper *DeribitETHOptionScraper) subscribe() {
 
@@ -129,91 +136,143 @@ type DeribitOptionResponse struct {
 	} `json:"params"`
 }
 
+func (scraper *DeribitETHOptionScraper) handleWSMessage() {
+	var response DeribitOptionResponse
+	var err error
+
+ 	err = scraper.wsClient.ReadJSON(&response)
+	if err != nil {
+		log.Errorln("Error reading wsclient", err)
+		return
+
+	}
+
+	var (
+		resolvedAskPX, resolvedAskSize, resolvedBidSize, resolvedBidPX float64
+	)
+
+	everything := "new"
+
+	b, _ := json.Marshal(response)
+	log.Infoln("Message Received", string(b[:]))
+	for _, askOut := range response.Params.Data.Asks {
+		log.Infoln("askOut", askOut)
+		switch everything {
+		case "delete":
+			log.Infoln("Delete")
+		case "new":
+			log.Infoln("new")
+			resolvedAskSize = askOut[1].(float64)
+			resolvedAskPX = askOut[0].(float64)
+		default:
+			log.Infoln(" askOut[0].(string)", askOut[0].(string))
+
+		}
+
+	}
+
+	for _, bidOut := range response.Params.Data.Bids {
+		log.Infoln("bidOut", bidOut)
+		switch everything {
+		case "delete":
+			log.Infoln("Delete")
+		case "new":
+			log.Infoln("new")
+			resolvedBidSize = bidOut[1].(float64)
+			resolvedBidPX = bidOut[0].(float64)
+		default:
+			log.Infoln(" bidOut[0].(string)", bidOut[0].(string))
+
+		}
+
+	}
+
+	var o = dia.OptionOrderbookDatum{
+		InstrumentName:  response.Params.Data.InstrumentName,
+		ObservationTime: time.Unix(response.Params.Data.Timestamp/1e3, 0),
+		AskSize:         resolvedAskSize,
+		AskPrice:        resolvedAskPX,
+		BidPrice:        resolvedBidPX,
+		BidSize:         resolvedBidSize,
+	}
+	log.Infoln("Got trade", o)
+
+	scraper.chanOrderBook <- &o
+
+	//optionMeta := dia.OptionMeta{
+	//	InstrumentName: response.Params.Data.InstrumentName,
+	//	BaseCurrency:   instrument.SettlementCurrency,
+	//	ExpirationTime: expTime,
+	//	StrikePrice:    strikePrice,
+	//	OptionType:     optionType,
+	//}
+	//
+	//scrapper.DataStore.SetOptionMeta(&optionMeta)
+
+}
 func (scraper *DeribitETHOptionScraper) Scrape() {
+
+
+ 	go scraper.heartBeat()
 	scraper.subscribe()
 	go func() {
 		for {
-			var response DeribitOptionResponse
-			var err error
-
-			err = scraper.wsClient.ReadJSON(&response)
-			if err != nil {
-				log.Errorln("Error reading wsclient", err)
-				scraper.wsClient.Close()
-
-				scraper.Scrape()
-				return
-
-			}
-
-			var (
-				resolvedAskPX, resolvedAskSize, resolvedBidSize, resolvedBidPX float64
-			)
-
-			everything := "new"
-
-			b, _ := json.Marshal(response)
-			log.Infoln("Message Received", string(b[:]))
-			for _, askOut := range response.Params.Data.Asks {
-				log.Infoln("askOut", askOut)
-				switch everything {
-				case "delete":
-					log.Infoln("Delete")
-				case "new":
-					log.Infoln("new")
-					resolvedAskSize = askOut[1].(float64)
-					resolvedAskPX = askOut[0].(float64)
-				default:
-					log.Infoln(" askOut[0].(string)", askOut[0].(string))
-
-				}
-
-			}
-
-			for _, bidOut := range response.Params.Data.Bids {
-				log.Infoln("bidOut", bidOut)
-				switch everything {
-				case "delete":
-					log.Infoln("Delete")
-				case "new":
-					log.Infoln("new")
-					resolvedBidSize = bidOut[1].(float64)
-					resolvedBidPX = bidOut[0].(float64)
-				default:
-					log.Infoln(" bidOut[0].(string)", bidOut[0].(string))
-
-				}
-
-			}
-
-
-
-			var o = dia.OptionOrderbookDatum{
-				InstrumentName:  response.Params.Data.InstrumentName,
-				ObservationTime: time.Unix(response.Params.Data.Timestamp/1e3, 0),
-				AskSize:         resolvedAskSize,
-				AskPrice:        resolvedAskPX,
-				BidPrice:        resolvedBidPX,
-				BidSize:         resolvedBidSize,
-			}
-			log.Infoln("Got trade", o)
-
-			scraper.chanOrderBook <- &o
-
-			//optionMeta := dia.OptionMeta{
-			//	InstrumentName: response.Params.Data.InstrumentName,
-			//	BaseCurrency:   instrument.SettlementCurrency,
-			//	ExpirationTime: expTime,
-			//	StrikePrice:    strikePrice,
-			//	OptionType:     optionType,
-			//}
-			//
-			//scrapper.DataStore.SetOptionMeta(&optionMeta)
+			scraper.handleWSMessage()
 
 		}
+
 	}()
 
 }
+
+func (scraper *DeribitETHOptionScraper) heartBeat() {
+	for {
+		var params map[string]int
+
+		id := 5600000
+		request := &DeribitRequest{
+			Jsonrpc: "2.0",
+			ID:      id,
+			Method:  "/public/set_heartbeat",
+		}
+		params = make(map[string]int)
+		params["interval"] = 30
+
+		request.Params = params
+		log.Info("set_heartbeat ", request)
+		scraper.wsClient.WriteJSON(request)
+		id++
+		time.Sleep(1 * time.Minute)
+	}
+}
+
+func (s *DeribitETHOptionScraper) send(message *map[string]interface{}, websocketConn *ws.Conn) error {
+	err := websocketConn.WriteJSON(*message)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *DeribitETHOptionScraper) handleRefreshToken(previousToken string, websocketConn *ws.Conn) (bool, error) {
+	if previousToken == "" {
+		return false, nil
+	}
+	// refresh
+	err := s.send(&map[string]interface{}{
+		"method": "public/auth",
+		"params": &map[string]string{
+			"grant_type":    "refresh_token",
+			"refresh_token": previousToken,
+		},
+		"jsonrpc": "2.0",
+	}, websocketConn)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
 
 type DeribitOptionsResponse struct {
 	Jsonrpc string              `json:"jsonrpc"`
