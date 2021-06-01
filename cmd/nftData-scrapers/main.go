ad72dd7b3132c0e535e7b98b97d44cc63c1d72a9
balda <marcotorello@gmail.com>
2021-06-01 23:38:42 +0200

    add cryptopunk scraper

--- cmd/nftData-scrapers/main.go
+++ cmd/nftData-scrapers/main.go
@@ -31,6 +31,9 @@ func main() {
 	case "Sorare":
 		log.Println("NFT Data Scraper: Start scraping data from Sorare")
 		scraper = nftdatascrapers.NewSorareScraper(rdb)
+	case "Cryptopunk":
+		log.Println("NFT Data Scraper: Start scraping data from Cryptopunk")
+		scraper = nftdatascrapers.NewCryptopunkScraper(rdb)
 	default:
 		for {
 			time.Sleep(24 * time.Hour)
