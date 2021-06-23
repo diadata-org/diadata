2e972521f5ec4275fb3a3d15ddfa15d33392b146
jppade <jppade@googlemail.com>
2021-06-23 15:37:58 +0200

    amended cryptopunk trades scraper

--- internal/pkg/nftTrade-scrapers/cryptopunks.go
+++ internal/pkg/nftTrade-scrapers/cryptopunks.go
@@ -12,13 +12,13 @@ import (
 
 	"github.com/diadata-org/diadata/config/nftContracts/cryptopunk"
 	"github.com/diadata-org/diadata/pkg/dia"
+	"github.com/diadata-org/diadata/pkg/dia/helpers/ethhelper"
 
 	// "github.com/diadata-org/diadata/pkg/dia/helpers/ethhelper"
 	models "github.com/diadata-org/diadata/pkg/model"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 const (
@@ -33,14 +33,14 @@ type CryptoPunkScraper struct {
 }
 
 func NewCryptoPunkScraper(rdb *models.RelDB) *CryptoPunkScraper {
-	// connection, err := ethhelper.NewETHClient()
-	// if err != nil {
-	// 	log.Error("Error connecting Eth Client")
-	// }
-	connection, err := ethclient.Dial("node url")
+	connection, err := ethhelper.NewETHClient()
 	if err != nil {
 		log.Error("Error connecting Eth Client")
 	}
+	// connection, err := ethclient.Dial("node url")
+	// if err != nil {
+	// 	log.Error("Error connecting Eth Client")
+	// }
 
 	tradeScraper := TradeScraper{
 		shutdown:      make(chan nothing),
@@ -62,10 +62,13 @@ func NewCryptoPunkScraper(rdb *models.RelDB) *CryptoPunkScraper {
 
 // mainLoop runs in a goroutine until channel s is closed.
 func (scraper *CryptoPunkScraper) mainLoop() {
-	for true {
+	for {
 		select {
 		case <-scraper.ticker.C:
-			scraper.UpdateTrades()
+			err := scraper.FetchTrades()
+			if err != nil {
+				log.Error("fetching nft trades: ", err)
+			}
 		case <-scraper.tradescraper.shutdown: // user requested shutdown
 			log.Printf("CryptoPunk scraper shutting down")
 			err := scraper.Close()
@@ -75,19 +78,9 @@ func (scraper *CryptoPunkScraper) mainLoop() {
 	}
 }
 
-func (scraper *CryptoPunkScraper) UpdateTrades() error {
-	trades, err := scraper.FetchTrades()
-	if err != nil {
-		return err
-	}
-	for _, trade := range trades {
-		scraper.GetTradeChannel() <- trade
-	}
-	return nil
-}
-
-func (scraper *CryptoPunkScraper) FetchTrades() (trades []dia.NFTTrade, err error) {
+func (scraper *CryptoPunkScraper) FetchTrades() error {
 	log.Info("fetch trades...")
+	var err error
 	if scraper.lastBlockNumber == nil || scraper.lastBlockNumber.Uint64() == 0 {
 		// TODO: what is the required value to the GetLastBlockNFTTrade method?
 		scraper.lastBlockNumber, err = scraper.tradescraper.datastore.GetLastBlockNFTTrade(dia.NFT{})
@@ -99,38 +92,32 @@ func (scraper *CryptoPunkScraper) FetchTrades() (trades []dia.NFTTrade, err erro
 	scraper.lastBlockNumber = big.NewInt(12653867)
 	filterer, err := cryptopunk.NewCryptoPunksMarketFilterer(scraper.contractAddress, scraper.tradescraper.ethConnection)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	// Get latest block number.
 	header, err := scraper.tradescraper.ethConnection.HeaderByNumber(context.Background(), nil)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	// TODO: It's a good practise to stay a litlle behind the head.
+	// TODO: It's a good practise to stay a little behind the head.
 	endBlockNumber := header.Number.Uint64() - 18
-	endBlockNumber = 12653869
+
 	// We need the cryptopunk abi to unpack the transfer event.
 	abi, err := abi.JSON(strings.NewReader(string(cryptopunk.CryptoPunksMarketABI)))
 	if err != nil {
-		return nil, err
+		return err
 	}
-	trades = make([]dia.NFTTrade, 0)
+	var iter *cryptopunk.CryptoPunksMarketPunkBoughtIterator
 	// Reduce the window size while there is an query limit error.
 	for {
 		fmt.Println("lastBlockNumber, endBlockNumber: ", scraper.lastBlockNumber.Uint64(), endBlockNumber)
 		// We're interested in the FilterPunkBought events when actual trades happened!
-		iter, err := filterer.FilterPunkBought(&bind.FilterOpts{
+		iter, err = filterer.FilterPunkBought(&bind.FilterOpts{
 			Start: scraper.lastBlockNumber.Uint64(),
 			End:   &endBlockNumber,
 		}, nil, nil, nil)
-
-	for iter.Next() {
-		fmt.Println("iter ")
-		// TODO: What value should i use for the blockchain argument?
-		nft, err := scraper.tradescraper.datastore.GetNFT(scraper.contractAddress.Hex(), dia.ETHEREUM, iter.Event.PunkIndex.String())
-
 		if err != nil {
 			if err.Error() == "query returned more than 10000 results" {
 				fmt.Println("Got `query returned more than 10000 results` error, reduce the window size and try again...")
@@ -138,26 +125,27 @@ func (scraper *CryptoPunkScraper) FetchTrades() (trades []dia.NFTTrade, err erro
 				continue
 			}
 			fmt.Println("error filtering FilterPunkBought: ", err)
-			return nil, err
+			return err
 		}
 
 		log.Infof("iter: %v", iter)
 		// Iter over FilterPunkBought events.
-		for iter.Next() {
-			// TODO: What value should i use for the blockchain argument?
-			// nft, err := scraper.tradescraper.datastore.GetNFT(scraper.contractAddress, "ethereum", iter.Event.PunkIndex.String())
-			// if err != nil {
-			// 	// TODO: should we continue if we failed to get NFT from the db or should we fail!
-			// 	// continue
-			// 	return nil, err
-			// }
 
-			// QUICK HACK: this is a workaround to a Cryptopunks contract bug that leads to an empty ToAddress.
+		for iter.Next() {
+			fmt.Println("iter ")
+			nft, err := scraper.tradescraper.datastore.GetNFT(scraper.contractAddress.Hex(), dia.ETHEREUM, iter.Event.PunkIndex.String())
+			if err != nil {
+				// TODO: should we continue if we failed to get NFT from the db or should we fail!
+				continue
+				// 	return nil, err
+			}
+
+			// This is a workaround to a Cryptopunks contract bug that leads to an empty ToAddress.
 			tx, err := scraper.tradescraper.ethConnection.TransactionReceipt(context.TODO(), iter.Event.Raw.TxHash)
 			if err != nil {
 				// TODO: should we continue if we failed to get tx or should we fail!
 				// continue
-				return nil, err
+				return err
 			}
 			log.Info("tx: ", tx.TxHash.Hex())
 
@@ -166,7 +154,6 @@ func (scraper *CryptoPunkScraper) FetchTrades() (trades []dia.NFTTrade, err erro
 				To    common.Address
 				Value *big.Int
 			}
-
 			for _, vLog := range tx.Logs {
 				log.Info("log", vLog)
 				err := abi.UnpackIntoInterface(&transferEvent, "Transfer", vLog.Data)
@@ -179,27 +166,43 @@ func (scraper *CryptoPunkScraper) FetchTrades() (trades []dia.NFTTrade, err erro
 				}
 			}
 
-			// trades = append(trades, dia.NFTTrade{
-			// 	NFT:         nft,
-			// 	BlockNumber: big.NewInt(int64(iter.Event.Raw.BlockNumber)),
-			// 	// TODO: Event.Value is in ETH value, how we can convert it to a USD value using
-			// 	// a internal function?
-			// 	PriceUSD:    float64(iter.Event.Value.Uint64()),
-			// 	FromAddress: iter.Event.FromAddress,
-			// 	ToAddress:   iter.Event.ToAddress,
-			// 	// TODO: exchange name? any Idea?
-			// 	Exchange: "",
-			// })
+			price := float64(iter.Event.Value.Uint64())
+			// If acceptBidForPunk is called, get the bid value from the bidding history.
+			// TO DO: Check that bid address is the same as trade toAddress.
+			// TO DO: Check that transaction input is acceptBidForPunk.
+			if price == 0 {
+				bid, err := scraper.tradescraper.datastore.GetLastNFTBid(scraper.contractAddress.Hex(), iter.Event.PunkIndex.String(), uint64(iter.Event.Raw.BlockNumber), iter.Event.Raw.Index)
+				if err != nil {
+					log.Error("could not find last bid")
+				}
+				fmt.Println("value is zero. Fetch from bids..")
+				fmt.Println(".. value: ", bid.Value)
+				price = bid.Value
+			}
+
+			trade := dia.NFTTrade{
+				NFT:         nft,
+				BlockNumber: big.NewInt(int64(iter.Event.Raw.BlockNumber)),
+				// TODO: Event.Value is in ETH value, how we can convert it to a USD value using
+				// a internal function?
+				PriceUSD:    float64(iter.Event.Value.Uint64()),
+				FromAddress: iter.Event.FromAddress,
+				ToAddress:   transferEvent.To,
+				Exchange:    "CryptopunkMarket",
+			}
+			scraper.GetTradeChannel() <- trade
 
 			log.Infof("got trade: ")
 			log.Infof("iter: %v", iter)
-			log.Info("price: ", float64(iter.Event.Value.Uint64()))
+			log.Info("price: ", price)
 			log.Info("from address: ", iter.Event.FromAddress)
 			log.Info("to address: ", iter.Event.ToAddress)
 			log.Info("to address(from the tx Transfer event): ", transferEvent.To)
 			log.Info("tx: ", iter.Event.Raw.TxHash)
 			log.Info("blockNumber: ", big.NewInt(int64(iter.Event.Raw.BlockNumber)))
 			log.Info("id: ", iter.Event.PunkIndex.String())
+			log.Info("input data: ", iter.Event.Raw.Data)
+			log.Info("input data string: ", iter.Event.Raw.Data)
 			log.Info("-----------------------------------------------")
 			log.Info(" ")
 			log.Info("-----------------------------------------------")
@@ -209,16 +212,7 @@ func (scraper *CryptoPunkScraper) FetchTrades() (trades []dia.NFTTrade, err erro
 
 	// Update the last lastBlockNumber value.
 	scraper.lastBlockNumber = new(big.Int).SetUint64(endBlockNumber)
-	return trades, nil
-}
-
-// GetTotalSupply returns the total supply of the NFT from on-chain.
-func (scraper *CryptoPunkScraper) GetTotalSupply() (*big.Int, error) {
-	contract, err := cryptopunk.NewCryptoPunksMarketCaller(scraper.contractAddress, scraper.tradescraper.ethConnection)
-	if err != nil {
-		fmt.Println("error getting contract: ", err)
-	}
-	return contract.TotalSupply(&bind.CallOpts{})
+	return nil
 }
 
 // GetDataChannel returns the scrapers data channel.
