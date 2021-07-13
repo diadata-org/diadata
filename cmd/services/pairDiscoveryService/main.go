821f3fe703628be9555821396694d8fa0e4a4034
jppade <jppade@googlemail.com>
2021-07-13 13:29:13 +0200

    increase ticker time in pairdiscovery

--- cmd/services/pairDiscoveryService/main.go
+++ cmd/services/pairDiscoveryService/main.go
@@ -54,7 +54,7 @@ func main() {
 	task := &Task{
 		closed: make(chan struct{}),
 		/// Retrieve every 4 hours
-		ticker: time.NewTicker(time.Second * 4 * 60),
+		ticker: time.NewTicker(time.Minute * 4 * 60),
 	}
 	relDB, err = models.NewRelDataStore()
 	if err != nil {
