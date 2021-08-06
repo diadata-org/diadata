25dfceae587d235a7146b87b5351e396310afd43
jppade <jppade@googlemail.com>
2021-08-06 16:10:26 +0200

    added some trading pairs

--- pkg/dia/helpers/SymbolIsBlackListed.go
+++ pkg/dia/helpers/SymbolIsBlackListed.go
@@ -175,6 +175,8 @@ func SymbolIsBlackListed(symbol string) bool {
 // AddressIsBlacklisted returns true if a token address is blacklisted
 func AddressIsBlacklisted(address common.Address) bool {
 	switch strings.ToLower(address.Hex()) {
+	case "0x5Dc02Ea99285E17656b8350722694c35154DB1E8":
+		return true
 	case "0xb2a9a0f34e3140de9b9a489b33fc049102a1808e":
 		return true
 	case "0x3e191a6ef96f87092fe8dce0d3f01977b08d6acf":
