50cae018942e64056a58442ad243082b9c12300a
nicky <nicky.gurbani@gmail.com>
2021-08-25 19:04:16 +0530

    check if tradeBlock is not empty

--- pkg/dia/helpers/queryHelper/filters.go
+++ pkg/dia/helpers/queryHelper/filters.go
@@ -27,15 +27,17 @@ func FilterMA(tradeBlocks []Block, asset dia.Asset, blockSize int) (filterPoints
 
 func FilterMAIR(tradeBlocks []Block, asset dia.Asset, blockSize int) (filterPoints []dia.FilterPoint) {
 	for _, block := range tradeBlocks {
-		maFilter := filters.NewFilterMAIR(asset, "", block.Trades[len(block.Trades)-1].Time, blockSize)
+		if len(block.Trades) > 0 {
+			maFilter := filters.NewFilterMAIR(asset, "", block.Trades[len(block.Trades)-1].Time, blockSize)
 
-		for _, trade := range block.Trades {
-			maFilter.Compute(trade)
+			for _, trade := range block.Trades {
+				maFilter.Compute(trade)
+			}
+
+			maFilter.FinalCompute(block.Trades[0].Time)
+			fp := maFilter.FilterPointForBlock()
+			filterPoints = append(filterPoints, *fp)
 		}
-
-		maFilter.FinalCompute(block.Trades[0].Time)
-		fp := maFilter.FilterPointForBlock()
-		filterPoints = append(filterPoints, *fp)
 	}
 	return filterPoints
 }
