d25599877761ec9f2cf68adb781b0b574b661b61
Eric Delaporte <eric.delaporte@build-ideas.de>
2021-11-01 15:43:42 +0100

    missed init of object

--- pkg/dia/ApiClient.go
+++ pkg/dia/ApiClient.go
@@ -152,6 +152,7 @@ func GetSymbolsList(url string) ([]string, error) {
 
 func GetConfigApi() (c *ConfigApi) {
 	if utils.Getenv("USE_ENV","false") == "true" {
+		c := ConfigApi{}
 		c.ApiKey = utils.Getenv("DIADATA_API_KEY", "")
 		c.SecretKey = utils.Getenv("DIADATA_SECRET_KEY", "")
 	} else {
