da1770304906617c9183118bc7c18838ead98cef
nicky <nicky.gurbani@gmail.com>
2021-11-08 00:23:27 +0530

    change starttime for trade query

--- pkg/graphql/resolver/root.go
+++ pkg/graphql/resolver/root.go
@@ -160,7 +160,7 @@ func (r *DiaResolver) GetChart(ctx context.Context, args struct {
 		}
 		tradeBlocks = queryhelper.NewBlockGenerator(trades).GenerateSize(blockSizeSeconds)
 	} else {
-		maxStartTime := endtime.Add(time.Duration(-((blockSizeSeconds - blockShiftSeconds) * 1000)) * time.Second)
+		maxStartTime := endtime.Add(time.Duration(-((blockSizeSeconds / blockShiftSeconds) * 1000)) * time.Second)
 
 		if starttime.Before(maxStartTime) {
 			starttime = maxStartTime
