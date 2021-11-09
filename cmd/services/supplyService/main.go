a32873ab608e83eb6665da22e198359ba2218cb1
Samuel Brack <samuel.brack@blockstate.com>
2021-11-09 09:47:35 +0100

    update circulating supply

--- cmd/services/supplyService/main.go
+++ cmd/services/supplyService/main.go
@@ -100,7 +100,7 @@ func setSupplies(tokenAddresses []string, lockedWalletsMap map[string][]string, 
 			}
 			supp.CirculatingSupply = float64(25549170)
 			// Set circulating supply
-			err = ds.SetDiaCirculatingSupply(float64(55481823))
+			err = ds.SetDiaCirculatingSupply(float64(56634600))
 			if err != nil {
 				log.Errorf("error setting circulating supply for %s: %v\n", supp.Symbol, err)
 			} else {
