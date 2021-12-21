eee8e31622cb1f62acb6f8650a7e329c446112b5
Samuel Brack <samuel.brack@blockstate.com>
2021-12-21 13:51:25 +0100

    added oracles

---
+++ cmd/blockchain/ethereum/diaDahliaOracleService/main.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/big"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/diadata-org/diadata/internal/pkg/blockchain-scrapers/blockchains/ethereum/diaOracleService"
+	models "github.com/diadata-org/diadata/pkg/model"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	/*
+	 * Read in Oracle address
+	 */
+	var deployedContract = flag.String("deployedContract", "", "Address of the deployed oracle contract")
+	var secretsFile = flag.String("secretsFile", "/run/secrets/oracle_keys_dahlia_celo", "File with wallet secrets")
+	var blockchainNode = flag.String("blockchainNode", "https://forno.celo.org", "Node address for blockchain connection")
+	var sleepSeconds = flag.Int("sleepSeconds", 10, "Number of seconds to sleep between calls")
+	var frequencySeconds = flag.Int("frequencySeconds", 120, "Number of seconds to sleep between checking oracle runs")
+	var chainId = flag.Int64("chainId", 42220, "Chain-ID of the network to connect to")
+	flag.Parse()
+
+	/*
+	 * Read secrets for unlocking the ETH account
+	 */
+	var lines []string
+	file, err := os.Open(*secretsFile) // Read in key information
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	if len(lines) != 2 {
+		log.Fatal("Secrets file should have exactly two lines")
+	}
+	key := lines[0]
+	key_password := lines[1]
+
+	//WBTC,ETH,USDC,CELO,UBE,MOBI
+	symbols := []string{"0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599", "0x0000000000000000000000000000000000000000", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "0x471EcE3750Da237f93B8E339c536989b8978a438", "0x00Be915B9dCf56a3CBE739D9B9c202ca692409EC", "0x73a210637f6F6B7005512677Ba6B3C96bb4AA44B"}
+	blockchains := []string{"Ethereum", "Ethereum", "Ethereum", "Celo", "Celo", "Celo"}
+
+	/*
+	 * Setup connection to contract, deploy if necessary
+	 */
+
+	conn, err := ethclient.Dial(*blockchainNode)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	auth, err := bind.NewTransactorWithChainID(strings.NewReader(key), key_password, big.NewInt(*chainId))
+	if err != nil {
+		log.Fatalf("Failed to create authorized transactor: %v", err)
+	}
+
+	var contract *diaOracleService.DIAOracle
+	err = deployOrBindContract(*deployedContract, conn, auth, &contract)
+	if err != nil {
+		log.Fatalf("Failed to Deploy or Bind contract: %v", err)
+	}
+
+	/*
+	 * Update Oracle periodically with top coins
+	 */
+	ticker := time.NewTicker(time.Duration(*frequencySeconds) * time.Second)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				for i, symbol := range symbols {
+					blockchain := blockchains[i]
+					err = periodicOracleUpdateHelper(auth, contract, conn, blockchain, symbol)
+					if err != nil {
+						log.Println(err)
+					}
+					time.Sleep(time.Duration(*sleepSeconds) * time.Second)
+				}
+			}
+		}
+	}()
+	select {}
+}
+
+func periodicOracleUpdateHelper(auth *bind.TransactOpts, contract *diaOracleService.DIAOracle, conn *ethclient.Client, blockchain string, symbol string) (error) {
+	// Get quotation for token and update Oracle
+	rawQ, err := getAssetQuotationFromDia(blockchain, symbol)
+	if err != nil {
+		log.Fatalf("Failed to retrieve %s quotation data from DIA: %v", symbol, err)
+		return err
+	}
+	rawQ.Name = symbol
+
+	err = updateQuotation(rawQ, auth, contract, conn)
+	if err != nil {
+		log.Fatalf("Failed to update DIA Oracle: %v", err)
+		return err
+	}
+	return nil
+}
+
+func deployOrBindContract(deployedContract string, conn *ethclient.Client, auth *bind.TransactOpts, contract **diaOracleService.DIAOracle) error {
+	var err error
+	if deployedContract != "" {
+		*contract, err = diaOracleService.NewDIAOracle(common.HexToAddress(deployedContract), conn)
+		if err != nil {
+			return err
+		}
+	} else {
+		// deploy contract
+		var addr common.Address
+		var tx *types.Transaction
+		addr, tx, *contract, err = diaOracleService.DeployDIAOracle(auth, conn)
+		if err != nil {
+			log.Fatalf("could not deploy contract: %v", err)
+			return err
+		}
+		log.Printf("Contract pending deploy: 0x%x\n", addr)
+		log.Printf("Transaction waiting to be mined: 0x%x\n\n", tx.Hash())
+		time.Sleep(180000 * time.Millisecond)
+	}
+	return nil
+}
+
+func updateQuotation(quotation *models.Quotation, auth *bind.TransactOpts, contract *diaOracleService.DIAOracle, conn *ethclient.Client) error {
+	symbol := quotation.Symbol + "/USD"
+	price := quotation.Price
+	timestamp := time.Now().Unix()
+	err := updateOracle(conn, contract, auth, symbol, int64(price*100000000), timestamp)
+	if err != nil {
+		log.Fatalf("Failed to update Oracle: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func updateOracle(
+	client *ethclient.Client,
+	contract *diaOracleService.DIAOracle,
+	auth *bind.TransactOpts,
+	key string,
+	value int64,
+	timestamp int64) error {
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get 110% of the gas price
+	fmt.Println(gasPrice)
+	fGas := new(big.Float).SetInt(gasPrice)
+	fGas.Mul(fGas, big.NewFloat(1.1))
+	gasPrice, _ = fGas.Int(nil)
+	fmt.Println(gasPrice)
+	// Write values to smart contract
+	tx, err := contract.SetValue(&bind.TransactOpts{
+		From:     auth.From,
+		Signer:   auth.Signer,
+		GasLimit: 1000725,
+		GasPrice: gasPrice,
+	}, key, big.NewInt(value), big.NewInt(timestamp))
+	if err != nil {
+		return err
+	}
+	fmt.Println(tx.GasPrice())
+	log.Printf("key: %s\n", key)
+	log.Printf("Tx To: %s\n", tx.To().String())
+	log.Printf("Tx Hash: 0x%x\n", tx.Hash())
+	return nil
+}
+func getAssetQuotationFromDia(blockchain, address string) (*models.Quotation, error) {
+	response, err := http.Get("https://rest.diadata.org/v1/assetQuotation/" + blockchain + "/" + address)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+	if 200 != response.StatusCode {
+		return nil, fmt.Errorf("Error on dia api with return code %d", response.StatusCode)
+	}
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	var quotation models.Quotation
+	err = quotation.UnmarshalBinary(contents)
+	if err != nil {
+		return nil, err
+	}
+	return &quotation, nil
+}
+
+func getQuotationFromDia(symbol string) (*models.Quotation, error) {
+	response, err := http.Get("https://rest.diadata.org/v1/quotation/" + strings.ToUpper(symbol))
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+	if 200 != response.StatusCode {
+		return nil, fmt.Errorf("Error on dia api with return code %d", response.StatusCode)
+	}
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	var quotation models.Quotation
+	err = quotation.UnmarshalBinary(contents)
+	if err != nil {
+		return nil, err
+	}
+	return &quotation, nil
+}
---
+++ cmd/blockchain/ethereum/diaDowsOracleService/main.go
@@ -0,0 +1,233 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/big"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/diadata-org/diadata/internal/pkg/blockchain-scrapers/blockchains/ethereum/diaDowsOracleService"
+	"github.com/diadata-org/diadata/pkg/dia"
+	models "github.com/diadata-org/diadata/pkg/model"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	/*
+	 * Read in Oracle address
+	 */
+	var deployedContract = flag.String("deployedContract", "", "Address of the deployed oracle contract")
+	var secretsFile = flag.String("secretsFile", "/run/secrets/oracle_keys_dows", "File with wallet secrets")
+	var blockchainNode = flag.String("blockchainNode", "https://matic-mainnet-full-rpc.bwarelabs.com", "Node address for blockchain connection")
+	var sleepSeconds = flag.Int("sleepSeconds", 1, "Number of seconds to sleep between calls")
+	var frequencySeconds = flag.Int("frequencySeconds", 120, "Number of seconds to sleep between checking oracle runs")
+	var deviationBips = flag.Int("deviationBips", 1, "Permille of deviation to trigger an oracle update")
+	var chainId = flag.Int64("chainId", 137, "Chain-ID of the network to connect to")
+	flag.Parse()
+
+	/*
+	 * Read secrets for unlocking the ETH account
+	 */
+	var lines []string
+	file, err := os.Open(*secretsFile) // Read in key information
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	if len(lines) != 2 {
+		log.Fatal("Secrets file should have exactly two lines")
+	}
+	key := lines[0]
+	key_password := lines[1]
+
+	/*
+	 * Setup connection to contract, deploy if necessary
+	 */
+
+	conn, err := ethclient.Dial(*blockchainNode)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	auth, err := bind.NewTransactorWithChainID(strings.NewReader(key), key_password, big.NewInt(*chainId))
+	if err != nil {
+		log.Fatalf("Failed to create authorized transactor: %v", err)
+	}
+
+	var contract *diaDowsOracleService.DIADowsOracle
+	err = deployOrBindContract(*deployedContract, conn, auth, &contract)
+	if err != nil {
+		log.Fatalf("Failed to Deploy or Bind contract: %v", err)
+	}
+	//periodicOracleUpdateHelper(*sleepSeconds, auth, contract, conn)
+	/*
+	 * Update Oracle periodically with top coins
+	 */
+	ticker := time.NewTicker(time.Duration(*frequencySeconds) * time.Second)
+	oldPrice := 0.0
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				oldPrice, err = periodicOracleUpdateHelper(*sleepSeconds, oldPrice, *deviationBips, auth, contract, conn)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}
+	}()
+	select {}
+}
+
+func periodicOracleUpdateHelper(sleepSeconds int, oldPrice float64, deviationBips int, auth *bind.TransactOpts, contract *diaDowsOracleService.DIADowsOracle, conn *ethclient.Client) (float64, error) {
+
+	// Get quotation for DOWS coin and update Oracle
+	rawDowsQ, err := getQuotationFromDia("DOWS")
+	if err != nil {
+		log.Fatalf("Failed to retrieve DOWS quotation data from DIA: %v", err)
+		return oldPrice, err
+	}
+	rawDowsQ.Name = "DOWS"
+
+	// Check for deviation
+	newPrice := rawDowsQ.Price
+
+	log.Println("In periodic block")
+	log.Println(oldPrice)
+	if (newPrice > (oldPrice * (1 + float64(deviationBips) / 10000))) || (newPrice < (oldPrice * (1 - float64(deviationBips) / 10000))) {
+		log.Println("Entering deviation based update zone")
+		err = updateQuotation(rawDowsQ, auth, contract, conn)
+		if err != nil {
+			log.Fatalf("Failed to update DOWS Oracle: %v", err)
+			return oldPrice, err
+		}
+		return newPrice, nil
+	}
+
+	return oldPrice, nil
+}
+
+func deployOrBindContract(deployedContract string, conn *ethclient.Client, auth *bind.TransactOpts, contract **diaDowsOracleService.DIADowsOracle) error {
+	var err error
+	if deployedContract != "" {
+		*contract, err = diaDowsOracleService.NewDIADowsOracle(common.HexToAddress(deployedContract), conn)
+		if err != nil {
+			return err
+		}
+	} else {
+		// deploy contract
+		var addr common.Address
+		var tx *types.Transaction
+		addr, tx, *contract, err = diaDowsOracleService.DeployDIADowsOracle(auth, conn)
+		if err != nil {
+			log.Fatalf("could not deploy contract: %v", err)
+			return err
+		}
+		log.Printf("Contract pending deploy: 0x%x\n", addr)
+		log.Printf("Transaction waiting to be mined: 0x%x\n\n", tx.Hash())
+		time.Sleep(180000 * time.Millisecond)
+	}
+	return nil
+}
+
+func updateQuotation(quotation *models.Quotation, auth *bind.TransactOpts, contract *diaDowsOracleService.DIADowsOracle, conn *ethclient.Client) error {
+	symbol := quotation.Symbol + "/USD"
+	price := quotation.Price
+	timestamp := time.Now().Unix()
+	err := updateOracle(conn, contract, auth, symbol, int64(price*100000000), timestamp)
+	if err != nil {
+		log.Fatalf("Failed to update Oracle: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func updatePair(quoteQuotation *models.Quotation, baseQuotation *models.Quotation, auth *bind.TransactOpts, contract *diaDowsOracleService.DIADowsOracle, conn *ethclient.Client) error {
+	symbol := quoteQuotation.Symbol + "/" + baseQuotation.Symbol
+	price := quoteQuotation.Price / baseQuotation.Price
+	timestamp := time.Now().Unix()
+	err := updateOracle(conn, contract, auth, symbol, int64(price*100000000), timestamp)
+	if err != nil {
+		log.Fatalf("Failed to update Oracle: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func updateOracle(
+	client *ethclient.Client,
+	contract *diaDowsOracleService.DIADowsOracle,
+	auth *bind.TransactOpts,
+	key string,
+	value int64,
+	timestamp int64) error {
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get 110% of the gas price
+	fmt.Println(gasPrice)
+	fGas := new(big.Float).SetInt(gasPrice)
+	fGas.Mul(fGas, big.NewFloat(1.1))
+	gasPrice, _ = fGas.Int(nil)
+	fmt.Println(gasPrice)
+	// Write values to smart contract
+	tx, err := contract.SetValue(&bind.TransactOpts{
+		From:     auth.From,
+		Signer:   auth.Signer,
+		GasLimit: 800725,
+		GasPrice: gasPrice,
+	}, key, big.NewInt(value), big.NewInt(timestamp))
+	if err != nil {
+		return err
+	}
+	fmt.Println(tx.GasPrice())
+	log.Printf("key: %s\n", key)
+	log.Printf("Nonce: %d\n", tx.Nonce())
+	log.Printf("Tx To: %s\n", tx.To().String())
+	log.Printf("Tx Hash: 0x%x\n", tx.Hash())
+	return nil
+}
+
+func getQuotationFromDia(symbol string) (*models.Quotation, error) {
+	response, err := http.Get(dia.BaseUrl + "/v1/quotation/" + strings.ToUpper(symbol))
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+	if 200 != response.StatusCode {
+		return nil, fmt.Errorf("Error on dia api with return code %d", response.StatusCode)
+	}
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	var quotation models.Quotation
+	err = quotation.UnmarshalBinary(contents)
+	if err != nil {
+		return nil, err
+	}
+	return &quotation, nil
+}
