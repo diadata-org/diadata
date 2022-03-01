afc77e65bb6dee924bd4fa638e822a1d26f519cb
Samuel Brack <samuel.brack@blockstate.com>
2022-03-01 18:07:39 +0100

    added kucoin, gateio

--- cmd/foreignscraper/candlestick/candlestick.go
+++ cmd/foreignscraper/candlestick/candlestick.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"strconv"
 	"strings"
 	"sync"
@@ -56,16 +58,20 @@ func handleExchangeScraper(exchange string, assets string, candleChan chan candl
 		log.Println("Binance Scraper: Start scraping")
 		err := scrapeBinance(assets, candleChan)
 		if err != nil {
-			log.Error(err)
+			log.Error("Binance scraper: ", err)
 		}
 	case "GateIO":
 		log.Println("Gateio Scraper: Start scraping")
 		err := scrapeGateio(assets, candleChan)
 		if err != nil {
-			log.Error(err)
+			log.Error("GateIO scraper: ", err)
 		}
 	case "Kucoin":
 		log.Println("Kucoin Scraper: Start scraping")
+		err := scrapeKucoin(assets, candleChan)
+		if err != nil {
+			log.Error("Kucoin scraper: ", err)
+		}
 	default:
 		log.Errorf("Unknown scraper name %s", exchange)
 	}
@@ -73,19 +79,20 @@ func handleExchangeScraper(exchange string, assets string, candleChan chan candl
 
 func scrapeBinance(assets string, candleChan chan candlestickMessage) error {
 	log.Info("Entered Binance handler")
-	wsBaseString := "wss://fstream.binance.com/stream?streams="
+	wsBaseString := "wss://stream.binance.com:9443/stream?streams="
 	wsAssetsString := ""
 
 	for _, asset := range strings.Split(assets, ",") {
 		wsAssetsString += strings.ToLower(asset) + "usdt@kline_1m" + "/"
 	}
+	// Remove trailing slash
+	wsAssetsString = wsAssetsString[:len(wsAssetsString)-1]
 	conn, _, err := ws.DefaultDialer.Dial(wsBaseString+wsAssetsString, nil)
 	if err != nil {
 		return err
 	}
 	defer conn.Close()
 
-	//TODO: Send into channel to main handler
 	for {
 		_, message, err := conn.ReadMessage()
 		if err != nil {
@@ -118,7 +125,7 @@ func scrapeBinance(assets string, candleChan chan candlestickMessage) error {
 
 func scrapeGateio(assets string, candleChan chan candlestickMessage) error {
 	log.Info("Entered GateIO handler")
-	wsBaseString := "wss://fx-ws-testnet.gateio.ws/v4/ws/"
+	wsBaseString := "wss://api.gateio.ws/ws/v4/"
 
 	conn, _, err := ws.DefaultDialer.Dial(wsBaseString, nil)
 	if err != nil {
@@ -126,12 +133,10 @@ func scrapeGateio(assets string, candleChan chan candlestickMessage) error {
 	}
 	defer conn.Close()
 	for _, asset := range strings.Split(assets, ",") {
-		msgToWrite := fmt.Sprintf("{\"time\":1,\"channel\":\"futures.candlesticks\",\"event\":\"subscribe\",\"payload\":[\"1m\",\"%s_USD\"]}", asset)
-		log.Println(msgToWrite)
+		msgToWrite := fmt.Sprintf("{\"time\":30,\"channel\":\"spot.candlesticks\",\"event\":\"subscribe\",\"payload\":[\"1m\",\"%s_USD\"]}", asset)
 		conn.WriteMessage(ws.TextMessage, []byte(msgToWrite))
 	}
 
-	//TODO: Send into channel to main handler
 	for {
 		_, message, err := conn.ReadMessage()
 		if err != nil {
@@ -139,6 +144,123 @@ func scrapeGateio(assets string, candleChan chan candlestickMessage) error {
 			return err
 		}
 		log.Printf("recv GateIO: %s", message)
+		messageMap := make(map[string]interface{})
+		err = json.Unmarshal(message, &messageMap)
+		if err != nil {
+			return err
+		}
+		result := messageMap["result"].(map[string]interface{})
+
+		// Check if we got a status msg
+		if result["status"] != nil {
+			continue
+		}
+
+		closingPriceString := result["c"].(string)
+		closingPrice, err := strconv.ParseFloat(closingPriceString, 64)
+		if err != nil {
+			return err
+		}
+		volumeString := result["v"].(string)
+		volume, err := strconv.ParseFloat(volumeString, 64)
+		if err != nil {
+			return err
+		}
+		timeUnixString := result["t"].(string)
+		timeUnix, err := strconv.ParseFloat(timeUnixString, 64)
+		if err != nil {
+			return err
+		}
+
+		foreignNameString := result["n"].(string)
+		foreignNameFiltered := strings.Split(foreignNameString, "_")[1]
+
+		candleStickMessage := candlestickMessage{
+			ForeignName:  strings.ToUpper(foreignNameFiltered) + "USDT",
+			ClosingPrice: closingPrice,
+			Volume:       volume,
+			Timestamp:    time.Unix(int64(timeUnix/1000), 0),
+			Source:       "GateIO",
+		}
+
+		candleChan <- candleStickMessage
+	}
+	return nil
+}
+
+func scrapeKucoin(assets string, candleChan chan candlestickMessage) error {
+	log.Info("Entered Kucoin handler")
+	wsBaseString := "wss://ws-api.kucoin.com/endpoint?token="
+
+	// Get token
+	tokenUrl := "https://api.kucoin.com/api/v1/bullet-public"
+	resp, err := http.Post(tokenUrl, "application/json", nil)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	bodyMap := make(map[string]interface{})
+	err = json.Unmarshal(body, &bodyMap)
+	if err != nil {
+		return err
+	}
+	token := bodyMap["data"].(map[string]interface{})["token"].(string)
+
+	conn, _, err := ws.DefaultDialer.Dial(wsBaseString + token, nil)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	for _, asset := range strings.Split(assets, ",") {
+		msgToWrite := fmt.Sprintf("{\"id\":1,\"type\":\"subscribe\",\"topic\": \"/market/candles:%s-USDT_1min\",\"response\": true}", strings.ToUpper(asset))
+		conn.WriteMessage(ws.TextMessage, []byte(msgToWrite))
+	}
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Errorln("read:", err)
+			return err
+		}
+		//log.Printf("recv Kucoin: %s", message)
+		messageMap := make(map[string]interface{})
+		err = json.Unmarshal(message, &messageMap)
+		if err != nil {
+			return err
+		}
+		// Check if we got a status msg
+		if messageMap["type"] != "message" {
+			continue
+		}
+		result := messageMap["data"].(map[string]interface{})
+		candles := result["candles"].([]interface{})
+
+		closingPriceString := candles[2].(string)
+		closingPrice, err := strconv.ParseFloat(closingPriceString, 64)
+		if err != nil {
+			return err
+		}
+		volumeString := candles[5].(string)
+		volume, err := strconv.ParseFloat(volumeString, 64)
+		if err != nil {
+			return err
+		}
+		timeUnix := result["time"].(float64)
+		timeUnix /= 1e9
+
+		foreignNameString := result["symbol"].(string)
+		foreignNameFiltered := strings.Split(foreignNameString, "-")[0]
+
+		candleStickMessage := candlestickMessage{
+			ForeignName:  foreignNameFiltered + "USDT",
+			ClosingPrice: closingPrice,
+			Volume:       volume,
+			Timestamp:    time.Unix(int64(timeUnix), 0),
+			Source:       "GateIO",
+		}
+
+		candleChan <- candleStickMessage
 	}
 	return nil
 }
