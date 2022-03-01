9dac5fd2bed80d6dcd88fecefa6707dc66dbe5b8
jppade <jppade@googlemail.com>
2022-03-01 16:08:16 +0100

    add handling channel to candlestick main.

--- cmd/foreignscraper/candlestick/candlestick.go
+++ cmd/foreignscraper/candlestick/candlestick.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	_ "github.com/diadata-org/diadata/pkg/model"
-	log "github.com/sirupsen/logrus"
 	ws "github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
 )
+
+type candlestickMessage struct {
+	ForeignName string
+	// ClosingPrice in USDT
+	ClosingPrice float64
+	Volume       float64
+	Timestamp    time.Time
+	Source       string
+}
 
 func main() {
 
@@ -20,30 +32,35 @@ func main() {
 	}*/
 
 	assets := flag.String("assets", "BTC,ETH", "asset symbols to query (from BTC, ETH, SOL, GLMR, DOT")
-	exchanges := flag.String("exchanges", "Binance,Kucoin", "exchanges to query (from Binance, Kucoin, Coinbase, Huobi, Okex, Gateio")
+	exchanges := flag.String("exchanges", "GateIO", "exchanges to query (from Binance, Kucoin, Coinbase, Huobi, Okex, Gateio")
 	flag.Parse()
+	cChan := make(chan candlestickMessage)
 
 	for _, exchange := range strings.Split(*exchanges, ",") {
 		wg.Add(1)
-		go handleExchangeScraper(exchange, *assets, &wg)
+		go handleExchangeScraper(exchange, *assets, cChan, &wg)
+	}
+	defer wg.Wait()
+	for {
+		// TO DO: Handle data every n minutes.
+		log.Info("data: ", <-cChan)
 	}
 
-	defer wg.Wait()
 }
 
-func handleExchangeScraper(exchange string, assets string, wg *sync.WaitGroup) {
+func handleExchangeScraper(exchange string, assets string, candleChan chan candlestickMessage, wg *sync.WaitGroup) {
 	defer wg.Done()
-	log.Println("Entered Exchange handler for %s", exchange)
+	log.Printf("Entered Exchange handler for %s", exchange)
 	switch exchange {
 	case "Binance":
 		log.Println("Binance Scraper: Start scraping")
-		err := scrapeBinance(assets)
+		err := scrapeBinance(assets, candleChan)
 		if err != nil {
 			log.Error(err)
 		}
-	case "Gateio":
+	case "GateIO":
 		log.Println("Gateio Scraper: Start scraping")
-		err := scrapeGateio(assets)
+		err := scrapeGateio(assets, candleChan)
 		if err != nil {
 			log.Error(err)
 		}
@@ -52,10 +69,9 @@ func handleExchangeScraper(exchange string, assets string, wg *sync.WaitGroup) {
 	default:
 		log.Errorf("Unknown scraper name %s", exchange)
 	}
-
 }
 
-func scrapeBinance(assets string) error {
+func scrapeBinance(assets string, candleChan chan candlestickMessage) error {
 	log.Info("Entered Binance handler")
 	wsBaseString := "wss://fstream.binance.com/stream?streams="
 	wsAssetsString := ""
@@ -63,7 +79,7 @@ func scrapeBinance(assets string) error {
 	for _, asset := range strings.Split(assets, ",") {
 		wsAssetsString += strings.ToLower(asset) + "usdt@kline_1m" + "/"
 	}
-	conn, _, err := ws.DefaultDialer.Dial(wsBaseString + wsAssetsString, nil)
+	conn, _, err := ws.DefaultDialer.Dial(wsBaseString+wsAssetsString, nil)
 	if err != nil {
 		return err
 	}
@@ -76,12 +92,31 @@ func scrapeBinance(assets string) error {
 			log.Errorln("read:", err)
 			return err
 		}
-		log.Printf("recv: %s", message)
+		messageMap := make(map[string]interface{})
+		err = json.Unmarshal(message, &messageMap)
+		data := messageMap["data"].(map[string]interface{})["k"].(map[string]interface{})
+
+		closingPriceString := data["c"].(string)
+		closingPrice, err := strconv.ParseFloat(closingPriceString, 64)
+		volumeString := data["V"].(string)
+		volume, err := strconv.ParseFloat(volumeString, 64)
+		timeUnix := data["t"].(float64)
+
+		candleStickMessage := candlestickMessage{
+			ForeignName:  data["s"].(string),
+			ClosingPrice: closingPrice,
+			Volume:       volume,
+			Timestamp:    time.Unix(int64(timeUnix/1000), 0),
+			Source:       "Binance",
+		}
+
+		candleChan <- candleStickMessage
+
 	}
 	return nil
 }
 
-func scrapeGateio(assets string) error {
+func scrapeGateio(assets string, candleChan chan candlestickMessage) error {
 	log.Info("Entered GateIO handler")
 	wsBaseString := "wss://fx-ws-testnet.gateio.ws/v4/ws/"
 
@@ -103,7 +138,11 @@ func scrapeGateio(assets string) error {
 			log.Errorln("read:", err)
 			return err
 		}
-		log.Printf("recv: %s", message)
+		log.Printf("recv GateIO: %s", message)
 	}
 	return nil
+}
+
+func makeCandle(candleData []candlestickMessage) {
+
 }
