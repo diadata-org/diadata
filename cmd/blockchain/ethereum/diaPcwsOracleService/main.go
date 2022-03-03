0ef605429892991fd987d1e53fe79b314a389cdc
Samuel Brack <samuel.brack@blockstate.com>
2022-03-03 19:13:48 +0100

    pcws oracle: new format

--- cmd/blockchain/ethereum/diaPcwsOracleService/main.go
+++ cmd/blockchain/ethereum/diaPcwsOracleService/main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"flag"
 	"fmt"
 	"github.com/diadata-org/diadata/pkg/dia/scraper/blockchain-scrapers/blockchains/ethereum/diaPcwsOracleService"
 	"io/ioutil"
 	"log"
 	"math/big"
 	"net/http"
-	"os"
+	"strconv"
 	"strings"
 	"time"
 
-	"github.com/diadata-org/diadata/pkg/dia"
 	models "github.com/diadata-org/diadata/pkg/model"
+	"github.com/diadata-org/diadata/pkg/utils"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -23,68 +21,53 @@ import (
 )
 
 func main() {
-	/*
-	 * Read in Oracle address
-	 */
-	var deployedContract = flag.String("deployedContract", "", "Address of the deployed oracle contract")
-	var secretsFile = flag.String("secretsFile", "/run/secrets/oracle_keys_pcws_bsc", "File with wallet secrets")
-	var blockchainNode = flag.String("blockchainNode", "https://bsc-dataseed.binance.org/", "Node address for blockchain connection")
-	var sleepSeconds = flag.Int("sleepSeconds", 120, "Number of seconds to sleep between calls")
-	var frequencySeconds = flag.Int("frequencySeconds", 86400, "Number of seconds to sleep between full oracle runs")
-	var chainId = flag.Int64("chainId", 56, "Chain-ID of the network to connect to")
-	flag.Parse()
-
-	/*
-	 * Read secrets for unlocking the ETH account
-	 */
-	var lines []string
-	file, err := os.Open(*secretsFile) // Read in key information
+	
+	key := utils.Getenv("PRIVATE_KEY", "")
+	key_password := utils.Getenv("PRIVATE_KEY_PASSWORD", "")
+	deployedContract := utils.Getenv("DEPLOYED_CONTRACT", "")
+	blockchainNode := utils.Getenv("BLOCKCHAIN_NODE", "")
+	sleepSeconds, err := strconv.Atoi(utils.Getenv("SLEEP_SECONDS", "120"))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to parse sleepSeconds: %v")
 	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+	frequencySeconds, err := strconv.Atoi(utils.Getenv("FREQUENCY_SECONDS", "120"))
+	if err != nil {
+		log.Fatalf("Failed to parse frequencySeconds: %v")
 	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	chainId, err := strconv.ParseInt(utils.Getenv("CHAIN_ID", "1"), 10, 64)
+	if err != nil {
+		log.Fatalf("Failed to parse chainId: %v")
 	}
-	if len(lines) != 2 {
-		log.Fatal("Secrets file should have exactly two lines")
-	}
-	key := lines[0]
-	key_password := lines[1]
 
 	/*
 	 * Setup connection to contract, deploy if necessary
 	 */
 
-	conn, err := ethclient.Dial(*blockchainNode)
+	conn, err := ethclient.Dial(blockchainNode)
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
 
-	auth, err := bind.NewTransactorWithChainID(strings.NewReader(key), key_password, big.NewInt(*chainId))
+	auth, err := bind.NewTransactorWithChainID(strings.NewReader(key), key_password, big.NewInt(chainId))
 	if err != nil {
 		log.Fatalf("Failed to create authorized transactor: %v", err)
 	}
 
 	var contract *diaPcwsOracleService.DIAPcwsOracle
-	err = deployOrBindContract(*deployedContract, conn, auth, &contract)
+	err = deployOrBindContract(deployedContract, conn, auth, &contract)
 	if err != nil {
 		log.Fatalf("Failed to Deploy or Bind contract: %v", err)
 	}
-	periodicOracleUpdateHelper(*sleepSeconds, auth, contract, conn)
+	periodicOracleUpdateHelper(sleepSeconds, auth, contract, conn)
 	/*
 	 * Update Oracle periodically with top coins
 	 */
-	ticker := time.NewTicker(time.Duration(*frequencySeconds) * time.Second)
+	ticker := time.NewTicker(time.Duration(frequencySeconds) * time.Second)
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				periodicOracleUpdateHelper(*sleepSeconds, auth, contract, conn)
+				periodicOracleUpdateHelper(sleepSeconds, auth, contract, conn)
 			}
 		}
 	}()
@@ -94,14 +77,14 @@ func main() {
 func periodicOracleUpdateHelper(sleepSeconds int, auth *bind.TransactOpts, contract *diaPcwsOracleService.DIAPcwsOracle, conn *ethclient.Client) error {
 
 	// Get quotation for pCWS coin and update Oracle
-	rawPcwsQ, err := getQuotationFromDia("PCWS")
+	rawPcwsQ, err := getAssetQuotationFromDia("BinanceSmartChain", "0xbcf39F0EDDa668C58371E519AF37CA705f2bFcbd")
 	if err != nil {
 		log.Fatalf("Failed to retrieve PCWS quotation data from DIA: %v", err)
 		return err
 	}
 	rawPcwsQ.Name = "PCWS"
 
-	rawBnbQ, err := getQuotationFromDia("BNB")
+	rawBnbQ, err := getAssetQuotationFromDia("BinanceSmartChain", "0x0000000000000000000000000000000000000000")
 	if err != nil {
 		log.Fatalf("Failed to retrieve BNB quotation data from DIA: %v", err)
 		return err
@@ -202,8 +185,8 @@ func updateOracle(
 	return nil
 }
 
-func getQuotationFromDia(symbol string) (*models.Quotation, error) {
-	response, err := http.Get(dia.BaseUrl + "/v1/quotation/" + strings.ToUpper(symbol))
+func getAssetQuotationFromDia(blockchain, address string) (*models.Quotation, error) {
+	response, err := http.Get("https://rest.diadata.org/v1/assetQuotation/" + blockchain + "/" + address)
 	if err != nil {
 		return nil, err
 	}
