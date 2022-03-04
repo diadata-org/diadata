97d239422a4cc476a55baaf295cf0ec1e02bfc90
Samuel Brack <samuel.brack@blockstate.com>
2022-03-04 19:38:48 +0100

    add cfbenchmarks eth oracle

--- cmd/blockchain/ethereum/diaCfBenchmarksOracleService/main.go
+++ cmd/blockchain/ethereum/diaCfBenchmarksOracleService/main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"math/big"
 	"net/http"
-	"os"
-	"strings"
 	"strconv"
+	"strings"
 	"time"
 
-	"github.com/diadata-org/diadata/internal/pkg/blockchain-scrapers/blockchains/ethereum/diaOracleServiceV2"
+	"github.com/diadata-org/diadata/pkg/dia/scraper/blockchain-scrapers/blockchains/ethereum/diaOracleServiceV2"
 	models "github.com/diadata-org/diadata/pkg/model"
+	"github.com/diadata-org/diadata/pkg/utils"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -24,58 +22,42 @@ import (
 )
 
 func main() {
-	/*
-	 * Read in Oracle address
-	 */
-
-	var deployedContract = flag.String("deployedContract", "", "Address of the deployed oracle contract")
-	var secretsFile = flag.String("secretsFile", "/run/secrets/oracle_keys", "File with wallet secrets")
-	var blockchainNode = flag.String("blockchainNode", "https://matic-mainnet-full-rpc.bwarelabs.com", "Node address for blockchain connection")
-	var sleepSeconds = flag.Int("sleepSeconds", 10, "Number of seconds to sleep between calls")
-	var frequencySeconds = flag.Int("frequencySeconds", 120, "Number of seconds to sleep between checking oracle runs")
-	var chainId = flag.Int64("chainId", 137, "Chain-ID of the network to connect to")
-	var apiSecretsFile = flag.String("apiSecretsFile", "/run/secrets/apisecrets", "File with API secrets")
-	flag.Parse()
-
-	/*
-	 * Read secrets for unlocking the ETH account
-	 */
-	var lines []string
-	file, err := os.Open(*secretsFile) // Read in key information
+	key := utils.Getenv("PRIVATE_KEY", "")
+	key_password := utils.Getenv("PRIVATE_KEY_PASSWORD", "")
+	apiUsername := utils.Getenv("API_USERNAME", "")
+	apiPassword := utils.Getenv("API_PASSWORD", "")
+	deployedContract := utils.Getenv("DEPLOYED_CONTRACT", "")
+	blockchainNode := utils.Getenv("BLOCKCHAIN_NODE", "")
+	sleepSeconds, err := strconv.Atoi(utils.Getenv("SLEEP_SECONDS", "120"))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to parse sleepSeconds: %v")
 	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+	frequencySeconds, err := strconv.Atoi(utils.Getenv("FREQUENCY_SECONDS", "120"))
+	if err != nil {
+		log.Fatalf("Failed to parse frequencySeconds: %v")
 	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	chainId, err := strconv.ParseInt(utils.Getenv("CHAIN_ID", "1"), 10, 64)
+	if err != nil {
+		log.Fatalf("Failed to parse chainId: %v")
 	}
-	if len(lines) != 2 {
-		log.Fatal("Secrets file should have exactly two lines")
-	}
-	key := lines[0]
-	key_password := lines[1]
 
 	symbols := []string{"ETH", "SOL"}
 	/*
 	 * Setup connection to contract, deploy if necessary
 	 */
 
-	conn, err := ethclient.Dial(*blockchainNode)
+	conn, err := ethclient.Dial(blockchainNode)
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
 
-	auth, err := bind.NewTransactorWithChainID(strings.NewReader(key), key_password, big.NewInt(*chainId))
+	auth, err := bind.NewTransactorWithChainID(strings.NewReader(key), key_password, big.NewInt(chainId))
 	if err != nil {
 		log.Fatalf("Failed to create authorized transactor: %v", err)
 	}
 
 	var contract *diaOracleServiceV2.DIAOracleV2
-	err = deployOrBindContract(*deployedContract, conn, auth, &contract)
+	err = deployOrBindContract(deployedContract, conn, auth, &contract)
 	if err != nil {
 		log.Fatalf("Failed to Deploy or Bind contract: %v", err)
 	}
@@ -83,17 +65,17 @@ func main() {
 	/*
 	 * Update Oracle periodically with top coins
 	 */
-	ticker := time.NewTicker(time.Duration(*frequencySeconds) * time.Second)
+	ticker := time.NewTicker(time.Duration(frequencySeconds) * time.Second)
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
 				for _, symbol := range symbols {
-					err = periodicOracleUpdateHelper(auth, contract, conn, symbol, *apiSecretsFile)
+					err = periodicOracleUpdateHelper(auth, contract, conn, symbol, apiUsername, apiPassword)
 					if err != nil {
 						log.Println(err)
 					}
-					time.Sleep(time.Duration(*sleepSeconds) * time.Second)
+					time.Sleep(time.Duration(sleepSeconds) * time.Second)
 				}
 			}
 		}
@@ -101,10 +83,10 @@ func main() {
 	select {}
 }
 
-func periodicOracleUpdateHelper(auth *bind.TransactOpts, contract *diaOracleServiceV2.DIAOracleV2, conn *ethclient.Client, symbol string, apiSecretsFile string) (error) {
+func periodicOracleUpdateHelper(auth *bind.TransactOpts, contract *diaOracleServiceV2.DIAOracleV2, conn *ethclient.Client, symbol string, apiUsername string, apiPassword string) (error) {
 
 	// Get latest API value and update Oracle
-	rawResult, err := getValueFromCfBenchmarks(symbol, apiSecretsFile)
+	rawResult, err := getValueFromCfBenchmarks(symbol, apiUsername, apiPassword)
 	if err != nil {
 		log.Fatalf("Failed to retrieve %s quotation data from CFBenchmarks: %v", symbol, err)
 		return err
@@ -210,27 +192,7 @@ func getAssetQuotationFromDia(blockchain, address string) (*models.Quotation, er
 	return &quotation, nil
 }
 
-func getValueFromCfBenchmarks(symbol string, apiSecretFile string) (float64, error) {
-	// Get the API key
-	var lines []string
-	file, err := os.Open(apiSecretFile) // Read in key information
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	if len(lines) != 2 {
-		log.Fatal("API secret file should have exactly two lines")
-	}
-	apiUsername := lines[0]
-	apiPassword := lines[1]
-
+func getValueFromCfBenchmarks(symbol string, apiUsername string, apiPassword string) (float64, error) {
 	// HTTP Basic Auth
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "https://oracleprod1.cfbenchmarks.com/api/v1/summary?id=DEFI_CF" + symbol + "USD", nil)
