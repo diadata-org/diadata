c8672eab09f3ae3042d36e1ffdfc9020d2db692d
jppade <jppade@googlemail.com>
2022-03-22 10:30:48 +0100

    add periodic trades flush in tbs.

--- internal/pkg/tradesBlockService/tradesBlockService.go
+++ internal/pkg/tradesBlockService/tradesBlockService.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"math"
 	"sort"
+	"strconv"
 	"sync"
 	"time"
 
@@ -16,10 +17,15 @@ import (
 
 type nothing struct{}
 
-var log *logrus.Logger
-
 func init() {
 	log = logrus.New()
+	batchTimeString = utils.Getenv("BATCH_TIME_SECONDS", "30")
+	var err error
+	batchTimeSeconds, err = strconv.Atoi(batchTimeString)
+	if err != nil {
+		log.Error("parse batchTimeString: ", err)
+	}
+
 }
 
 var (
@@ -31,7 +37,10 @@ var (
 		"PAX":  "",
 		"BUSD": "",
 	}
-	tol = float64(0.1)
+	tol              = float64(0.1)
+	log              *logrus.Logger
+	batchTimeString  string
+	batchTimeSeconds int
 )
 
 type TradesBlockService struct {
@@ -49,6 +58,7 @@ type TradesBlockService struct {
 	datastore        models.Datastore
 	historical       bool
 	writeMeasurement string
+	batchTicker      *time.Ticker
 }
 
 func NewTradesBlockService(datastore models.Datastore, blockDuration int64, historical bool) *TradesBlockService {
@@ -64,12 +74,14 @@ func NewTradesBlockService(datastore models.Datastore, blockDuration int64, hist
 		priceCache:      make(map[dia.Asset]float64),
 		datastore:       datastore,
 		historical:      historical,
+		batchTicker:     time.NewTicker(time.Duration(batchTimeSeconds) * time.Second),
 	}
 	if historical {
 		s.writeMeasurement = utils.Getenv("INFLUX_MEASUREMENT_WRITE", "tradesTmp")
 	}
 	log.Info("write measurement: ", s.writeMeasurement)
 	log.Info("historical: ", s.historical)
+	log.Info("batch ticker time: ", batchTimeSeconds)
 	go s.mainLoop()
 	return s
 }
@@ -84,15 +96,18 @@ func (s *TradesBlockService) mainLoop() {
 			return
 		case t := <-s.chanTrades:
 			s.process(*t)
+		case <-s.batchTicker.C:
+			err := s.datastore.Flush()
+			if err != nil {
+				log.Error("flush influx batch: ", err)
+			}
 		}
 	}
 }
 
 func (s *TradesBlockService) process(t dia.Trade) {
-	// tInit := time.Now()
 
 	var verifiedTrade bool
-	// baseTokenSymbol := t.GetBaseToken()
 
 	// Price estimation can only be done for verified pairs.
 	// Trades with unverified pairs are still saved, but not sent to the filtersBlockService.
@@ -109,27 +124,8 @@ func (s *TradesBlockService) process(t dia.Trade) {
 			var price float64
 			var ok bool
 			var err error
-			if s.historical {
-				// Look for historic price of base token at trade time...
-				if _, ok = s.priceCache[t.BaseToken]; ok {
-					price = s.priceCache[t.BaseToken]
-				} else {
-					price, err = s.datastore.GetAssetPriceUSD(t.BaseToken, t.Time)
-					s.priceCache[t.BaseToken] = price
-					if t.BaseToken.Address == "0x0000000000000000000000000000000000000000" {
-						if t.BaseToken.Blockchain == "Bitcoin" {
-							log.Infof("quotation for BTC from influx: %v", price)
-						}
-						if t.BaseToken.Blockchain == "Ethereum" {
-							log.Infof("quotation for ETH from influx: %v", price)
-						}
-					}
-
-				}
-			} else {
-				// ...or latest price. This method is quicker as it first queries the cache.
-				// Comment Philipp 09/11/2021: This might still be too slow, as it queries influx
-				// as soon as there is no quotation in the cache.
+			if !s.historical {
+				// Get latest price from cache.
 				// price, err = s.datastore.GetAssetPriceUSDLatest(t.BaseToken)
 
 				basetoken := t.BaseToken
@@ -148,7 +144,6 @@ func (s *TradesBlockService) process(t dia.Trade) {
 						Blockchain: "Ethereum",
 					}
 				}
-				// -----------------------------------------
 
 				if _, ok = s.priceCache[basetoken]; ok {
 					price = s.priceCache[basetoken]
@@ -159,15 +154,29 @@ func (s *TradesBlockService) process(t dia.Trade) {
 					log.Infof("quotation for %s from redis cache: %v", basetoken.Symbol, price)
 				}
 
+			} else {
+
+				// Look for historic price of base token at trade time.
+				if _, ok = s.priceCache[t.BaseToken]; ok {
+					price = s.priceCache[t.BaseToken]
+				} else {
+					price, err = s.datastore.GetAssetPriceUSD(t.BaseToken, t.Time)
+					s.priceCache[t.BaseToken] = price
+					if t.BaseToken.Address == "0x0000000000000000000000000000000000000000" {
+						if t.BaseToken.Blockchain == "Bitcoin" {
+							log.Infof("quotation for BTC from influx: %v", price)
+						}
+						if t.BaseToken.Blockchain == "Ethereum" {
+							log.Infof("quotation for ETH from influx: %v", price)
+						}
+					}
+				}
+
 			}
 			if err != nil {
 				log.Errorf("Cannot use trade %s. Can't find quotation for base token.", t.Pair)
 			} else {
 				if price > 0.0 {
-					// log.Infof("price of trade %s on exchange %s: %v", t.Pair, t.Source, t.Price)
-					// log.Info("price of base token: ", price)
-					// log.Info("resulting estimatedUSDPrice: ", t.Price*price)
-					// TO DO: Switch to big.Float?
 					t.EstimatedUSDPrice = t.Price * price
 					if t.EstimatedUSDPrice > 0 {
 						verifiedTrade = true
@@ -185,16 +194,15 @@ func (s *TradesBlockService) process(t dia.Trade) {
 		}
 	}
 	// Comment Philipp: We could make another check here. Store CG and/or CMC quotation in redis cache
-	// and compare with estimatedUSDPrice. If deviation is too large ignore trade. If we do so,
-	// we should already think about how to do it best with regards to historic values, as these are coming up.
+	// and compare with estimatedUSDPrice. If deviation is too large ignore trade.
 	var err error
-	if s.historical {
-		err = s.datastore.SaveTradeInfluxToTable(&t, s.writeMeasurement)
+	if !s.historical {
+		err = s.datastore.SaveTradeInflux(&t)
 		if err != nil {
 			log.Error(err)
 		}
 	} else {
-		err = s.datastore.SaveTradeInflux(&t)
+		err = s.datastore.SaveTradeInfluxToTable(&t, s.writeMeasurement)
 		if err != nil {
 			log.Error(err)
 		}
