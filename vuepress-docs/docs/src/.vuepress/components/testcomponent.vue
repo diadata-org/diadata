2cd03ccbcf7cb5d4a4db6ac943a4520dd5710700
udodia <92979489+udodia@users.noreply.github.com>
2022-03-25 12:00:17 +0100

    Create testcomponent.vue

---
+++ vuepress-docs/docs/src/.vuepress/components/testcomponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="testAlert">{{ displayText }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['displayText'],
+  methods: {
+    testAlert() {
+      alert('Test Alert');
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  background-color: #4CAF50;
+}
+</style>
