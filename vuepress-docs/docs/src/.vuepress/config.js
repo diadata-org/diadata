0c2943d3020f6e2d525f85435caee15a493566be
udodia <92979489+udodia@users.noreply.github.com>
2022-03-28 11:47:15 +0200

    Update config.js

--- vuepress-docs/docs/src/.vuepress/config.js
+++ vuepress-docs/docs/src/.vuepress/config.js
@@ -60,7 +60,7 @@ module.exports = {
         path: '/',      // optional, link of the title, which should be an absolute path and must exist
         collapsable: false, // optional, defaults to true
         sidebarDepth: 1,    // optional, defaults to 1
-        children: []
+        children: [],
       },
       {
         title: 'Data access',
@@ -121,7 +121,7 @@ module.exports = {
         {
           title: 'Contribute',
           collapsable: true,
-          children: [ /* ... */ ],
+          children: [ ],
         }
       ]
     }
