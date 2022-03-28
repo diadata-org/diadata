c9ee73d389d62aba1a4b1cbb1a4274e89baedcaf
udodia <92979489+udodia@users.noreply.github.com>
2022-03-28 12:45:35 +0200

    Create ProfileCard.vue

---
+++ vuepress-docs/docs/src/.vuepress/components/ProfileCard.vue
@@ -0,0 +1,21 @@
+<template>
+<section class="profile-card">
+    <img class="profile-img" :src="$attrs.imgSrc" />
+    <h1>Sample component with rich content</h1>
+    <div>
+        <slot /> <!-- This is where your markdown content will be inserted -->
+    </div>
+</section>
+</template>
+
+<style scoped>
+.profile-card {
+    display: grid;
+    /* ... more styles ...*/
+}
+
+.profile-img {
+    border-radius: 50%;
+    /* ... more styles ...*/
+}
+</style>
