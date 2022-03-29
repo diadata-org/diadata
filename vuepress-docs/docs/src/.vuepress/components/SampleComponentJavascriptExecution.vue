408d6c2b133fd1e220e739cf2459cbf8c3588416
udodia <92979489+udodia@users.noreply.github.com>
2022-03-29 10:16:08 +0200

    Create SampleComponentJavascriptExecution.vue

---
+++ vuepress-docs/docs/src/.vuepress/components/SampleComponentJavascriptExecution.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  <h1>SampleComponentJavascriptExecution.vue</h1>
+  <h2>{{ headlineText }}</h2>
+    <button @click="testAlert">{{ displayText }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['displayText','headlineText'],
+  methods: {
+    testAlert() {
+      alert('Test Alert');
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  background-color: #4CAF50;
+}
+</style>
