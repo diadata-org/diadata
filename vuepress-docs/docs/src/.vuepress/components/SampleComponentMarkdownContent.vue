a299df2cbebed4faaa2a9219c0f250cc4283521c
udodia <92979489+udodia@users.noreply.github.com>
2022-03-29 10:12:52 +0200

    Create SampleComponentMarkdownContent.vue

---
+++ vuepress-docs/docs/src/.vuepress/components/SampleComponentMarkdownContent.vue
@@ -0,0 +1,21 @@
+<template>
+<section class="some-section-class">
+    <img class="some-img-class" :src="$attrs.imgSrc" />
+    <h1>Sample component with rich content</h1>
+    <div>
+        <slot /> <!-- This is where your markdown content will be inserted -->
+    </div>
+</section>
+</template>
+
+<style scoped>
+.some-section-class {
+    display: grid;
+    border:solid 1px red;
+    /* ... more styles ...*/
+}
+.some-img-class {
+    border-radius: 50%;
+    /* ... more styles ...*/
+}
+</style>
