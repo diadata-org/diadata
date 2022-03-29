def45023f5cc1732b32264d9727c08dac558cace
udodia <92979489+udodia@users.noreply.github.com>
2022-03-29 11:26:35 +0200

    Update SampleComponentJavascriptExecution.vue

--- vuepress-docs/docs/src/.vuepress/components/SampleComponentJavascriptExecution.vue
+++ vuepress-docs/docs/src/.vuepress/components/SampleComponentJavascriptExecution.vue
@@ -11,7 +11,7 @@ export default {
   props: ['displayText','headlineText'],
   methods: {
     testAlert() {
-      alert("why does this not work");
+      alert("{{ displayText }}");
     }
   }
 }
