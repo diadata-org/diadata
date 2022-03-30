faf5e46d48053514528610f11c9064d113444952
udodia <92979489+udodia@users.noreply.github.com>
2022-03-30 10:32:30 +0200

    Update SampleComponentForLoop.vue

--- vuepress-docs/docs/src/.vuepress/components/SampleComponentForLoop.vue
+++ vuepress-docs/docs/src/.vuepress/components/SampleComponentForLoop.vue
@@ -10,7 +10,7 @@ export default {
   props: ['MyList'],
   data: function() {
     return {
-      MyList: this.props['MyList'].split(','),
+      MyList: props['MyList'].split(','),
       //MyList: MyInternalList.split(',')
       //bigCats: ['Mountain Lion', '	Clouded leopard', 'Eurasian lynx']
     }
