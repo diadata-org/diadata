c5c9c72b85e5b6770d8436b637b5ef7ea3919616
udodia <92979489+udodia@users.noreply.github.com>
2022-03-30 09:30:58 +0200

    Create SampleComponentForLoop.vue

---
+++ vuepress-docs/docs/src/.vuepress/components/SampleComponentForLoop.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <li v-for="bigCat in bigCats" :key="bigCat">
+      {{ bigCat }}
+    </li>
+  </div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      bigCats: ['Mountain Lion', '	Clouded leopard', 'Eurasian lynx']
+    }
+  }
+}
+</script>
