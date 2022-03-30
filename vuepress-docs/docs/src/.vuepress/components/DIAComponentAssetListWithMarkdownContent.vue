c95bae60c37067846ab8e85dab1aa355ef8ed209
udodia <92979489+udodia@users.noreply.github.com>
2022-03-30 12:08:27 +0200

    Create DIAComponentAssetListWithMarkdownContent.vue

---
+++ vuepress-docs/docs/src/.vuepress/components/DIAComponentAssetListWithMarkdownContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <li v-for="InternalArrayItem in MyInternalList" :key="InternalArrayItem">
+      {{ InternalArrayItem }}
+    </li>
+  </div>
+</template>
+<script>
+export default {
+  props: ['AssetList'],
+  data: function() {
+    return {
+      MyInternalList: this.AssetList.split(','),
+    }
+  }
+ 
+}
+</script>
