1681b405be1be1296f4da2b1eebc7fdf64088847
udodia <92979489+udodia@users.noreply.github.com>
2022-03-30 12:36:00 +0200

    Update DIAComponentAssetListWithMarkdownContent.vue

--- vuepress-docs/docs/src/.vuepress/components/DIAComponentAssetListWithMarkdownContent.vue
+++ vuepress-docs/docs/src/.vuepress/components/DIAComponentAssetListWithMarkdownContent.vue
@@ -18,7 +18,11 @@ export default {
       return assetId;
     },
     parseAndReturnAssetList( assetList ) {
-      var returnList = assetList.split(',');
+      var tmpList = assetList.split(',');
+      var returnList = new Array();
+      for ( i=0; i<tmpList.length; i++ ) {
+        returnList.push( tmpList[i] );
+      }
       return returnList;
     }
   },
