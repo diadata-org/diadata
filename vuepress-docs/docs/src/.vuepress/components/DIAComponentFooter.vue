e11ed4c246d86920fe135db479f6e676f2227170
udodia <92979489+udodia@users.noreply.github.com>
2022-03-31 11:22:18 +0200

    Update DIAComponentFooter.vue

--- vuepress-docs/docs/src/.vuepress/components/DIAComponentFooter.vue
+++ vuepress-docs/docs/src/.vuepress/components/DIAComponentFooter.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="DIAStyleFooter">
-see src/.vuepress/components/DIAComponentFooter.vue
+FOOTER see src/.vuepress/components/DIAComponentFooter.vue
   <a href="devnull">Imprint</a>
 </div>
 </template>
