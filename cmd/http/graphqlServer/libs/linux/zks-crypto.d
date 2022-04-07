9d3cbd4f78c9a908297c7df7b8f345aba6952b5b
Eric Delaporte <eric.delaporte@build-ideas.de>
2022-04-07 15:03:50 +0200

    updated nfts to use go 1.17

---
+++ cmd/http/graphqlServer/libs/linux/zks-crypto.d
@@ -0,0 +1,1 @@
+/home/runner/work/zksync-crypto-c/zksync-crypto-c/target/release/libzks_crypto.so: /home/runner/work/zksync-crypto-c/zksync-crypto-c/zks-crypto-c/build.rs /home/runner/work/zksync-crypto-c/zksync-crypto-c/zks-crypto-c/src/lib.rs /home/runner/work/zksync-crypto-c/zksync-crypto-c/zks-crypto-c/src/utils.rs
