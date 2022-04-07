776604e4bd876d9e89a916d16e1ab6e0249804cb
jppade <jppade@googlemail.com>
2022-04-07 17:40:59 +0200

    add some cex pairs. verify some symbols.

--- pkg/dia/Messages.go
+++ pkg/dia/Messages.go
@@ -23,7 +23,7 @@ const (
 	POLYGON                                 = "Polygon"
 	CELO                                    = "Celo"
 	FANTOM                                  = "Fantom"
-	NEAR                                    = "Near"
+	NEAR                                    = "NEAR"
 	AURORA                                  = "Aurora"
 	SOLANA                                  = "Solana"
 	FLOW                                    = "Flow"
