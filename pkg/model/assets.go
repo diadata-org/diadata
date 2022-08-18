d3bb7acf8af15905c9858902f9097137bdd1d1d4
nicky <nicky.gurbani@gmail.com>
2022-08-18 18:18:27 +0530

    log GetAssetsWithVOL query

--- pkg/model/assets.go
+++ pkg/model/assets.go
@@ -999,7 +999,7 @@ func (rdb *RelDB) GetAssetsWithVOLRange(starttime time.Time, endtime time.Time) 
 // GetAssetsWithVOL returns the first @numAssets assets with entry in the assetvolume table, sorted by volume in descending order.
 // If @numAssets==0, all assets are returned.
 // If @substring is not the empty string, results are filtered by the first letters being @substring.
-func (rdb *RelDB) GetAssetsWithVOL(numAssets int64, skip int64, substring string) (volumeSortedAssets []dia.AssetVolume, err error) {
+func (rdb *RelDB) c(numAssets int64, skip int64, substring string) (volumeSortedAssets []dia.AssetVolume, err error) {
 	var (
 		queryString string
 		query       string
@@ -1023,10 +1023,13 @@ func (rdb *RelDB) GetAssetsWithVOL(numAssets int64, skip int64, substring string
 		}
 	}
 
+	log.Infoln("query query", query)
+
 	rows, err = rdb.postgresClient.Query(context.Background(), query)
 	if err != nil {
 		return
 	}
+
 	defer rows.Close()
 
 	for rows.Next() {
