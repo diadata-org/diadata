b69317e61a51711d764e378b2cdb4ad4d69582da
Eric Delaporte <eric.delaporte@build-ideas.de>
2022-08-26 17:50:50 +0200

    added monitoring from database to service

--- cmd/http/monitoringServer/go.mod
+++ cmd/http/monitoringServer/go.mod
@@ -1,6 +1,6 @@
 module github.com/diadata-org/diadata/http/monitoringServer
 
-go 1.14
+go 1.17
 
 replace (
 	k8s.io/api v1.5.2 => k8s.io/api v0.21.3
@@ -8,11 +8,74 @@ replace (
 )
 
 require (
-	github.com/diadata-org/diadata v1.4.1-rc-144-probes
+	github.com/diadata-org/diadata v1.4.1-rc-351
 	github.com/gin-gonic/gin v1.7.2
-	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/google/uuid v1.3.0
+	github.com/jackc/pgx/v4 v4.11.0
 	github.com/sirupsen/logrus v1.8.1
 	k8s.io/api v0.21.3
 	k8s.io/apimachinery v0.21.3
 	k8s.io/client-go v0.21.3
+)
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190717042225-c3de453c63f4 // indirect
+	github.com/btcsuite/btcd v0.21.0-beta // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/ethereum/go-ethereum v1.10.10 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-logr/logr v0.4.0 // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/gofuzz v1.1.1-0.20200604201612-c04b05f3adfa // indirect
+	github.com/googleapis/gnostic v0.4.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/influxdata/influxdb1-client v0.0.0-20200827194710-b269163b24ab // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.8.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.0.6 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.7.0 // indirect
+	github.com/jackc/puddle v1.1.3 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/prometheus/common v0.7.0 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tklauser/go-sysconf v0.3.5 // indirect
+	github.com/tklauser/numcpus v0.2.2 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1 // indirect
+	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/klog/v2 v2.8.0 // indirect
+	k8s.io/utils v0.0.0-20201110183641-67b214c5f920 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.1.2 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
 )
--- cmd/http/monitoringServer/main.go
+++ cmd/http/monitoringServer/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/diadata-org/diadata/http/monitoringServer/config"
 	"github.com/diadata-org/diadata/http/monitoringServer/databases"
 	"github.com/diadata-org/diadata/http/monitoringServer/enums"
 	"github.com/diadata-org/diadata/http/monitoringServer/nodes"
 	"github.com/diadata-org/diadata/http/monitoringServer/platform"
+	"github.com/diadata-org/diadata/pkg/dia/helpers/db"
 	"github.com/diadata-org/diadata/pkg/http/restApi"
 	"github.com/diadata-org/diadata/pkg/utils"
 	"github.com/diadata-org/diadata/pkg/utils/probes"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4/pgxpool"
 	log "github.com/sirupsen/logrus"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"net/http"
+	"strings"
 )
 
 var startupDone = false
@@ -51,6 +58,91 @@ func live() bool {
 	return config.GetKubernetesConnection() != nil
 }
 
+type MonitoringGroup struct {
+	id            uuid.UUID
+	groupName     string
+	groupParentId uuid.UUID
+}
+
+type MonitoringItem struct {
+	itemName       string
+	k8sNamespace   string
+	k8sServiceName string
+}
+
+func getMonitoringGroupStates() []config.State {
+	return getMonitoringGroupConfigStates(db.PostgresDatabase(), uuid.Nil)
+}
+
+func getMonitoringGroupConfigStates(conn *pgxpool.Pool, groupParentId uuid.UUID) (states []config.State) {
+	parentWhere := ""
+	if groupParentId != uuid.Nil {
+		parentWhere = fmt.Sprintf(" where group_parent_id = %s", groupParentId)
+	}
+	query := fmt.Sprintf("select id, group_name from monitoring_groups %s", parentWhere)
+
+	log.Info("reading service monitoring endpoints")
+	rows, err := conn.Query(context.Background(), query)
+	if err != nil {
+		log.Error("error reading endpoint from postgres", err)
+		return nil
+	}
+
+	kube := config.GetKubernetesConnection()
+
+	for rows.Next() {
+		var monitoringGroup MonitoringGroup
+		err := rows.Scan(
+			&monitoringGroup.id,
+			&monitoringGroup.groupName,
+			&monitoringGroup.groupParentId,
+		)
+		monitorState := config.GetOperationalHealthState(monitoringGroup.groupName)
+		itemQuery := fmt.Sprintf("select item_name, k8s_namespace, k8s_servicename from monitoring_items WHERE monitoring_group_id = '%s' ", monitoringGroup.groupParentId.String())
+		itemRows, err := conn.Query(context.Background(), itemQuery)
+		if err != nil {
+			log.Error("error reading endpoint from postgres", err)
+			return nil
+		}
+		for itemRows.Next() {
+			var monitoringItem MonitoringItem
+			err = itemRows.Scan(
+				&monitoringItem.itemName,
+				&monitoringItem.k8sNamespace,
+				&monitoringItem.k8sServiceName,
+			)
+			itemState := config.GetMajorHealthState(monitoringItem.k8sServiceName)
+			listOptions := metaV1.ListOptions{}
+			services, serviceErr := kube.CoreV1().Services(monitoringItem.k8sNamespace).List(context.TODO(), listOptions)
+			if serviceErr != nil {
+				log.Error(serviceErr)
+				return nil
+			}
+			for _, service := range services.Items {
+				if strings.Contains(service.Name, monitoringItem.k8sServiceName) {
+					if len(service.Status.Conditions) == 0 {
+						itemState.State = enums.HealthStateOperational
+					}
+					break
+				}
+			}
+			monitorState.Subsection = append(monitorState.Subsection, itemState)
+			subStates := getMonitoringGroupConfigStates(conn, monitoringGroup.id)
+
+			subState := config.GetOperationalHealthState(monitoringGroup.groupName + " Children")
+			for _, subStateItem := range subStates {
+				subState.Subsection = append(subState.Subsection, subStateItem)
+			}
+		}
+		fmt.Printf("%v", monitorState)
+		defer itemRows.Close()
+		states = append(states, monitorState)
+	}
+	defer rows.Close()
+	log.Info("finished reading static endpoints")
+	return
+}
+
 func mergeStateSlicesAsSubsection(name string, states []config.State) config.State {
 	state := config.GetOperationalHealthState(name)
 
@@ -74,7 +166,10 @@ func mergeStateSlicesAsSubsection(name string, states []config.State) config.Sta
 }
 
 func GetAllStates(context *gin.Context) {
-	var states []config.State
+	states := getMonitoringGroupStates()
+	if states == nil {
+		states = []config.State{}
+	}
 	dbStates := databases.GetAllStates()
 	states = append(states, mergeStateSlicesAsSubsection("databases", dbStates))
 	nodeStates := nodes.GetAllStates()
