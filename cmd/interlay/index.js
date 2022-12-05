4f4c153331f96c2ba76b23cedc10cbf23b34d91b
nicky <nicky.gurbani@gmail.com>
2022-12-05 12:21:08 +0530

    add interlay rest api

--- cmd/interlay/index.js
+++ cmd/interlay/index.js
@@ -1,74 +1,14 @@
 const { ApiPromise, WsProvider } = require("@polkadot/api");
+const { totalIssuance, oracleaggregrate,totalUserVaultCollateral,collateralCurrencies } = require("./helper");
+
 const ethers = require("ethers");
 const bignumber = ethers.BigNumber;
 
-async function collateralCurrencies(api) {
-  const collateralCurrencies =
-    await api.query.vaultRegistry.systemCollateralCeiling.entries();
-  const collateralCurrenciesmap = new Map();
-  collateralCurrencies.map((collateralCurrencie) => {
-    let key = collateralCurrencie[0].toHuman();
-    let value = collateralCurrencie[1].toHuman();
-    // TODO filter only ibtc as wrapped {"collateral":{"Token":"DOT"},"wrapped":{"Token":"IBTC"}}
-    // console.log("key",key[0].collateral.Token);
-    // console.log("value",JSON.stringify(value));
+let express = require('express');
+let app = express();
 
-    collateralCurrenciesmap.set(key[0].collateral.Token, value);
-  });
-  return collateralCurrenciesmap;
-}
 
-//https://github.com/DefiLlama/DefiLlama-Adapters/blob/f0bcc4e65ebe43ce00c77951376a2249f2931dc3/projects/interlay-collateral/api.js
-
-async function totalUserVaultCollateral(api) {
-  const totalUserVaultCollaterals =
-    await api.query.vaultRegistry.totalUserVaultCollateral.entries();
-  const totalUserVaultCollateralsmap = new Map();
-
-  totalUserVaultCollaterals.map((totalUserVaultCollateral) => {
-    let key = totalUserVaultCollateral[0].toHuman();
-    let value = totalUserVaultCollateral[1].toHuman();
-
-    totalUserVaultCollateralsmap.set(key[0].collateral.Token, value);
-  });
-  return totalUserVaultCollateralsmap;
-}
-
-async function oracleaggregrate(api) {
-  const oracleaggregrators = await api.query.oracle.aggregate.entries();
-
-  const oracleaggregratemap = new Map();
-
-  oracleaggregrators.map((oracleaggregrate) => {
-    let key = oracleaggregrate[0].toHuman();
-    let value = oracleaggregrate[1].toHuman();
-
-    if (key[0].ExchangeRate) {
-      console.log("key,", key);
-      console.log("value,", value);
-      oracleaggregratemap.set(key[0].ExchangeRate.Token, value);
-    }
-  });
-  return oracleaggregratemap;
-}
-
-async function totalIssuance(api) {
-  const totalIssuances = await api.query.tokens.totalIssuance.entries();
-  const totalIssuancesemap = new Map();
-
-  totalIssuances.map((totalIssuance) => {
-    let key = totalIssuance[0].toHuman();
-
-    let value = totalIssuance[1].toHuman();
-
-    console.log("key", key);
-    console.log("value", value);
-    totalIssuancesemap.set(key[0].Token, value);
-  });
-  return totalIssuancesemap;
-}
-
-async function setup() {
+async function getValues() {
   const wsProvider = new WsProvider(
     "wss://interlay.api.onfinality.io/public-ws"
   );
@@ -125,8 +65,8 @@ assert!(total_issued < total_backable);
     // TODO  oracleaggregatecurrency/currentcurrencydecimal ie  (10^10 / 10^8)
     oracleaggregatecurrency = oracleaggregatecurrency.div(1e2);
 
-    totalUserVaultCollateralcurrency =
-      totalUserVaultCollateralcurrency.div(1e10);
+    // totalUserVaultCollateralcurrency =
+    //   totalUserVaultCollateralcurrency.div(1e10);
 
     total_backable = total_backable.add(
       totalUserVaultCollateralcurrency.div(oracleaggregatecurrency)
@@ -136,12 +76,28 @@ assert!(total_issued < total_backable);
   let total_issued = bignumber.from(
     totalIssuancesemap.get("IBTC").replaceAll(",", "")
   );
-  total_issued = total_issued.div(1e8);
-
-  console.log("total_backable", total_backable.toString());
-  console.log("total_issued", total_issued.toString());
+  // total_issued = total_issued.div(1e8);
 
   await api.disconnect();
+  total_backable = total_backable.div(1e2);
+  return {total_backable:total_backable.toString(),total_issued:total_issued.toString(), decimal:8}
+
+  // return total_backable,total_issued;
+
+  // console.log("total_backable", total_backable.toString());
+  // console.log("total_issued", total_issued.toString());
+
 }
 
-setup();
+app.get('/customer/interlay/state', async function (req, res) {
+  let values =  await getValues();
+  res.send(values);
+})
+
+let server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log(" App listening at http://%s:%s", host, port)
+})
+
+
