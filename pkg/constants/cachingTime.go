fa38b89bba52992eff3c3b068404ae3c258b1e19
Eric Delaporte <eric.delaporte@build-ideas.de>
2023-02-02 11:25:32 +0100

    marked constants as exported

--- pkg/constants/cachingTime.go
+++ pkg/constants/cachingTime.go
@@ -3,9 +3,9 @@ package constants
 import "time"
 
 const (
-	cachingTime1Sec   = 1 * time.Second
-	cachingTime20Secs = 20 * time.Second
-	cachingTimeShort  = time.Minute * 2
-	// cachingTimeMedium = time.Minute * 10
-	cachingTimeLong = time.Minute * 100
+	CachingTime1Sec   = 1 * time.Second
+	CachingTime20Secs = 20 * time.Second
+	CachingTimeShort  = time.Minute * 2
+	CachingTimeMedium = time.Minute * 10
+	CachingTimeLong   = time.Minute * 100
 )
