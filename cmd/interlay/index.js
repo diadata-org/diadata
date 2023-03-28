3693d1dea3c84e7266bb0dc4b8a5cf7f2e02d0c4
nicky <nicky.gurbani@gmail.com>
2023-03-28 06:49:17 +0530

    feature: LSD endpoints and methodology

--- cmd/interlay/index.js
+++ cmd/interlay/index.js
@@ -1,167 +1,33 @@
 const { ApiPromise, WsProvider } = require("@polkadot/api");
-const {
-  totalIssuance,
-  oracleaggregrate,
-  totalUserVaultCollateral,
-  collateralCurrencies,
-} = require("./helper");
-const { tokenPool, bifrosttokenIssuance ,vTokenIssuance} = require("./bifrosthelper");
+const { getInterlayValues } = require("./interlayhelper");
+const { getBiFrostValues } = require("./bifrosthelper");
 
-const ethers = require("ethers");
-const bignumber = ethers.BigNumber;
+const { cronstart } = require("./cron");
+let cron = require("node-cron");
+
+const { tokenkey, redis, allowedTokens, pricekey } = require("./utils");
+
+let cache = redis();
+
+cronstart();
+
+cron.schedule("10 * * * *", () => {
+  console.log("running a task every minute");
+  cronstart();
+});
 
 let express = require("express");
 let app = express();
 
-async function getBiFrostValues(token) {
-  let providerurl = "";
-
-  switch (token) {
-    case "KSM":
-      providerurl = "wss://bifrost-parachain.api.onfinality.io/public-ws";
-      break;
-  }
-
-  const wsProvider = new WsProvider(
-    // "wss://interlay.api.onfinality.io/public-ws"
-    providerurl
-  );
-  const api = await ApiPromise.create({
-    provider: wsProvider,
-  });
-
-  let tokeninpool = await tokenPool(api, token);
-  // let tokenIssuance = await bifrosttokenIssuance(api, token);
-  let vtokenIssuance = await vTokenIssuance(api,token)
-
-  bignumber.from(
-    tokeninpool.replaceAll(",", "")
-  ).toString()
-
-  console.log("tokeninpool",tokeninpool)
-
-  
-
-
-  return {
-    total_backable:  bignumber.from(
-      tokeninpool.replaceAll(",", "")
-    ).toString(),
-    total_issued: vtokenIssuance.toString(),
-    decimal: 12,
-    token: token,
-    time: Date.now(),
-
-  };
-}
-
-async function getValues(token) {
-  let providerurl = "";
-
-  switch (token) {
-    case "IBTC":
-      providerurl = "wss://interlay.api.onfinality.io/public-ws";
-      break;
-    case "KBTC":
-      providerurl = "wss://kintsugi.api.onfinality.io/public-ws";
-      break;
-  }
-  const wsProvider = new WsProvider(
-    // "wss://interlay.api.onfinality.io/public-ws"
-    providerurl
-  );
-  const api = await ApiPromise.create({
-    provider: wsProvider,
-  });
-
-  let collateralCurrenciesmap = new Map();
-  let totalUserVaultCollateralmap = new Map();
-  let oracleaggregatormap = new Map();
-  let totalIssuancesemap = new Map();
-
-  collateralCurrenciesmap = await collateralCurrencies(api);
-  console.log("collateralCurrencies", collateralCurrenciesmap);
-
-  totalUserVaultCollateralmap = await totalUserVaultCollateral(api);
-  console.log("totalUserVaultCollateral", totalUserVaultCollateralmap);
-
-  oracleaggregatormap = await oracleaggregrate(api);
-  console.log("oracleaggregrate", oracleaggregatormap);
-
-  totalIssuancesemap = await totalIssuance(api);
-  console.log("totalIssuance", totalIssuancesemap);
-
-  /*
-
-total_backable = collateral_currencies.map(|currency| vaultRegistry.totalUserVaultCollateral[currency] / oracle.aggregate[currency]).sum();
-total_issued = tokens.totalIssuance[IBTC];
-assert!(total_issued < total_backable);
-
-*/
-
-  let total_backable = bignumber.from(0);
-
-  for (let [collateralCurrency, value] of collateralCurrenciesmap) {
-    console.log("collateralCurrency", collateralCurrency);
-
-    let totalUserVaultCollateralcurrency = bignumber.from(
-      totalUserVaultCollateralmap.get(collateralCurrency).replaceAll(",", "")
-    );
-    let oracleaggregatecurrency = bignumber.from(
-      oracleaggregatormap.get(collateralCurrency).replaceAll(",", "")
-    );
-    console.log(
-      "totalUserVaultCollateralcurrenct",
-      totalUserVaultCollateralcurrency.toString()
-    );
-
-    oracleaggregatecurrency = oracleaggregatecurrency.div(1e12);
-    oracleaggregatecurrency = oracleaggregatecurrency.div(1e6); //TODO while doing 1e18 its crashing somehow.
-
-    let currentcurrencydecimal = 10;
-    let btcdecimal = 8;
-    // TODO  oracleaggregatecurrency/currentcurrencydecimal ie  (10^10 / 10^8)
-    oracleaggregatecurrency = oracleaggregatecurrency.div(1e2);
-
-    // totalUserVaultCollateralcurrency =
-    //   totalUserVaultCollateralcurrency.div(1e10);
-
-    total_backable = total_backable.add(
-      totalUserVaultCollateralcurrency.div(oracleaggregatecurrency)
-    );
-  }
-
-  console.log("totalIssuancesemap", totalIssuancesemap);
-
-  let total_issued = bignumber.from(
-    totalIssuancesemap.get(token).replaceAll(",", "")
-  );
-  // total_issued = total_issued.div(1e8);
-
-  await api.disconnect();
-  total_backable = total_backable.div(1e2);
-  return {
-    total_backable: total_backable.toString(),
-    total_issued: total_issued.toString(),
-    decimal: 8,
-    token: token,
-  };
-
-  // return total_backable,total_issued;
-
-  // console.log("total_backable", total_backable.toString());
-  // console.log("total_issued", total_issued.toString());
-}
-
 app.get("/customer/interlay/state/:token", async function (req, res) {
   // kbtc, ibtc
-  const allowedtokens = ["IBTC", "KBTC"];
+  const allowedTokens = ["IBTC", "KBTC"];
 
   let token = req.params.token;
   token = token.toUpperCase();
 
-  if (allowedtokens.includes(token)) {
-    let values = await getValues(token);
+  if (allowedTokens.includes(token)) {
+    let values = await getInterlayValues(token);
     res.send(values);
   } else {
     res.send({ err: "invalid token use, IBTC or KBTC" });
@@ -170,17 +36,69 @@ app.get("/customer/interlay/state/:token", async function (req, res) {
 
 app.get("/customer/bifrost/state/:token", async function (req, res) {
   // kbtc, ibtc
-  const allowedtokens = ["KSM"];
+  const allowedTokens = ["KSM"];
 
   let token = req.params.token;
   token = token.toUpperCase();
 
-  if (allowedtokens.includes(token)) {
+  if (allowedTokens.includes(token)) {
     let values = await getBiFrostValues(token);
     res.send(values);
   } else {
-    res.send({ err: "invalid token use, IBTC or KBTC" });
+    res.send({ err: "invalid token use, KSM" });
   }
+});
+
+let values = {
+  Token: "",
+  FairPrice: 100,
+  Collateralratio: {
+    IssuedToken: "1957632312",
+    LockedToken: "6853401398",
+    Ratio: "",
+  },
+  BaseAssetSymbol: "",
+  BaseAssetPrice: "100",
+  Issuer: "",
+};
+
+app.get("/xlsd", async function (req, res) {
+  // kbtc, ibtc
+
+  let response = [];
+
+  for (const token of allowedtokens) {
+    let tokenvalues = JSON.parse(JSON.stringify(values));
+    let baseassetprice;
+    let cacheddata = JSON.parse(
+      await cache.get(tokenkey(token.source, token.vtoken))
+    );
+    baseassetprice = await cache.get(pricekey(token.token));
+
+    if (cacheddata && cacheddata.collateral_ratio) {
+      tokenvalues.Collateralratio = {
+        IssuedToken: cacheddata.collateral_ratio.issued_token,
+        LockedToken: cacheddata.collateral_ratio.locked_token,
+        Ratio: cacheddata.collateral_ratio.ratio,
+      };
+    }
+    if (cacheddata && cacheddata.fair_price) {
+      tokenvalues.FairPrice = cacheddata.fair_price;
+    }
+    if (cacheddata && cacheddata.decimal) {
+      tokenvalues.decimal = cacheddata.decimal;
+    }
+    // if (cacheddata && cacheddata.ratio) {
+    //   tokenvalues.ratio = cacheddata.ratio;
+    // }
+    tokenvalues.BaseAssetPrice = parseFloat(baseassetprice);
+    tokenvalues.Token = token.vtoken;
+    tokenvalues.BaseAssetSymbol = token.token;
+    tokenvalues.Issuer = token.issuer;
+
+    response.push(tokenvalues);
+  }
+  res.send(response);
 });
 
 let server = app.listen(8081, function () {
---
+++ cmd/interlay/interlayhelper.js
@@ -0,0 +1,190 @@
+
+const { ApiPromise, WsProvider } = require("@polkadot/api");
+const ethers = require("ethers");
+const bignumber = ethers.BigNumber;
+const {createResponse,getPrice} = require("./utils")
+
+
+async function collateralCurrencies(api) {
+    const collateralCurrencies =
+      await api.query.vaultRegistry.systemCollateralCeiling.entries();
+    const collateralCurrenciesmap = new Map();
+    collateralCurrencies.map((collateralCurrencie) => {
+      let key = collateralCurrencie[0].toHuman();
+      let value = collateralCurrencie[1].toHuman();
+      // TODO filter only ibtc as wrapped {"collateral":{"Token":"DOT"},"wrapped":{"Token":"IBTC"}}
+      // console.log("key",key[0].collateral.Token);
+      // console.log("value",JSON.stringify(value));
+  
+      collateralCurrenciesmap.set(key[0].collateral.Token, value);
+    });
+    return collateralCurrenciesmap;
+  }
+  
+  //https://github.com/DefiLlama/DefiLlama-Adapters/blob/f0bcc4e65ebe43ce00c77951376a2249f2931dc3/projects/interlay-collateral/api.js
+  
+  async function totalUserVaultCollateral(api) {
+    const totalUserVaultCollaterals =
+      await api.query.vaultRegistry.totalUserVaultCollateral.entries();
+    const totalUserVaultCollateralsmap = new Map();
+  
+    totalUserVaultCollaterals.map((totalUserVaultCollateral) => {
+      let key = totalUserVaultCollateral[0].toHuman();
+      let value = totalUserVaultCollateral[1].toHuman();
+  
+      totalUserVaultCollateralsmap.set(key[0].collateral.Token, value);
+    });
+    return totalUserVaultCollateralsmap;
+  }
+  
+  async function oracleaggregrate(api) {
+    const oracleaggregrators = await api.query.oracle.aggregate.entries();
+  
+    const oracleaggregratemap = new Map();
+  
+    oracleaggregrators.map((oracleaggregrate) => {
+      let key = oracleaggregrate[0].toHuman();
+      let value = oracleaggregrate[1].toHuman();
+  
+      if (key[0].ExchangeRate) {
+        console.log("key,", key);
+        console.log("value,", value);
+        oracleaggregratemap.set(key[0].ExchangeRate.Token, value);
+      }
+    });
+    return oracleaggregratemap;
+  }
+  
+  async function totalIssuance(api) {
+    const totalIssuances = await api.query.tokens.totalIssuance.entries();
+    const totalIssuancesemap = new Map();
+  
+    totalIssuances.map((totalIssuance) => {
+      let key = totalIssuance[0].toHuman();
+  
+      let value = totalIssuance[1].toHuman();
+  
+      console.log("key", key);
+      console.log("value", value);
+      totalIssuancesemap.set(key[0].Token, value);
+    });
+    return totalIssuancesemap;
+  }
+  
+  async function getInterlayValues(token) {
+    let providerurl = "";
+  
+    switch (token) {
+      case "IBTC":
+        providerurl = "wss://interlay.api.onfinality.io/public-ws";
+        break;
+      case "KBTC":
+        providerurl = "wss://kintsugi.api.onfinality.io/public-ws";
+        break;
+    }
+    const wsProvider = new WsProvider(
+      // "wss://interlay.api.onfinality.io/public-ws"
+      providerurl
+    );
+    const api = await ApiPromise.create({
+      provider: wsProvider,
+    });
+  
+    let collateralCurrenciesmap = new Map();
+    let totalUserVaultCollateralmap = new Map();
+    let oracleaggregatormap = new Map();
+    let totalIssuancesemap = new Map();
+  
+    collateralCurrenciesmap = await collateralCurrencies(api);
+    console.log("collateralCurrencies", collateralCurrenciesmap);
+  
+    totalUserVaultCollateralmap = await totalUserVaultCollateral(api);
+    console.log("totalUserVaultCollateral", totalUserVaultCollateralmap);
+  
+    oracleaggregatormap = await oracleaggregrate(api);
+    console.log("oracleaggregrate", oracleaggregatormap);
+  
+    totalIssuancesemap = await totalIssuance(api);
+    console.log("totalIssuance", totalIssuancesemap);
+  
+    /*
+  
+  total_backable = collateral_currencies.map(|currency| vaultRegistry.totalUserVaultCollateral[currency] / oracle.aggregate[currency]).sum();
+  total_issued = tokens.totalIssuance[IBTC];
+  assert!(total_issued < total_backable);
+  
+  */
+  
+    let total_backable = bignumber.from(0);
+  
+    for (let [collateralCurrency, value] of collateralCurrenciesmap) {
+      console.log("collateralCurrency", collateralCurrency);
+
+      let  collateralCurrencyString = totalUserVaultCollateralmap.get(collateralCurrency)
+      let totalUserVaultCollateralcurrency;
+      if(collateralCurrencyString){
+        totalUserVaultCollateralcurrency = bignumber.from(
+          totalUserVaultCollateralmap.get(collateralCurrency).replaceAll(",", "")
+        );
+      }
+  
+        
+      let oracleaggregatecurrency = bignumber.from(
+        oracleaggregatormap.get(collateralCurrency).replaceAll(",", "")
+      );
+      // console.log(
+      //   "totalUserVaultCollateralcurrenct",
+      //   totalUserVaultCollateralcurrency.toString()
+      // );
+  
+      oracleaggregatecurrency = oracleaggregatecurrency.div(1e12);
+      oracleaggregatecurrency = oracleaggregatecurrency.div(1e6); //TODO while doing 1e18 its crashing somehow.
+  
+      let currentcurrencydecimal = 10;
+      let btcdecimal = 8;
+      // TODO  oracleaggregatecurrency/currentcurrencydecimal ie  (10^10 / 10^8)
+      oracleaggregatecurrency = oracleaggregatecurrency.div(1e2);
+  
+      // totalUserVaultCollateralcurrency =
+      //   totalUserVaultCollateralcurrency.div(1e10);
+  
+      if(totalUserVaultCollateralcurrency){
+        total_backable = total_backable.add(
+          totalUserVaultCollateralcurrency.div(oracleaggregatecurrency)
+        );
+      }
+      
+    }
+  
+    console.log("totalIssuancesemap", totalIssuancesemap);
+  
+    let total_issued = bignumber.from(
+      totalIssuancesemap.get(token).replaceAll(",", "")
+    );
+    // total_issued = total_issued.div(1e8);
+  
+    await api.disconnect();
+    total_backable = total_backable.div(1e2);
+
+
+    return {
+      total_backable: total_backable.toString(),
+      total_issued: total_issued.toString(),
+      decimal: 8,
+      token: token,
+    };
+  
+    // return total_backable,total_issued;
+  
+    // console.log("total_backable", total_backable.toString());
+    // console.log("total_issued", total_issued.toString());
+  }
+
+  
+ module.exports = {
+    totalIssuance:totalIssuance,
+    oracleaggregrate:oracleaggregrate,
+    totalUserVaultCollateral:totalUserVaultCollateral,
+    collateralCurrencies:collateralCurrencies,
+    getInterlayValues:getInterlayValues,
+ }
