24f4611e1cfdfccb56fa4e88c3f4c8916de3825d
jppade <jppade@googlemail.com>
2023-04-20 09:44:41 +0200

    minor changes in cmc hist quotes scraper.

--- cmd/foreignscraper/historicalQuotes/main.go
+++ cmd/foreignscraper/historicalQuotes/main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"os"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -12,46 +11,61 @@ import (
 	"net/http"
 
 	"github.com/diadata-org/diadata/pkg/dia"
-	"github.com/diadata-org/diadata/pkg/model"
+	models "github.com/diadata-org/diadata/pkg/model"
+	"github.com/diadata-org/diadata/pkg/utils"
 )
 
-func fetchCMCQuotation(assetSymbol string, timestamp time.Time, apiKey string) (float64, error) {
-	url := fmt.Sprintf("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/historical?symbol=%s&interval=hourly&time_start=%d", assetSymbol, timestamp.Unix())
+type cmcQuote struct {
+	Quote struct {
+		Price     float64   `json:"price"`
+		Timestamp time.Time `json:"timestamp"`
+	} `json:"USD"`
+}
+
+type cmcFullQuote struct {
+	FullQuote cmcQuote  `json:"quote"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+type cmcQuotes struct {
+	Quotes []cmcFullQuote `json:"quotes"`
+	Symbol string         `json:"symbol"`
+}
+
+type cmcResponse struct {
+	Data cmcQuotes `json:"data"`
+}
+
+func fetchCMCQuotations(assetSymbol string, starttime time.Time, endtime time.Time, apiKey string) (response cmcResponse, err error) {
+	log.Infof("starttime -- endtime: %v -- %v ", starttime, endtime)
+
+	url := fmt.Sprintf("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/historical?symbol=%s&interval=hourly&time_start=%d&time_end=%d", assetSymbol, starttime.Unix(), endtime.Unix())
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return 0, err
+		return
 	}
 
 	req.Header.Set("X-CMC_PRO_API_KEY", apiKey)
 
-	resp, err := http.DefaultClient.Do(req)
+	resp, statusCode, err := utils.HTTPRequest(req)
 	if err != nil {
-		return 0, err
+		return
 	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return 0, errors.New("non-200 status code from Coinmarketcap API")
-	}
-
-	var result map[string]interface{}
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		return 0, err
+	if statusCode != http.StatusOK {
+		err = errors.New("non-200 status code from Coinmarketcap API")
+		return
 	}
 
-	quoteData := result["data"].(map[string]interface{})
-	quote := quoteData[assetSymbol].(map[string]interface{})
-	priceData := quote["quote"].(map[string]interface{})
-	usdData := priceData["USD"].(map[string]interface{})
-	price, ok := usdData["price"].(float64)
-	if !ok {
-		return 0, errors.New("unable to extract price from API response")
+	if err := json.Unmarshal(resp, &response); err != nil {
+		log.Error("")
 	}
 
-	return price, nil
+	return
 }
 
 func main() {
+
+	// TO DO: Switch historical quotes to full assets.
 	log.Info("Starting historical quotes scraper")
 
 	// Create an instance of the DIA datastore
@@ -63,40 +77,46 @@ func main() {
 	ethAsset := dia.Asset{
 		Symbol:     "ETH",
 		Name:       "Ethereum",
-		Address:    "",
+		Address:    "0x0000000000000000000000000000000000000000",
 		Decimals:   18,
 		Blockchain: "Ethereum",
 	}
 
-	cmcAPIKey := os.Getenv("CMC_API_KEY")
+	cmcAPIKey := utils.Getenv("CMC_API_KEY", "676d0014-2760-485c-8e6d-3de41396cb48")
 
-	ticker := time.NewTicker(1 * time.Hour)
-	for range ticker.C {
-		// Get the timestamp of the last recorded quotation
-		lastTimestamp, err := datastore.GetLastHistoricalQuoteTimestamp(ethAsset.Symbol)
+	// Get the timestamp of the last recorded quotation
+	lastTimestamp, err := datastore.GetLastHistoricalQuoteTimestamp(ethAsset.Symbol)
+	if err != nil {
+		log.Infof("No historical quotes found for %s, using current time", ethAsset.Symbol)
+		endtime := time.Now()
+		starttime := endtime.AddDate(0, 0, -89)
+		quotes, err := fetchCMCQuotations(ethAsset.Symbol, starttime, endtime, cmcAPIKey)
 		if err != nil {
-			log.Infof("No historical quotes found for %s, using current time", ethAsset.Symbol)
-			lastTimestamp = time.Now().Add(-1 * time.Hour)
+			log.Errorf("Failed to fetch quotation from CMC API: %v", err)
 		}
+		for _, quote := range quotes.Data.Quotes {
+			historicalQuote := models.HistoricalQuote{
+				Symbol:    ethAsset.Symbol,
+				Price:     quote.FullQuote.Quote.Price,
+				QuoteTime: quote.FullQuote.Quote.Timestamp,
+				Source:    "Coinmarketcap",
+			}
+			if err := datastore.SetHistoricalQuote(&historicalQuote); err != nil {
+				log.Errorf("Failed to store historical quote: %v", err)
+			} else {
+				log.Infof("Stored historical quote for %s at %s", ethAsset.Symbol, quote.FullQuote.Quote.Timestamp)
+			}
+		}
+	}
+
+	ticker := time.NewTicker(1 * time.Minute)
+	for range ticker.C {
 
 		// Fetch hourly ETH quotations from the last timestamp until now
 		for timestamp := lastTimestamp.Add(1 * time.Hour); timestamp.Before(time.Now()); timestamp = timestamp.Add(1 * time.Hour) {
 			quote, err := datastore.GetAssetQuotation(ethAsset, timestamp)
 			if err != nil {
-				log.Infof("Quotation not found in our DB for timestamp %s, fetching from CMC API", timestamp)
-
-				price, err := fetchCMCQuotation(ethAsset.Symbol, timestamp, cmcAPIKey)
-				if err != nil {
-					log.Errorf("Failed to fetch quotation from CMC API: %v", err)
-					continue
-				}
-
-				quote = &models.AssetQuotation{
-					Asset:  ethAsset,
-					Price:  price,
-					Time:   timestamp,
-					Source: "Coinmarketcap",
-				}
+				log.Error("get asset quotation: ", err)
 			}
 
 			historicalQuote := models.HistoricalQuote{
