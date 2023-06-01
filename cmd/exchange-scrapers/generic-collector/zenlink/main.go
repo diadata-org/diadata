3a5ef27fec6277b6d13b93437c4029f04125cfc9
jppade <jppade@googlemail.com>
2023-06-01 16:40:01 +0200

    isolate zenlink dex scraper and add build files.

--- cmd/exchange-scrapers/generic-collector/zenlink/main.go
+++ cmd/exchange-scrapers/generic-collector/zenlink/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strconv"
 	"sync"
 	"time"
 
@@ -13,73 +12,31 @@ import (
 	scrapers "github.com/diadata-org/diadata/pkg/dia/scraper/exchange-scrapers"
 	models "github.com/diadata-org/diadata/pkg/model"
 	"github.com/diadata-org/diadata/pkg/utils"
-	"github.com/diadata-org/diadata/pkg/utils/probes"
 	"github.com/segmentio/kafka-go"
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-	exchange = flag.String("exchange", "", "which exchange")
+	exchange = dia.ZenlinkswapExchange
 	// mode==current:		default mode. Trades are forwarded to TBS and FBS.
 	// mode==storeTrades:	trades are not forwarded to TBS and FBS and stored as raw trades in influx.
 	// mode==estimation:	trades are forwarded to tradesEstimationService, i.e. same as storeTrades mode
 	//						but estimatedUSDPrice is filled by tradesEstimationService.
 	// mode==historical:	trades are sent through kafka to TBS in tradesHistorical topic.
-	mode                 = flag.String("mode", "current", "either storeTrades, current, historical or estimation")
-	swapTradesOnExchange = []string{
-		dia.CurveFIExchange,
-		dia.CurveFIExchangeFantom,
-		dia.CurveFIExchangeMoonbeam,
-		dia.CurveFIExchangePolygon,
-		dia.OmniDexExchange,
-		dia.DiffusionExchange,
-		dia.SolarbeamExchange,
-		dia.AnyswapExchange,
-		dia.HermesExchange,
-		dia.HuckleberryExchange,
-		dia.NetswapExchange,
-	}
-	pairsfile   = flag.Bool("pairsfile", false, "read pairs from json file in config folder.")
-	StartupDone = false
-	// delay until the service will be restarted
-	restartDelayMinutes, _ = strconv.Atoi(utils.Getenv("RESTART_DELAY_MINUTES", "720"))
-	startTime              = time.Now()
+	mode              = flag.String("mode", "current", "either storeTrades, current, historical or estimation")
+	pairsfile         = flag.Bool("pairsfile", false, "read pairs from json file in config folder.")
+	replicaKafkaTopic string
 )
-
-// main manages all PairScrapers and handles incoming trade information
-func ready() bool {
-	return StartupDone
-}
-
-func live() bool {
-	if !StartupDone {
-		return false
-	}
-	return startTime.Before(startTime.Add(time.Minute * time.Duration(restartDelayMinutes)))
-}
 
 func init() {
 	flag.Parse()
-	if *exchange == "" {
-		flag.Usage()
-		for e := range scrapers.Exchanges {
-			log.Info("exchange: ", e)
-		}
-		for {
-			time.Sleep(24 * time.Hour)
-		}
-	}
-	if !isValidExchange(*exchange) {
-		log.Fatal("Invalid exchange string: ", *exchange)
-	}
+	replicaKafkaTopic = utils.Getenv("REPLICA_KAFKA_TOPIC", "false")
 }
 
 // main manages all PairScrapers and handles incoming trade information
 func main() {
 
-	log.Infof("start collector for %s in %s mode...", *exchange, *mode)
-
-	log.Infof("%s starttime, %s duration", startTime, startTime.Add(time.Minute*time.Duration(restartDelayMinutes)))
+	log.Infof("start collector for %s in %s mode...", exchange, *mode)
 
 	relDB, err := models.NewRelDataStore()
 	if err != nil {
@@ -94,40 +51,37 @@ func main() {
 	// Fetch exchange pairs from database or json file in config folder.
 	var pairsExchange []dia.ExchangePair
 	if !*pairsfile {
-		pairsExchange, err = relDB.GetExchangePairSymbols(*exchange)
+		pairsExchange, err = relDB.GetExchangePairSymbols(exchange)
 		if err != nil {
 			log.Fatal("fetch pairs from database: ", err)
 		}
 	} else {
 		log.Error("error on GetExchangePairSymbols", err)
-		cc := configCollectors.NewConfigCollectors(*exchange, ".json")
+		cc := configCollectors.NewConfigCollectors(exchange, ".json")
 		pairsExchange = cc.AllPairs()
 	}
 	log.Info("available exchangePairs:", len(pairsExchange))
 
-	// Make API scraper.
-	configApi, err := dia.GetConfig(*exchange)
-	if err != nil {
-		log.Warning("no config for exchange's api ", err)
-	}
-
-	exchangeConfig, err := relDB.GetExchange(dia.KuCoinExchange)
+	exchangeConfig, err := relDB.GetExchange(dia.ZenlinkswapExchange)
 	if err != nil {
 		log.Warning("no config for exchange ", err)
 	}
 
-	diaExchange := dia.Exchange{Name: dia.KuCoinExchange, Centralized: true, WatchdogDelay: exchangeConfig.WatchdogDelay}
-	es := scrapers.NewKuCoinScraper(configApi.ApiKey, configApi.SecretKey, diaExchange, true, relDB)
+	diaExchange := dia.Exchange{Name: exchange, Centralized: true, WatchdogDelay: exchangeConfig.WatchdogDelay}
+	es := scrapers.NewZenlinkScraper(diaExchange, true)
 
 	// Set up kafka writers for various modes.
 	var (
-		w     *kafka.Writer
-		wTest *kafka.Writer
+		w *kafka.Writer
+		// This topic can be used to forward trades to services other than the prod. tradesblockservice.
+		wReplica *kafka.Writer
+		wTest    *kafka.Writer
 	)
 
 	switch *mode {
 	case "current":
 		w = kafkaHelper.NewWriter(kafkaHelper.TopicTrades)
+		wReplica = kafkaHelper.NewWriter(kafkaHelper.TopicTradesReplica)
 		wTest = kafkaHelper.NewWriter(kafkaHelper.TopicTradesTest)
 	case "estimation":
 		w = kafkaHelper.NewWriter(kafkaHelper.TopicTradesEstimation)
@@ -144,11 +98,11 @@ func main() {
 
 	wg := sync.WaitGroup{}
 
-	if scrapers.Exchanges[*exchange].Centralized {
+	if scrapers.Exchanges[exchange].Centralized {
 
 		// Scrape pairs for CEX scrapers.
 		for _, configPair := range pairsExchange {
-			log.Println("Adding pair:", configPair.Symbol, configPair.ForeignName, "on exchange", *exchange)
+			log.Println("Adding pair:", configPair.Symbol, configPair.ForeignName, "on exchange", exchange)
 			_, err := es.ScrapePair(dia.ExchangePair{
 				Symbol:      configPair.Symbol,
 				ForeignName: configPair.ForeignName})
@@ -167,15 +121,15 @@ func main() {
 		defer wg.Wait()
 
 	}
-	go handleTrades(es.Channel(), &wg, w, wTest, ds, *exchange, *mode)
-
-	probes.Start(live, ready)
-	StartupDone = true
+	go handleTrades(es.Channel(), &wg, w, wTest, wReplica, ds, exchange, *mode)
 }
 
-func handleTrades(c chan *dia.Trade, wg *sync.WaitGroup, w *kafka.Writer, wTest *kafka.Writer, ds *models.DB, exchange string, mode string) {
+func handleTrades(c chan *dia.Trade, wg *sync.WaitGroup, w *kafka.Writer, wTest *kafka.Writer, wReplica *kafka.Writer, ds *models.DB, exchange string, mode string) {
 	lastTradeTime := time.Now()
 	watchdogDelay := scrapers.Exchanges[exchange].WatchdogDelay
+	if watchdogDelay == 0 {
+		watchdogDelay = scrapers.ExchangeDuplicates[exchange].WatchdogDelay
+	}
 	t := time.NewTicker(time.Duration(watchdogDelay) * time.Second)
 	for {
 		select {
@@ -202,9 +156,18 @@ func handleTrades(c chan *dia.Trade, wg *sync.WaitGroup, w *kafka.Writer, wTest 
 					log.Error(err)
 				}
 
-				// Write trade to test Kafka.
-				if mode == "current" {
-					err = writeTradeToKafka(wTest, t)
+				if scrapers.Exchanges[t.Source].Centralized {
+					// Write CEX trades to test Kafka.
+					if mode == "current" {
+						err = writeTradeToKafka(wTest, t)
+						if err != nil {
+							log.Error(err)
+						}
+					}
+				}
+
+				if replicaKafkaTopic == "true" {
+					err := writeTradeToKafka(wReplica, t)
 					if err != nil {
 						log.Error(err)
 					}
@@ -216,15 +179,11 @@ func handleTrades(c chan *dia.Trade, wg *sync.WaitGroup, w *kafka.Writer, wTest 
 				err := ds.SaveTradeInflux(t)
 				if err != nil {
 					log.Error(err)
-				} else {
-					log.Info("saved trade")
 				}
 			}
 
 			if mode == "assetmap" {
-
 				fmt.Println("recieved trade", t)
-
 			}
 		}
 	}
@@ -237,26 +196,16 @@ func writeTradeToKafka(w *kafka.Writer, t *dia.Trade) error {
 		return err
 	}
 
-	// Write reversed trade to Kafka as well for some exchanges.
-	if utils.Contains(&swapTradesOnExchange, t.Source) {
-		tSwapped, err := dia.SwapTrade(*t)
+	// Write reversed trade to Kafka as well for Zenlink.
+	tSwapped, err := dia.SwapTrade(*t)
+	if err != nil {
+		log.Error("swap trade: ", err)
+	} else {
+		err = kafkaHelper.WriteMessage(w, &tSwapped)
 		if err != nil {
-			log.Error("swap trade: ", err)
-		} else {
-			err = kafkaHelper.WriteMessage(w, &tSwapped)
-			if err != nil {
-				return err
-			}
+			return err
 		}
 	}
-	return nil
-}
 
-func isValidExchange(estring string) bool {
-	for e := range scrapers.Exchanges {
-		if e == estring {
-			return true
-		}
-	}
-	return false
+	return nil
 }
