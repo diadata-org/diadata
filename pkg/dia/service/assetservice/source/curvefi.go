dd12ec867443da5760e6f2de1d1f49f6e105f09b
jppade <jppade@googlemail.com>
2023-09-06 14:29:20 +0200

    add factories to curve assetcollector and liquidity scraper.

--- pkg/dia/service/assetservice/source/curvefi.go
+++ pkg/dia/service/assetservice/source/curvefi.go
@@ -55,7 +55,8 @@ func NewCurvefiAssetSource(exchange dia.Exchange) *CurvefiAssetSource {
 		cryptoswapPools := curveRegistry{Type: 1, Address: common.HexToAddress("0x8F942C20D02bEfc377D41445793068908E2250D0")}
 		metaPools := curveRegistry{Type: 2, Address: common.HexToAddress("0xB9fC157394Af804a3578134A6585C0dc9cc990d4")}
 		factoryPools := curveRegistry{Type: 3, Address: common.HexToAddress("0xF18056Bbd320E96A48e3Fbf8bC061322531aac99")}
-		registries := []curveRegistry{factoryPools, basePools, cryptoswapPools, metaPools}
+		factory2Pools := curveRegistry{Type: 3, Address: common.HexToAddress("0x4F8846Ae9380B90d2E71D5e3D042dff3E7ebb40d")}
+		registries := []curveRegistry{factoryPools, factory2Pools, basePools, cryptoswapPools, metaPools}
 		cas = makeCurvefiAssetSource(exchange, registries, curveRestDialEth, uniswapWaitMilliseconds)
 	case dia.CurveFIExchangeFantom:
 		exchange.Contract = ""
