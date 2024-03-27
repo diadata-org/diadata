675830449bc3d809b605bc192d937054f4dcd9c1
jppade <jppade@googlemail.com>
2024-03-27 11:25:17 +0100

    fix api url in okx trades scraper.

--- pkg/dia/scraper/exchange-scrapers/OKExScraper.go
+++ pkg/dia/scraper/exchange-scrapers/OKExScraper.go
@@ -19,7 +19,6 @@ import (
 	"github.com/zekroTJA/timedmap"
 )
 
-// var _OKExSocketURL = "wss://ws.okex.com:8443/ws/v5/public"
 var _OKExSocketURL = utils.Getenv("OKEX_WS_URL", "wss://ws.okx.com:8443/ws/v5/public")
 
 //var _OKExSocketURL = url.URL{Scheme: "wss", Host: "real.okex.com:10441", Path: "/ws/v1", RawQuery: "compress=true"}
@@ -78,7 +77,6 @@ func NewOKExScraper(exchange dia.Exchange, scrape bool, relDB *models.RelDB) *OK
 	if err != nil {
 		log.Error("dial:", err)
 	}
-
 	s.wsClient = SwConn
 	if scrape {
 		go s.mainLoop()
@@ -131,13 +129,12 @@ type AllOKEXMarketResponse struct {
 
 // Subscribe again to all channels
 func (s *OKExScraper) subscribeToALL() {
-
 	var (
 		resp     AllOKEXMarketResponse
 		allPairs []OKEXArgs
 	)
 
-	b, _, err := utils.GetRequest("https://ws.okx.com/api/v5/public/instruments?instType=SPOT")
+	b, _, err := utils.GetRequest("https://okx.com/api/v5/public/instruments?instType=SPOT")
 	if err != nil {
 		log.Errorln("Error getting OKex market", err)
 	}
