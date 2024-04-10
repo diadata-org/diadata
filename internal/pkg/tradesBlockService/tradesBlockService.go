fd7bf4e75b60501b20651ce5ca41788332c8c197
jppade <jppade@googlemail.com>
2024-04-10 09:03:42 +0200

    amend logging in tbs.

--- internal/pkg/tradesBlockService/tradesBlockService.go
+++ internal/pkg/tradesBlockService/tradesBlockService.go
@@ -278,7 +278,7 @@ func (s *TradesBlockService) checkTrade(t dia.Trade) bool {
 	if err == nil {
 		liquidity, lowerBound := pool.GetPoolLiquidityUSD()
 		if quoteVolumeOk && !lowerBound && quoteVolume > volumeLiquidityRatio*liquidity {
-			log.Warn("discard trade due to volumeLiquidityRatio: ", t)
+			log.Warnf("pool %s has liquidity %v.\n discard trade due to volumeLiquidityRatio: %v", pool.Address, liquidity, t)
 			return false
 		}
 	}
