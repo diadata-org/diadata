ed4c7f3ee6630de7a3ee98882e8a9f75e83f0a04
jppade <jppade@googlemail.com>
2024-11-05 09:40:52 +0100

    add pearlfistableswap scraper suite.

--- pkg/dia/Config.go
+++ pkg/dia/Config.go
@@ -114,6 +114,7 @@ const (
 	MaverickExchangeBNB                = "Maverick-bnb"
 	PearlfiExchangeTestnet             = "Pearlfi-Testnet"
 	PearlfiExchange                    = "Pearlfi"
+	PearlfiStableswapExchange          = "PearlfiStableswap"
 	RamsesV1Exchange                   = "RamsesV1"
 	RamsesV2Exchange                   = "RamsesV2"
 	NileV1Exchange                     = "NileV1"
