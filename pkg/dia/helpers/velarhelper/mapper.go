df0433a1862d22d3571035a64d2fa56aeb8b689b
nkorn1lov <nikolai.kornilov@protofire.io>
2025-01-30 17:25:06 +0300

    velar: fix trade handling of univ2 pools

--- pkg/dia/helpers/velarhelper/mapper.go
+++ pkg/dia/helpers/velarhelper/mapper.go
@@ -1,66 +1,121 @@
 package velarhelper
 
 import (
-	"math/big"
+	"encoding/hex"
+	"fmt"
 	"strings"
+
+	stacks "github.com/diadata-org/diadata/pkg/dia/helpers/stackshelper"
 )
 
-func ExtractSwapInfo(response string) SwapInfo {
-	swapInfo := SwapInfo{}
+func DecodeSwapEvents(tx stacks.Transaction) ([]SwapEvent, error) {
+	result := make([]SwapEvent, 0, len(tx.Events))
+	lastEventIndex := -1
 
-	swapInfo.AmountIn = extractUint(response, "amt-in u")
-	swapInfo.AmountOut = extractUint(response, "amt-out u")
-	swapInfo.TokenIn = extractString(response, "token-in '")
-	swapInfo.TokenOut = extractString(response, "token-out '")
-	swapInfo.Symbol = extractString(response, "symbol \"")
-	swapInfo.LpToken = extractString(response, "lp-token '")
-	swapInfo.Token0 = extractString(response, "token0 '")
-	swapInfo.Token1 = extractString(response, "token1 '")
+	for i, e := range tx.Events {
+		if !isSwapEvent(e) {
+			continue
+		}
+		bytes, err := hex.DecodeString(e.ContractLog.Value.Hex[2:])
+		if err != nil {
+			return nil, err
+		}
+		event, err := stacks.DeserializeCVTuple(bytes)
+		if err != nil {
+			return nil, err
+		}
 
-	swapInfo.ProtocolFee = extractFee(response, "protocol-fee (tuple (den u", " (num u")
-	swapInfo.ShareFee = extractFee(response, "share-fee (tuple (den u", " (num u")
-	swapInfo.SwapFee = extractFee(response, "swap-fee (tuple (den u", " (num u")
+		info := SwapEvent{TxID: tx.TxID, Timestamp: tx.BlockTime}
+		info.AmountIn, err = stacks.DeserializeCVUint(event["amt-in"])
+		if err != nil {
+			return nil, err
+		}
+		info.AmountOut, err = stacks.DeserializeCVUint(event["amt-out"])
+		if err != nil {
+			return nil, err
+		}
 
-	return swapInfo
+		poolAssets, err := decodePoolAssets(event["pool"])
+		if err != nil {
+			return nil, err
+		}
+		info.TickerID = poolAssets[0] + "_" + poolAssets[1]
+
+		if val, ok := event["token-in"]; ok {
+			info.TokenIn, err = stacks.DeserializeCVPrincipal(val)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			sender, err := stacks.DeserializeCVPrincipal(event["user"])
+			if err != nil {
+				return nil, err
+			}
+		outer:
+			for j := lastEventIndex + 1; j < i; j++ {
+				ev := &tx.Events[j]
+				if ev.Asset.EventType != "transfer" ||
+					ev.Asset.Sender != sender ||
+					ev.Asset.Recipient != e.ContractLog.ContractID {
+					continue
+				}
+
+				switch ev.Type {
+				case "fungible_token_asset":
+					info.TokenIn = strings.Split(ev.Asset.ID, "::")[0]
+					break outer
+				case "stx_asset":
+					for _, a := range poolAssets {
+						if a == DeployerAddress+".wstx" {
+							info.TokenIn = a
+							break outer
+						}
+					}
+				}
+			}
+		}
+
+		switch info.TokenIn {
+		case poolAssets[0]:
+			info.TokenOut = poolAssets[1]
+		case poolAssets[1]:
+			info.TokenOut = poolAssets[0]
+		default:
+			err = fmt.Errorf("unable to decode input token address: no matches for %s", info.TokenIn)
+			return nil, err
+		}
+
+		result = append(result, info)
+		lastEventIndex = i
+	}
+
+	return result, nil
 }
 
-func extractUint(response, prefix string) *big.Int {
-	result := big.NewInt(0)
-
-	start := strings.Index(response, prefix)
-	if start == -1 {
-		return result
-	}
-	start += len(prefix)
-
-	end := strings.IndexAny(response[start:], " )")
-	if end == -1 {
-		return result
-	}
-
-	result.SetString(response[start:start+end], 10)
-	return result
+func isSwapEvent(ev stacks.Event) bool {
+	log := ev.ContractLog
+	return ev.Type == "smart_contract_log" &&
+		log.Topic == "print" &&
+		(strings.HasPrefix(log.ContractID, DeployerAddress) || strings.HasPrefix(log.ContractID, DeployerAddressV2)) &&
+		strings.Contains(log.Value.Repr, "(op \"swap\")")
 }
 
-func extractString(response, prefix string) string {
-	start := strings.Index(response, prefix)
-	if start == -1 {
-		return ""
-	}
-	start += len(prefix)
+func decodePoolAssets(src []byte) ([2]string, error) {
+	result := [2]string{}
 
-	end := strings.IndexAny(response[start:], " )")
-	if end == -1 {
-		return ""
+	pool, err := stacks.DeserializeCVTuple(src)
+	if err != nil {
+		return result, err
 	}
 
-	return response[start : start+end]
-}
+	result[0], err = stacks.DeserializeCVPrincipal(pool["token0"])
+	if err != nil {
+		return result, err
+	}
+	result[1], err = stacks.DeserializeCVPrincipal(pool["token1"])
+	if err != nil {
+		return result, err
+	}
 
-func extractFee(response, denPrefix, numPrefix string) Fee {
-	fee := Fee{}
-	fee.Denominator = extractUint(response, denPrefix)
-	fee.Numerator = extractUint(response, numPrefix)
-
-	return fee
+	return result, nil
 }
