ba810305cd355a1812cec11fb849561bc11bd608
Samuel Brack <samuel.brack@diadata.org>
2025-09-19 12:49:25 +0200

    Add kafka logs for TBS (#1224)
    * Add kafka logs for TBS
    
    * Fix format string

--- cmd/services/tradesBlockService/main.go
+++ cmd/services/tradesBlockService/main.go
@@ -89,6 +89,8 @@ func main() {
 	go handleBlocks(service, &wg, kafkaWriter)
 
 	log.Printf("starting...")
+	log.Printf("Reading from Kafka topic:  %d", tradesTopic)
+	log.Printf("Writing to Kafka topic:    %d", tradesBlockTopic)
 
 	for {
 		m, err := kafkaReader.ReadMessage(context.Background())
